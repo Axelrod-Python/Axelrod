2ae4555ad4976b91cc0f66ca7ca992b3bc8e5f84
vince <vincent.knight@gmail.com>
2015-02-18 08:39:08 +0000

    Have list of strategies in init file

--- axelrod/strategies/__init__.py
+++ axelrod/strategies/__init__.py
@@ -1,1 +1,17 @@
 from cooperator import *
+from defector import *
+from grudger import *
+from random import *
+from titfortat import *
+from gobymajority import *
+
+strategies = [
+        Defector,
+        Cooperator,
+        TitForTat,
+        Grudger,
+        GoByMajority,
+        Random,
+        ]
+
+
