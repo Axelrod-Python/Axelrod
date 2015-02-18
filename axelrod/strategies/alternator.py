94870874c254bdaba26d15661ad94bd2af2c3c85
Vincent Knight <vincent.knight@gmail.com>
2015-02-18 12:11:23 +0000

    Adding alternator to the players

---
+++ axelrod/strategies/alternator.py
@@ -0,0 +1,19 @@
+from axelrod import Player
+
+class Alternator(Player):
+    """
+    A player who alternates between cooperating and defecting
+    """
+    def strategy(self, opponent):
+        """
+        Alternate 'C' and 'D'
+        """
+        if self.history[-1] == 'C':
+            return 'D'
+        return 'C'
+
+    def __repr__(self):
+        """
+        The string method for the strategy:
+        """
+        return 'Alternator'
