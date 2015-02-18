d2e4cb743ba5a8cbefbf2c60db00b1974d1d8d1e
vince <vincent.knight@gmail.com>
2015-02-18 11:26:56 +0000

    Have tests for grudger

--- axelrod/strategies/grudger.py
+++ axelrod/strategies/grudger.py
@@ -6,27 +6,6 @@ class Grudger(Player):
     """
     def strategy(self, opponent):
         """
-        Begins by playing 'C':
-
-            >>> P1 = Grudger()
-            >>> P2 = Player()
-            >>> P1.strategy(P2)
-            'C'
-
-        This is affected by the history of the opponent:
-
-            >>> P1.history = ['C', 'C', 'C']
-            >>> P2.history = ['C', 'C', 'C']
-            >>> P1.strategy(P2)
-            'C'
-
-        If at any point the opponent defects then the player will forever defect:
-
-            >>> P1.history = ['C', 'C', 'D', 'D', 'D']
-            >>> P2.history = ['C', 'D', 'C', 'C', 'C']
-            >>> P1.strategy(P2)
-            'D'
-        >>>
         """
         if 'D' in opponent.history:
             return 'D'
@@ -34,10 +13,6 @@ class Grudger(Player):
 
     def __repr__(self):
         """
-i       The string method for the strategy:
-
-            >>> P1 = Grudger()
-            >>> print P1
-            Grudger
+        The string method for the strategy.
         """
         return 'Grudger'
