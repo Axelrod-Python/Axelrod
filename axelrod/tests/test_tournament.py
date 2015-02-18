313127447f4522f3776fb37730cfd1240ec4e86b
vince <vincent.knight@gmail.com>
2015-02-18 10:50:32 +0000

    A lot of methods written to test tournament

--- axelrod/tests/test_tournament.py
+++ axelrod/tests/test_tournament.py
@@ -1,10 +1,8 @@
 import unittest
 import axelrod
+import random
 
-class TestTournament(unittest.TestCase):
-
-    def setUp(self):
-        pass
+class TestInitialisation(unittest.TestCase):
 
     def test_initialisation(self):
         """
@@ -15,6 +13,9 @@ class TestTournament(unittest.TestCase):
         P3 = axelrod.Defector()
         tournament = axelrod.Axelrod(P1, P2, P3)
         self.assertEqual([str(s) for s in tournament.players], ['Defector', 'Defector', 'Defector'])
+
+
+class TestRoundRobin(unittest.TestCase):
 
     def test_round_robin_defector_v_cooperator(self):
         """
@@ -81,6 +82,50 @@ class TestTournament(unittest.TestCase):
         tournament = axelrod.Axelrod(P1, P2, P3, P4, P5)
         tournament.round_robin(turns=10)
         self.assertEqual([(str(player), player.score) for player in sorted(tournament.players, key=lambda x: x.score)], [('Tit For Tat', 101), ('Grudger', 101), ('Go By Majority', 101), ('Defector', 108), ('Cooperator', 110)])
+
+class Tournament(unittest.TestCase):
+
+    def test_tournament(self):
+        """
+        Test tournament
+        """
+        P1 = axelrod.Cooperator()
+        P2 = axelrod.TitForTat()
+        P3 = axelrod.Defector()
+        P4 = axelrod.Grudger()
+        P5 = axelrod.GoByMajority()
+        tournament = axelrod.Axelrod(P1, P2, P3, P4, P5)
+        results = tournament.tournament(turns=200, repetitions=5)
+        self.assertEqual(sorted([(str(player), results[player]) for player in sorted(results.keys())]), sorted([
+       ('Tit For Tat', [2001, 2001, 2001, 2001, 2001]),
+       ('Cooperator', [2200, 2200, 2200, 2200, 2200]),
+       ('Defector', [2388, 2388, 2388, 2388, 2388]),
+       ('Grudger', [2001, 2001, 2001, 2001, 2001]),
+       ('Go By Majority', [2001, 2001, 2001, 2001, 2001])]))
+
+    def test_reset_player_history(self):
+        """
+        Test that method to reset history in between repetitions of tournament works
+        """
+        P1 = axelrod.Defector()
+        P1.history = ['C', 'D']
+        P2 = axelrod.Defector()
+        P2.history = ['D', 'D']
+        tournament = axelrod.Axelrod(P1, P2)
+        tournament.reset_player_history()
+        self.assertEqual([[],[]],[player.history for player in tournament.players])
+
+    def test_reset_player_scores(self):
+        """
+        Test that method to reset scores in between repetitions of tournament works
+        """
+        P1 = axelrod.Defector()
+        P1.score = 58
+        P2 = axelrod.Defector()
+        P2.score = 95
+        tournament = axelrod.Axelrod(P1, P2)
+        tournament.reset_player_scores()
+        self.assertEqual([0, 0],[player.score for player in tournament.players])
 
 if __name__ == '__main__':
     unittest.main()
