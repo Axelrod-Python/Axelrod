44a8dfc771f5f51be9bdc050ba2c60edc5c0b3b3
vince <vincent.knight@gmail.com>
2015-02-18 11:33:56 +0000

    Test for random player

--- axelrod/strategies/rand.py
+++ axelrod/strategies/rand.py
@@ -7,41 +7,12 @@ class Random(Player):
     """
     def strategy(self, opponent):
         """
-        Always returns 'C'
-
-            >>> random.seed(1)
-            >>> P1 = Random()
-            >>> P2 = Player()
-            >>> P1.strategy(P2)
-            'C'
-
-        This is not affect by the history of either player:
-
-            >>> random.seed(1)
-            >>> P1.history = ['C', 'D', 'C']
-            >>> P2.history = ['C', 'C', 'D']
-            >>> P1.strategy(P2)
-            'C'
-
-        It is simply a random choice:
-
-            >>> random.seed(2)
-            >>> P1.strategy(P2)
-            'D'
-            >>> P1.strategy(P2)
-            'D'
-            >>> P1.strategy(P2)
-            'C'
-        >>>
+        Randomly picks a strategy (not affected by history)
         """
         return random.choice(['C','D'])
 
     def __repr__(self):
         """
-i       The string method for the strategy:
-
-            >>> P1 = Cooperator()
-            >>> print P1
-            Cooperator
+        The string method for the strategy:
         """
         return 'Random'
---
+++ axelrod/tests/test_rand.py
@@ -0,0 +1,28 @@
+"""
+Test for the random strategy
+"""
+import unittest
+import axelrod
+import random
+
+class TestRandom(unittest.TestCase):
+
+    def test_strategy(self):
+        """
+        Test that strategy is randomly picked (not affected by history)
+        """
+        random.seed(1)
+        P1 = axelrod.Random()
+        P2 = axelrod.Player()
+        self.assertEqual(P1.strategy(P2), 'C')
+        random.seed(1)
+        P1.history = ['C', 'D', 'C']
+        P2.history = ['C', 'C', 'D']
+        self.assertEqual(P1.strategy(P2), 'C')
+        random.seed(2)
+        self.assertEqual(P1.strategy(P2), 'D')
+
+    def test_representation(self):
+        P1 = axelrod.Random()
+        self.assertEqual(str(P1), 'Random')
+
