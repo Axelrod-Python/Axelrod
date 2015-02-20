19f136abb7f65d1c39d22b72ab51c97f787eaf4e
vince <vincent.knight@gmail.com>
2015-02-20 11:36:44 +0000

    Sorting the axis for the plot

--- run_tournament.py
+++ run_tournament.py
@@ -4,6 +4,7 @@ A script to run the Axelrod tournament using all the strategies present in `axel
 from __future__ import division
 import axelrod
 import matplotlib.pyplot as plt
+from numpy import median
 
 turns = 1000  # Number of turns in the round robin tournament
 repetitions = 50  # Number of repetitions of the tournament
@@ -11,8 +12,9 @@ repetitions = 50  # Number of repetitions of the tournament
 strategies = [strategy() for strategy in axelrod.strategies]
 axelrod = axelrod.Axelrod(*strategies)
 results = axelrod.tournament(turns=turns, repetitions=repetitions)
+players = sorted(axelrod.players, key = lambda x: median(results[x]))
 
-plt.boxplot([[score / turns for score in results[player]] for player in axelrod.players])
-plt.xticks(range(1, len(axelrod.players) + 2), [str(p) for p in axelrod.players], rotation=90)
+plt.boxplot([[score / turns for score in results[player]] for player in players])
+plt.xticks(range(1, len(axelrod.players) + 2), [str(p) for p in players], rotation=90)
 plt.title('Mean score per stage game over {} rounds repeated {} times'.format(turns, repetitions))
 plt.savefig('results.png', bbox_inches='tight')
