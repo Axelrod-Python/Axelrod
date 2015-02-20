6d1837ef992d7c22c9d60a3690c434abdf8d8c29
Martin Chorley <m.j.chorley@cs.cardiff.ac.uk>
2015-02-20 16:25:42 +0000

    working on visualisation

--- run_tournament.py
+++ run_tournament.py
@@ -2,6 +2,7 @@
 A script to run the Axelrod tournament using all the strategies present in `axelrod/strategies`
 """
 from __future__ import division
+import csv
 import axelrod
 import matplotlib.pyplot as plt
 from numpy import median
@@ -13,6 +14,12 @@ strategies = [strategy() for strategy in axelrod.strategies]
 axelrod = axelrod.Axelrod(*strategies)
 results = axelrod.tournament(turns=turns, repetitions=repetitions)
 players = sorted(axelrod.players, key = lambda x: median(results[x]))
+
+with open('results.csv', 'w') as csv_file:
+    csv_writer = csv.DictWriter(csv_file, ['player', 'scores'])
+    csv_writer.writeheader()
+    for player in players:
+        csv_writer.writerow({'player': player, 'scores': results[player]})
 
 plt.boxplot([[score / (turns * (len(players)-1)) for score in results[player]] for player in players])
 plt.xticks(range(1, len(players) + 2), [str(p) for p in players], rotation=90)
