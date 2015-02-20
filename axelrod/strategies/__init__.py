43a6115185d3385051810598cd3aa02292d8efe7
Karol M. Langner <karol.langner@gmail.com>
2015-02-20 16:41:23 -0500

    Merge GoByMajority with the version with limited memory

--- axelrod/strategies/__init__.py
+++ axelrod/strategies/__init__.py
@@ -4,7 +4,6 @@ from grudger import *
 from rand import *
 from titfortat import *
 from gobymajority import *
-from gobyrecent import *
 from alternator import *
 from grumpy import *
 from averagecopier import *
@@ -18,10 +17,10 @@ strategies = [
         TitForTat,
         Grudger,
         GoByMajority,
-        GoByRecentMajority5,
-        GoByRecentMajority10,
-        GoByRecentMajority20,
-        GoByRecentMajority40,
+        GoByMajority5,
+        GoByMajority10,
+        GoByMajority20,
+        GoByMajority40,
         Random,
         Alternator,
         Grumpy,
