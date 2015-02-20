8b04f494475cf69881cd7624b53925158a25e635
jasonyoung <jas.young314@gmail.com>
2015-02-20 13:33:37 +0000

    removed divide by zero error

--- run_tournament.py
+++ run_tournament.py
@@ -16,12 +16,13 @@ all_strategies = strategies + cheating_strategies
 graphs_to_plot = {'results.png':strategies, 'cheating_results.png':cheating_strategies, 'all_results':all_strategies}
 
 for plot in graphs_to_plot:
-    axelrod_tournament = axelrod.Axelrod(*graphs_to_plot[plot])
-    results = axelrod_tournament.tournament(turns=turns, repetitions=repetitions)
-    players = sorted(axelrod_tournament.players, key = lambda x: median(results[x]))
+    if not len(graphs_to_plot[plot]) == 1:
+        axelrod_tournament = axelrod.Axelrod(*graphs_to_plot[plot])
+        results = axelrod_tournament.tournament(turns=turns, repetitions=repetitions)
+        players = sorted(axelrod_tournament.players, key = lambda x: median(results[x]))
 
-    plt.boxplot([[score / turns for score in results[player]] for player in players])
-    plt.xticks(range(1, len(axelrod_tournament.players) + 2), [str(p) for p in players], rotation=90)
-    plt.title('Mean score per stage game over {} rounds repeated {} times'.format(turns, repetitions))
-    plt.savefig(plot, bbox_inches='tight')
-    plt.clf()
+        plt.boxplot([[score / (turns * (len(players) - 1)) for score in results[player]] for player in players])
+        plt.xticks(range(1, len(axelrod_tournament.players) + 2), [str(p) for p in players], rotation=90)
+        plt.title('Mean score per stage game over {} rounds repeated {} times'.format(turns, repetitions))
+        plt.savefig(plot, bbox_inches='tight')
+        plt.clf()
