8c49a6f59ce410aa268d0085fa2a5ee1d5615647
vince <vincent.knight@gmail.com>
2015-02-20 19:19:01 +0000

    Modifying the strategy slightly

--- axelrod/strategies/geller.py
+++ axelrod/strategies/geller.py
@@ -3,7 +3,7 @@
 """
 Geller - by Martin Chorley (@martinjc), heavily inspired by Matthew Williams (@voxmjw)
 
-This code is inspired by Matthew Williams' talk 
+This code is inspired by Matthew Williams' talk
 "Cheating at rock-paper-scissors — meta-programming in Python"
 given at Django Weekend Cardiff in February 2014.
 
@@ -11,7 +11,7 @@ His code is here: https://github.com/mattjw/rps_metaprogramming
 and there's some more info here: http://www.mattjw.net/2014/02/rps-metaprogramming/
 
 This code is **way** simpler than Matt's, as in this exercise we already
-have access to the opponent instance, so don't need to go 
+have access to the opponent instance, so don't need to go
 hunting for it in the stack. Instead we can just call it to
 see what it's going to play, and return a result based on that
 
@@ -26,18 +26,11 @@ from axelrod import Player
 class Geller(Player):
 
     def strategy(self, opponent):
-
-        #
-        # See what the opponent will throw
-        # respond with whatever gives least jail time
-        rand_state = random.getstate()
-        opp_move = opponent.strategy(self)
-        random.setstate(rand_state)
-
-        if opp_move == 'C':
-            return 'C'
-        elif opp_move == 'D':
-            return 'D'
+        """
+        Look at what the opponent will play in the next round and choose strategy that gives the least jail time.
+        This is equivalent to playing the same strategy as that which the opponent will play.
+        """
+        return opponent.strategy(self)
 
     def __repr__(self):
         """
