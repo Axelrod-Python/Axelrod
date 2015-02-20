67b9be95c8fa62d30b879727bb29736fa26834f7
Karol M. Langner <karol.langner@gmail.com>
2015-02-20 16:28:23 -0500

    Generalize GoByRecentMajority and add 5, 10, 20 and 40 variants

--- axelrod/tests/test_gobyrecent.py
+++ axelrod/tests/test_gobyrecent.py
@@ -6,27 +6,33 @@ import axelrod
 
 class TestGoByRecent(unittest.TestCase):
 
+    lengths = [5, 10, 20, 40]
+    get_player = lambda self, L: getattr(axelrod, 'GoByRecentMajority%i' % L)()
+
     def test_initial_strategy(self):
         """
         Starts by cooperating
         """
-        P1 = axelrod.GoByRecentMajority10()
         P2 = axelrod.Player()
-        self.assertEqual(P1.strategy(P2), 'C')
+        for L in self.lengths:
+            P1 = self.get_player(L)
+            self.assertEqual(P1.strategy(P2), 'C')
 
     def test_effect_of_strategy(self):
         """
         If opponent cooperates at least as often as they defect in the last ten turns then the player cooperates
         """
-        P1 = axelrod.GoByRecentMajority10()
         P2 = axelrod.Player()
-        P1.history = ['D'] * 15
-        P2.history = ['D'] * 10 + ['C'] * 5
-        self.assertEqual(P1.strategy(P2), 'C')
-        P1.history = ['C'] * 15
-        P2.history = ['C'] * 9 + ['D'] * 6
-        self.assertEqual(P1.strategy(P2), 'D')
+        for L in self.lengths:
+            P1 = self.get_player(L)
+            P1.history = ['D'] * int(1.5*L)
+            P2.history = ['D'] * (L-1) + ['C'] * (L//2 + 1)
+            self.assertEqual(P1.strategy(P2), 'C')
+            P1.history = ['C'] * int(1.5*L)
+            P2.history = ['C'] * (L-1) + ['D'] * (L//2 + 1)
+            self.assertEqual(P1.strategy(P2), 'D')
 
     def test_representation(self):
-        P1 = axelrod.GoByRecentMajority10()
-        self.assertEqual(str(P1), 'Go By Recent Majority 10')
+        for L in self.lengths:
+            P1 = self.get_player(L)
+            self.assertEqual(str(P1), 'Go By Majority/%i' % L)
