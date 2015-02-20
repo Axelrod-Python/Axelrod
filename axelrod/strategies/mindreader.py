2e3453d5987594a164bb5931e77d4630d31d3bbb
jasonyoung <jas.young314@gmail.com>
2015-02-20 11:53:59 +0000

    refactored the way mindreader looks ahead

--- axelrod/strategies/mindreader.py
+++ axelrod/strategies/mindreader.py
@@ -15,38 +15,33 @@ class MindReader(Player):
         return ['C','D'][best_strategy]
 
 
+
+    def simulate_match(self, opponent, strategy, rounds = 50):
+        """
+        Simulates a number of matches 
+        """
+        for match in range(rounds):
+            play_1, play_2 = strategy, opponent.strategy(self)
+            self.history.append(play_1)
+            opponent.history.append(play_2)
+
     def look_ahead(self, opponent, rounds = 50):
         """
         Plays a number of rounds to determine the best strategy 
         """
         results = []
         tournement = Axelrod()
+        strategies = ['C', 'D']
 
         dummy_history_self = copy.copy(self.history)
         dummy_history_opponent = copy.copy(opponent.history)
 
-        for match in range(rounds):
-            play_1 = self.strategy_coop(opponent)
-            play_2 = opponent.strategy(self)
-            self.history.append(play_1)
-            opponent.history.append(play_2)
+        for strategy in strategies:
+            self.simulate_match(opponent, strategy, rounds)
+            results.append(tournement.calculate_scores(self, opponent)[0])
 
-        results.append(tournement.calculate_scores(self, opponent)[0])
-
-        self.history = copy.copy(dummy_history_self)
-        opponent.history = copy.copy(dummy_history_opponent)
-
-        for match in range(rounds):
-            play_1 = self.strategy_defect(opponent)
-            play_2 = opponent.strategy(self)
-            self.history.append(play_1)
-            opponent.history.append(play_2)
-
-
-        results.append(tournement.calculate_scores(self, opponent)[0])
-
-        self.history = copy.copy(dummy_history_self)
-        opponent.history = copy.copy(dummy_history_opponent)
+            self.history = copy.copy(dummy_history_self)
+            opponent.history = copy.copy(dummy_history_opponent)
 
         return results.index(min(results))
 
@@ -55,17 +50,3 @@ class MindReader(Player):
         The string method for the strategy.
         """
         return 'Mind Reader'
-
-    def strategy_coop(self, opponent):
-        """
-        Always Cooperate
-        """
-
-        return 'C'
-
-    def strategy_defect(self, opponent):
-        """
-        Always defect
-        """
-
-        return 'D'
