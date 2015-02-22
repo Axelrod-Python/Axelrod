df115e01abbfd5523e970e84cc2f6f828126ca59
Thomas Campbell <thomas.campbell@tanti.org.uk>
2015-02-22 15:28:03 +0000

    Adds forgiver unittest.

---
+++ axelrod/tests/test_forgiver.py
@@ -0,0 +1,35 @@
+"""
+Test for the forgiver strategy
+"""
+import unittest
+import axelrod
+
+class TestForgiver(unittest.TestCase):
+
+    def test_initial_strategy(self):
+        """
+        Starts by cooperating
+        """
+        P1 = axelrod.Forgiver()
+        P2 = axelrod.Player()
+        self.assertEqual(P1.strategy(P2), 'C')
+
+    def test_effect_of_strategy(self):
+        """
+        If opponent has defected more than 10 percent of the time, defect.
+        """
+        P1 = axelrod.Forgiver()
+        P2 = axelrod.Player()
+        P1.history = ['C', 'C', 'C', 'C']
+        P2.history = ['C', 'C', 'C', 'C']
+        self.assertEqual(P1.strategy(P2), 'C')
+        P1.history = ['C', 'C', 'C', 'C', 'D']
+        P2.history = ['C', 'C', 'C', 'D', 'C']
+        self.assertEqual(P1.strategy(P2), 'D')
+        P1.history = ['C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C']
+        P2.history = ['C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'D']
+        self.assertEqual(P1.strategy(P2), 'C')
+
+    def test_representation(self):
+        P1 = axelrod.Forgiver()
+        self.assertEqual(str(P1), 'Forgiver')
