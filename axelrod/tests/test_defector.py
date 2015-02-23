4ec97f38f7df17add6637e3fa62510301e597119
Karol M. Langner <karol.langner@gmail.com>
2015-02-23 18:20:47 -0500

    Add Tricky Defector

--- axelrod/tests/test_defector.py
+++ axelrod/tests/test_defector.py
@@ -20,3 +20,17 @@ class TestDefector(unittest.TestCase):
     def test_representation(self):
         P1 = axelrod.Defector()
         self.assertEqual(str(P1), 'Defector')
+
+class TestTrickyDefector(unittest.TestCase):
+
+    def test_strategy(self):
+        """Test if it is trying to trick opponent."""
+        P1 = axelrod.TrickyDefector()
+        P2 = axelrod.Player()
+        self.assertEqual(P1.strategy(P2), 'D')
+        P1.history = ['D', 'D', 'D', 'D']
+        P2.history = ['C', 'D', 'D', 'D']
+
+    def test_representation(self):
+        P1 = axelrod.TrickyDefector()
+        self.assertEqual(str(P1), 'Tricky Defector')
