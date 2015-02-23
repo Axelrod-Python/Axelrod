ebb2824f851b3457322f7875443ade4f39513b17
Martin Chorley <m.j.chorley@cs.cardiff.ac.uk>
2015-02-23 21:01:17 +0000

    better panning + zooming, also reset

--- js/vis.js
+++ js/vis.js
@@ -1,22 +1,5 @@
 (function() {
 
-    // function for handling zoom event
-    var zoom_handler = function() {
-        container.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-    }
-
-    // find the indexes of the whisker reach values
-    function whisker_reach(d, k) {
-        var q1 = d3.quantile(d, 0.25);  // first quartile
-        var q3 = d3.quantile(d, 0.75);  // third quartile
-        var reach = (q3 - q1) * k;      // reach
-        var i = -1;                     // start at the beginning
-        while (d[++i] < (q1 - reach));    // find the first non-outlier
-        var j = d.length;               // start at the end
-        while (d[--j] > (q3 + reach));    // find the first non-outlier
-        return [i, j];                  // return the indexes
-    }
-
     // the visualisation
     var svg;
     var container;
@@ -27,9 +10,6 @@
 
     // some padding around the visualisation
     var padding = {'bottom': 170, 'left': 90, 'right': 40, 'top': 40};
-
-    // how long transitions will last
-    var transition_duration = 1000;
 
     // scale from median values to height
     var y_scale = d3.scale.linear()
@@ -48,23 +28,66 @@
     // axes
     var x_axis = d3.svg.axis()
         .scale(x_scale)
-        .orient("bottom");
+        .orient('bottom');
 
     var y_axis = d3.svg.axis()
         .scale(y_scale)
-        .orient("left");
+        .orient('left');
+
+    // axes
+    var x_axis = d3.svg.axis()
+        .scale(x_scale)
+        .orient('bottom');
+
+    var y_axis = d3.svg.axis()
+        .scale(y_scale)
+        .orient('left');
+
+    // how long transitions will last
+    var transition_duration = 1000;
+
+    // function for handling zoom event
+    var zoom_handler = function() {
+        
+        svg.select('.y.axis')
+            .transition()
+            .call(y_axis);
+
+        svg.select('.x.axis')
+            .transition()
+            .attr('transform', 'translate(' + d3.event.translate[0] + ',' +(height-padding.bottom)+ ')')
+            .call(x_axis.scale(x_scale.rangeRoundBands([padding.left * d3.event.scale, (width-padding.right) * d3.event.scale],.5 * d3.event.scale, .4 * d3.event.scale)));
+
+        svg.select('.x.axis')
+            .selectAll('text')
+            .style('text-anchor', 'end');
+
+        container.attr('transform', 'translate(' + d3.event.translate + ')scale(' + d3.event.scale + ')');
+    }
+
+
+    // find the indexes of the whisker reach values
+    function whisker_reach(d, k) {
+        var q1 = d3.quantile(d, 0.25);  // first quartile
+        var q3 = d3.quantile(d, 0.75);  // third quartile
+        var reach = (q3 - q1) * k;      // reach
+        var i = -1;                     // start at the beginning
+        while (d[++i] < (q1 - reach));    // find the first non-outlier
+        var j = d.length;               // start at the end
+        while (d[--j] > (q3 + reach));    // find the first non-outlier
+        return [i, j];                  // return the indexes
+    }
 
     var zoom = d3.behavior.zoom()
         .scaleExtent([0, 10])
-        .on("zoom", zoom_handler);
+        .on("zoom", zoom_handler)
+        .y(y_scale);
 
     // data
     var results;
     // player names
     var players;
     var cheaters;
-
-
 
     // initialise the visualisation
     var init_plot = function() {
@@ -86,21 +109,37 @@
             .call(zoom);
 
         container = svg.append('g')
-            .attr("class", "container")
+            .attr('class', 'container')
             .call(zoom);
 
+        svg.append('rect')
+            .attr('x', padding.left)
+            .attr('y', height - padding.bottom)
+            .attr('width', width - padding.right)
+            .attr('height', padding.bottom)
+            .attr('fill', 'white')
+            .attr('stroke', 'none');
+
+        svg.append('rect')
+            .attr('x', 0)
+            .attr('y', 0)
+            .attr('width', padding.left)
+            .attr('height', height)
+            .attr('fill', 'white')
+            .attr('stroke', 'none');
+
         // add an x-axis element
-        container.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0," + (height-padding.bottom) + ")");
+        svg.append('g')
+          .attr('class', 'x axis')
+          .attr('transform', 'translate(0,' + (height-padding.bottom) + ')');
 
         // rotate the x-axis labels
-        container.selectAll('.x.axis text')
-            .style("text-anchor", "end")
-            .attr("transform", "translate(-15,10)rotate(-90)");
+        svg.selectAll('.x.axis text')
+            .style('text-anchor', 'end')
+            .attr('transform', 'translate(-15,10)rotate(-90)');
 
         // add a y-axis and label
-        container.append("g")
+        svg.append("g")
           .attr("class", "y axis")
           .attr("transform", "translate(" + (padding.left) + ", " + "0)")
         .append("text")
@@ -110,6 +149,7 @@
           .attr("dy", "1em")
           .style("text-anchor", "middle")
           .text("Mean score per game over 200 rounds repeated 50 times");
+
     }
 
     // function to draw a circle for an outlying data value
@@ -123,6 +163,36 @@
             .attr('fill', 'none')
             .attr('class', 'outlier')
             .style('opacity', 0);            
+    }
+
+    var reset = function() {
+        svg.remove();
+        var sel = document.getElementById('result_select');
+        var selected = sel.options[sel.selectedIndex].value;
+
+        // reset y_scale scale from median values to height
+        y_scale = d3.scale.linear()
+            .range([height-padding.bottom, padding.top]);
+
+        // reset x_scale from player name to width
+        x_scale = d3.scale.ordinal()
+            .rangeRoundBands([padding.left, width-padding.right], 0.5, 0.4);
+        
+        // reset x_axis
+        x_axis = d3.svg.axis()
+            .scale(x_scale)
+            .orient('bottom');
+
+        // reset y_axis
+        y_axis = d3.svg.axis()
+            .scale(y_scale)
+            .orient('left');
+        
+        // reinitialise plot
+        init_plot();
+
+        // load the currently selected results set
+        load_results(selected);        
     }
 
     // load results from the specified file
@@ -160,6 +230,8 @@
             cheater_scale.domain(medians);
             y_scale.domain([min, max]);
             x_scale.domain(players);
+
+            zoom.y(y_scale);
 
             // draw the visualisation
             draw_plot(results);
@@ -324,17 +396,17 @@
             .remove();
           
         // update the axes
-        container.select('.x.axis')
+        svg.select('.x.axis')
             .transition()
             .duration(transition_duration)
             .call(x_axis);
 
-        container.select('.x.axis')
+        svg.select('.x.axis')
             .selectAll('text')
             .attr('transform', 'translate(-15, 15)rotate(-90)')
             .style('text-anchor', 'end');
 
-        container.select('.y.axis')
+        svg.select('.y.axis')
             .transition()
             .duration(transition_duration)
             .call(y_axis);
@@ -346,6 +418,8 @@
         var selected = this.options[this.selectedIndex].value;
         load_results(selected);
     });
+
+    d3.select('#reset').on('click', reset);
 
     // initialise the plot and draw some results
     init_plot();
