5b6ec93f89182ebbd3276f4d09b7eb2af410f0c9
James Campbell <james.campbell@tanti.org.uk>
2015-02-25 18:48:45 +0000

    creates new strategy, Retaliate

---
+++ axelrod/strategies/retaliate.py
@@ -0,0 +1,16 @@
+from axelrod import Player
+
+
+class Retaliate(Player):
+    """
+    A player starts by cooperating but will retaliate once the opponent
+    has won more than 10 times the number of defections the player has.
+    """
+    name = "Retaliate"
+
+    def strategy(self, opponent):
+        history = zip(self.history, opponent.history)
+        if history.count(('C', 'D')) > history.count(('D', 'C')) * 0.1:
+            return 'D'
+
+        return 'C'
