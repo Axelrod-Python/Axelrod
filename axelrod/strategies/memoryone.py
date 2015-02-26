60a5318a59439e83cedee25ef060bd443c880ad5
Marc Harper <marc.harper@gmail.com>
2015-02-26 11:07:06 -0800

    More efficient WSLS

--- axelrod/strategies/memoryone.py
+++ axelrod/strategies/memoryone.py
@@ -4,6 +4,34 @@ from axelrod import Player
 
 """IPD Strategies: http://www.prisoners-dilemma.com/strategies.html"""
 
+class WinStayLoseShift(Player):
+    """Win-Stay Lose-Shift, also called Pavlov."""
+
+    name = 'Win-Stay Lose-Shift'
+
+    def __init__(self, initial='C'):
+        Player.__init__(self)
+        self._four_vector = dict( zip(  [ ('C','C'), ('C','D'), ('D','C'), ('D','D')], map(float, four_vector) ) )
+        
+        self.response_dict = {
+            ('C','C'): 'C',
+            ('C','D'): 'D',            
+            ('D','C'): 'D',
+            ('D','D'): 'C',
+        }
+        self._initial = initial
+    
+    def strategy(self, opponent):
+        """Almost always cooperates, but will try to trick the opponent by defecting.
+
+        Defect once in a while in order to get a better payout, when the opponent
+        has not defected in the last ten turns and only cooperated during last 3 turns.
+        """
+        if not opponent.history:
+            return self._initial
+        last_round = (self.history[-1], opponent.history[-1])
+        return self.response_dict[last_round]
+
 class MemoryOnePlayer(Player):
     """Uses a four-vector for strategies based on the last round of play, (P(C|CC), P(C|CD), P(C|DC), P(C|DD)), defaults to Win-Stay Lose-Shift ."""
 
@@ -25,15 +53,6 @@ class MemoryOnePlayer(Player):
         if r < p:
             return 'C'
         return 'D'
-
-class WinStayLoseShift(MemoryOnePlayer):
-    """Win-Stay Lose-Shift, also called Pavlov."""
-
-    name = 'Win-Stay Lose-Shift'
-
-    def __init__(self):
-        four_vector = [1,0,0,1]
-        super(self.__class__, self).__init__(four_vector)
 
 def zd_vector1(chi):
     return (1. - (2. * chi - 2.) / (4. * chi + 1.), 0.,
