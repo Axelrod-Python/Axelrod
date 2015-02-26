6d2f92b8a4d0e7dc78388306cfef559c4d770a5f
jasonyoung <jas.young314@gmail.com>
2015-02-26 14:39:04 +0000

    added more tests and documentation

--- axelrod/strategies/mindcontrol.py
+++ axelrod/strategies/mindcontrol.py
@@ -6,7 +6,9 @@ class MindControl(Player):
     name = 'Mind Control'
 
     def strategy(self, opponent):
-        """Alters the opponents strategy method """
+        """Alters the opponents strategy method to be a lambda function which always returns C
+        This player will then always return D to take advantage of this
+        """
         
         opponent.strategy = lambda opponent: 'C'
 
