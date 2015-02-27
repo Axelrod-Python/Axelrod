1b764c1fbe7fe2971c552ceba915ffc7c9132624
Timothy Standen <timothyf1@live.co.uk>
2015-02-27 18:12:08 +0000

    Tiyded the code

--- axelrod/strategies/mathematicalconstants.py
+++ axelrod/strategies/mathematicalconstants.py
@@ -1,66 +1,41 @@
 from axelrod import Player
 from math import sqrt, pi, e
 
-class Golden(Player):
+class CotoDeRatio(Player):
+
+    def strategy(self,opponent):
+
+        """initially co-operates"""
+        if len(opponent.history) == 0:
+            return 'C'
+
+        """to avoid initial division by zero"""
+        if sum([s == 'D' for s in opponent.history]) == 0:
+            return 'D'
+
+        """otherwise compare ratio to golden mean"""
+        if (sum([p == 'C' for p in opponent.history]) + sum([q == 'C' for q in self.history]))/(sum([x == 'D' for x in opponent.history]) + sum([y == 'D' for y in self.history])) > self.ratio:
+            return 'D'
+        return 'C'
+
+
+class Golden(CotoDeRatio):
     """The player will always aim to bring the ratio of co-operations to defections closer to the golden mean"""
 
     name = 'Golden'
-    golden_ratio = (1 + sqrt(5)) / 2
-
-    def strategy(self,opponent):
-
-        """initially co-operates"""
-        if len(opponent.history) == 0:
-            return 'C'
-
-        """to avoid initial division by zero"""
-        if sum([s == 'D' for s in opponent.history]) == 0:
-            return 'D'
-
-        """otherwise compare ratio to golden mean"""
-        if (sum([p == 'C' for p in opponent.history]) + sum([q == 'C' for q in self.history]))/(sum([x == 'D' for x in opponent.history]) + sum([y == 'D' for y in self.history])) > self.golden_ratio:
-            return 'D'
-        return 'C'
+    ratio = (1 + sqrt(5)) / 2
 
 
-class Pi(Player):
-    """The player will always aim to bring the ratio of co-operations to defections closer to the golden mean"""
+class Pi(CotoDeRatio):
+    """The player will always aim to bring the ratio of co-operations to defections closer to the pi"""
 
     name = 'Pi'
-
-    def strategy(self,opponent):
-
-        """initially co-operates"""
-        if len(opponent.history) == 0:
-            return 'C'
-
-        """to avoid initial division by zero"""
-        if sum([s == 'D' for s in opponent.history]) == 0:
-            return 'D'
-
-        """otherwise compare ratio to golden mean"""
-        if (sum([p == 'C' for p in opponent.history]) + sum([q == 'C' for q in self.history]))/(sum([x == 'D' for x in opponent.history]) + sum([y == 'D' for y in self.history])) > pi:
-            return 'D'
-        return 'C'
+    ratio = pi
 
 
-class e(Player):
-    """The player will always aim to bring the ratio of co-operations to defections closer to the golden mean"""
+class e(CotoDeRatio):
+    """The player will always aim to bring the ratio of co-operations to defections closer to the e"""
 
     name = 'e'
-
-    def strategy(self,opponent):
-
-        """initially co-operates"""
-        if len(opponent.history) == 0:
-            return 'C'
-
-        """to avoid initial division by zero"""
-        if sum([s == 'D' for s in opponent.history]) == 0:
-            return 'D'
-
-        """otherwise compare ratio to golden mean"""
-        if (sum([p == 'C' for p in opponent.history]) + sum([q == 'C' for q in self.history]))/(sum([x == 'D' for x in opponent.history]) + sum([y == 'D' for y in self.history])) > e:
-            return 'D'
-        return 'C'
+    ratio = e
 
