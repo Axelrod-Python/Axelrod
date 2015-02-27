e6ff6aa124d3176e8ef48147b3f4f9184aac884c
Owen Campbell <owen.campbell@empiria.co.uk>
2015-02-27 18:29:32 +0000

    adds reset method and test

--- axelrod/strategies/retaliate.py
+++ axelrod/strategies/retaliate.py
@@ -14,3 +14,39 @@ class Retaliate(Player):
             return 'D'
 
         return 'C'
+
+
+class LimitedRetaliate(Player):
+    """
+    A player that co-operates unless the opponent defects and wins.
+    It will then retaliate by defecting. It stops when either, it has beaten
+    the opponent 10 times more often that it has lost or it reaches the
+    retaliation limit (20 defections).
+    """
+    name = 'Limited Retaliate'
+    retaliation_limit = 20
+    retaliating = False
+    retaliation_count = 0
+
+    def strategy(self, opponent):
+        history = zip(self.history, opponent.history)
+        if history.count(('C', 'D')) > history.count(('D', 'C')) * 0.1:
+            self.retaliating = True
+        else:
+            self.retaliating = False
+            self.retaliation_count = 0
+
+        if self.retaliating:
+            if self.retaliation_count < self.retaliation_limit:
+                self.retaliation_count += 1
+                return 'D'
+            else:
+                self.retaliation_count = 0
+                self.retaliating = False
+
+        return 'C'
+
+    def reset(self):
+        super(LimitedRetaliate, self).reset()
+        self.retaliating = False
+        self.retaliation_count = 0
