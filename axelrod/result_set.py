ab5b04eed99098673365f2d270d1d3bc47749993
Owen Campbell <owen.campbell@tanti.org.uk>
2015-03-03 21:37:44 +0000

    adds content to plot method

--- axelrod/result_set.py
+++ axelrod/result_set.py
@@ -1,5 +1,6 @@
 import numpy
 from StringIO import StringIO
+import matplotlib.pyplot as plt
 
 
 class ResultSet(object):
@@ -8,10 +9,12 @@ class ResultSet(object):
     and to return them as plots or csv files.
     """
 
-    def __init__(self, players, repetitions):
+    def __init__(self, players, turns, repetitions):
         self.players = players
         self.nplayers = len(players)
         player_list = list(range(len(players)))
+        self.turns = turns
+        self.repetitions = repetitions
         repetition_list = list(range(repetitions))
         self.results = [
             [[0 for irep in repetition_list]
@@ -40,7 +43,12 @@ class ResultSet(object):
             self.output_initialised = True
 
     def plot(self):
-        pass
+        self.init_output()
+        fig = plt.figure()
+        plt.boxplot([s / (self.turns * (len(self.ranking) - 1)) for s in self.scores[self.ranking]])
+        plt.xticks(range(1, len(self.ranked_names) + 2), [str(n) for n in self.ranked_names], rotation=90)
+        plt.title('Mean score per stage game over {} rounds repeated {} times ({} strategies)'.format(self.turns, self.repetitions, len(self.ranking)))
+        return fig
 
     def csv(self):
         self.init_output()
