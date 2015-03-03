c458f5d49d229e9b7359cd6850517b782a4ab133
Owen Campbell <owen.campbell@tanti.org.uk>
2015-03-03 18:19:35 +0000

    refactors csv method to separate testable methods

--- axelrod/result_set.py
+++ axelrod/result_set.py
@@ -10,32 +10,46 @@ class ResultSet(object):
 
     def __init__(self, players, repetitions):
         self.players = players
+        self.nplayers = len(players)
         player_list = list(range(len(players)))
         repetition_list = list(range(repetitions))
         self.results = [
             [[0 for irep in repetition_list]
                 for j in player_list] for i in player_list]
+        self.output_initialised = False
 
-    def scores(self):
+    def generate_scores(self):
         numpy_array_results = numpy.array(self.results)
         return numpy_array_results.sum(axis=1)
+
+    def generate_ranking(self, scores):
+        ranking = sorted(
+            range(self.nplayers),
+            key=lambda i: numpy.median(scores[i]))
+        return ranking
+
+    def generate_ranked_names(self, ranking):
+        ranked_names = [str(self.players[i]) for i in ranking]
+        return ranked_names
+
+    def init_output(self):
+        if not self.output_initialised:
+            self.scores = self.generate_scores()
+            self.ranking = self.generate_ranking(self.scores)
+            self.ranked_names = self.generate_ranked_names(self.ranking)
+            self.output_initialised = True
 
     def plot(self):
         pass
 
     def csv(self):
-        nplayers = len(self.players)
-        scores = self.scores()
-        ranking = sorted(
-            range(nplayers),
-            key=lambda i: numpy.median(scores[i]))
-        ranked_names = [str(self.players[i]) for i in ranking]
+        self.init_output()
         csv_string = StringIO()
-        header = ", ".join(ranked_names) + "\n"
+        header = ", ".join(self.ranked_names) + "\n"
         csv_string.write(header)
         numpy.savetxt(
             csv_string,
-            scores[ranking].transpose(),
+            self.scores[self.ranking].transpose(),
             delimiter=", ",
             fmt='%i')
         return csv_string.getvalue()
