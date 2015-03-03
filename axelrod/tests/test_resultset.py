32e19848311e528c326fc205831551b455848ac8
Owen Campbell <owen.campbell@tanti.org.uk>
2015-03-03 18:19:35 +0000

    refactors csv method to separate testable methods

--- axelrod/tests/test_resultset.py
+++ axelrod/tests/test_resultset.py
@@ -21,14 +21,25 @@ class TestResultSet(unittest.TestCase):
             [[0, 0, 0, 0], [0, 0, 0, 0]],
             [[0, 0, 0, 0], [0, 0, 0, 0]]
         ]
+        self.assertEquals(rs.nplayers, 2)
         self.assertEquals(rs.players, players)
         self.assertEquals(rs.results, expected_results)
+        self.assertFalse(rs.output_initialised)
 
-    def test_scores(self):
+    def test_generate_scores(self):
         players = (axelrod.Player(), axelrod.Player())
         rs = axelrod.ResultSet(players, 4)
         expected_results = numpy.array([[0, 0, 0, 0], [0, 0, 0, 0]])
-        self.assertTrue(numpy.array_equal(rs.scores(), expected_results))
+        self.assertTrue(numpy.array_equal(rs.generate_scores(), expected_results))
+
+    def test_generate_ranking(self):
+        pass
+
+    def test_generate_ranked_names(self):
+        pass
+
+    def test_init_output(self):
+        pass
 
     def test_plot(self):
         pass
