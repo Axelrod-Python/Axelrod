dd9f81c1d73ddb47fd9a2d2cde640bd99d292de8
vince <vincent.knight@gmail.com>
2015-03-06 10:58:38 +0000

    Have made directory to output optional

--- run_tournament.py
+++ run_tournament.py
@@ -6,6 +6,7 @@ The code for strategies is present in `axelrod/strategies`.
 from __future__ import division
 
 import argparse
+import os
 import time
 
 import numpy
@@ -15,19 +16,19 @@ import matplotlib.pyplot as plt
 import axelrod
 
 
-def run_tournament(turns, repetitions, exclude_basic, exclude_strategies, exclude_cheating, exclude_all):
+def run_tournament(turns, repetitions, exclude_basic, exclude_strategies, exclude_cheating, exclude_all, output_directory):
     """Main function for running Axelrod tournaments."""
     graphs_to_plot = {}
 
     init_strategies = lambda S: [s() for s in S]
     if not exclude_basic:
-        graphs_to_plot['basic_results.png'] = init_strategies(axelrod.basic_strategies)
+        graphs_to_plot[os.path.join(output_directory, 'basic_results.png')] = init_strategies(axelrod.basic_strategies)
     if not exclude_strategies:
-        graphs_to_plot['results.png'] = init_strategies(axelrod.strategies)
+        graphs_to_plot[os.path.join(output_directory, 'results.png')] = init_strategies(axelrod.strategies)
     if not exclude_cheating:
-        graphs_to_plot['cheating_results.png'] = init_strategies(axelrod.cheating_strategies)
+        graphs_to_plot[os.path.join(output_directory, 'cheating_results.png')] = init_strategies(axelrod.cheating_strategies)
     if not exclude_all:
-        graphs_to_plot['all_results.png'] = init_strategies(axelrod.all_strategies)
+        graphs_to_plot[os.path.join(output_directory, 'all_results.png')] = init_strategies(axelrod.all_strategies)
 
     for plot in graphs_to_plot:
         if len(graphs_to_plot[plot]) != 1:
@@ -43,14 +44,14 @@ def run_tournament(turns, repetitions, exclude_basic, exclude_strategies, exclud
             # # Save the scores from this tournament to a CSV file.
             csv = results.csv()
             fname = plot.replace('.png', '.csv')
-            with open('./assets/' + fname, 'w') as f:
+            with open(fname, 'w') as f:
                 f.write(csv)
 
             # Save plots with the scores.
             plt.boxplot([s / (turns * (len(results.ranking) - 1)) for s in results.scores[results.ranking]])
             plt.xticks(range(1, len(results.ranked_names) + 2), [str(n) for n in results.ranked_names], rotation=90)
             plt.title('Mean score per stage game over {} rounds repeated {} times ({} strategies)'.format(turns, repetitions, len(results.ranking)))
-            plt.savefig('./assets/' + plot, bbox_inches='tight')
+            plt.savefig(plot, bbox_inches='tight')
             plt.clf()
 
 if __name__ == "__main__":
@@ -59,6 +60,7 @@ if __name__ == "__main__":
     parser.add_argument('-v', '--verbose', action='store_true', help='show verbose messages')
     parser.add_argument('-t', '--turns', type=int, default=200, help='turns per pair')
     parser.add_argument('-r', '--repetitions', type=int, default=50, help='round-robin repetitions')
+    parser.add_argument('-o', '--output_directory', default='./assets/', help='output directory')
     parser.add_argument('--xb', action='store_true', help='exlude basic strategies plot')
     parser.add_argument('--xs', action='store_true', help='exlude ordinary strategies plot')
     parser.add_argument('--xc', action='store_true', help='exclude cheating strategies plot')
@@ -73,7 +75,7 @@ if __name__ == "__main__":
         print 'Ordinary strategies plot: ' + str(not args.xs)
         print 'Cheating strategies plot: ' + str(not args.xc)
         print 'Combined strategies plot: ' + str(not args.xa)
-    run_tournament(args.turns, args.repetitions, args.xb, args.xs, args.xc, args.xa)
+    run_tournament(args.turns, args.repetitions, args.xb, args.xs, args.xc, args.xa, args.output_directory)
 
     dt = time.time() - t0
     print "Finished in %.1fs" % dt
