36af339de626e8f49c2a348d856913ae0509dc7b
Owen Campbell <owen.campbell@tanti.org.uk>
2015-03-07 17:32:19 +0000

    adds content to dataset, xticks and title methods

--- axelrod/box_plot.py
+++ axelrod/box_plot.py
@@ -1,13 +1,18 @@
 class BoxPlot(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, result_set):
+        self.result_set = result_set
 
     def dataset(self):
-        pass
+        return [
+            s / (self.result_set.turns * (len(self.result_set.ranking) - 1))
+            for s in self.result_set.scores[self.result_set.ranking]]
 
     def xticks(self):
-        pass
+        return range(1, len(self.result_set.ranked_names) + 2), [str(n) for n in self.result_set.ranked_names]
 
     def title(self):
-        pass
+        return 'Mean score per stage game over {} rounds repeated {} times ({} strategies)'.format(
+            self.result_set.turns,
+            self.result_set.repetitions,
+            len(self.result_set.ranking))
