21ba5ba5f0d348ffbcc554fbdf22f5173409beb3
Owen Campbell <owen.campbell@tanti.org.uk>
2015-03-07 15:29:10 +0000

    creates files and stubs for boxplot class and tests

---
+++ axelrod/tests/test_box_plot.py
@@ -0,0 +1,21 @@
+import unittest
+import axelrod
+
+
+class TestBoxPlot(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    def test_init(self):
+        pass
+
+    def test_dataset(self):
+        pass
+
+    def test_xticks(self):
+        pass
+
+    def test_title(self):
+        pass
