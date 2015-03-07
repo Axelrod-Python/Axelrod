2be3c30e633fa32f8a7fd2b9df1a64af524b8d2f
Owen Campbell <owen.campbell@tanti.org.uk>
2015-03-07 16:30:38 +0000

    creates initial tests

--- axelrod/tests/test_box_plot.py
+++ axelrod/tests/test_box_plot.py
@@ -6,16 +6,31 @@ class TestBoxPlot(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        pass
+        players = ('Player1', 'Player2', 'Player3')
+        results = [
+            [[0, 0], [10, 10], [21, 21]],
+            [[10, 8], [0, 0], [16, 20]],
+            [[16, 16], [16, 16], [0, 0]]]
+        cls.test_result_set = axelrod.ResultSet(players, 5, 2)
+        cls.test_result_set.results = results
+        cls.test_result_set.init_output()
+
+        cls.expected_dataset = 'dataset'
+        cls.expected_xticks = 'xticks'
+        cls.expected_title = 'title'
 
     def test_init(self):
-        pass
+        bp = axelrod.BoxPlot(self.test_result_set)
+        self.assertEquals(bp.results_set, self.test_result_set)
 
     def test_dataset(self):
-        pass
+        bp = axelrod.BoxPlot(self.test_result_set)
+        self.assertEquals(bp.dataset, self.expected_dataset)
 
     def test_xticks(self):
-        pass
+        bp = axelrod.BoxPlot(self.test_result_set)
+        self.assertEquals(bp.xticks, self.expected_xticks)
 
     def test_title(self):
-        pass
+        bp = axelrod.BoxPlot(self.test_result_set)
+        self.assertEquals(bp.title, self.expected_title)
