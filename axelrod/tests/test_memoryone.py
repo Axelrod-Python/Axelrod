4259a0a3da8d095d605b7562356f5266d5750c27
vince <vincent.knight@gmail.com>
2015-03-08 11:13:09 +0000

    Adding tests for generous tit for tat

--- axelrod/tests/test_memoryone.py
+++ axelrod/tests/test_memoryone.py
@@ -1,5 +1,7 @@
 """Test for the memoryone strategies."""
 
+import random
+
 import axelrod
 
 from test_player import TestPlayer
@@ -32,3 +34,54 @@ class TestWinStayLostShift(TestPlayer):
         P1.history = ['D']
         P2.history = ['D']
         self.assertEqual(P1.strategy(P2), 'C')
+
+class TestGTFT(TestPlayer):
+
+    name = "Generous Tit-For-Tat"
+    player = axelrod.GTFT
+    stochastic = True
+
+    def test_strategy(self):
+        P1 = self.player()
+        P2 = axelrod.Player()
+        self.assertEqual(P1.strategy(P2), 'C')
+
+        P1.history = ['C']
+        P2.history = ['C']
+        random.seed(2)
+        # With probability .05 will defect
+        self.assertEqual(P1.strategy(P2), 'D')
+        # But otherwise will cooperate
+        self.assertEqual(P1.strategy(P2), 'C')
+        self.assertEqual(P1.strategy(P2), 'C')
+        self.assertEqual(P1.strategy(P2), 'C')
+
+        P1.history = ['C']
+        P2.history = ['D']
+        random.seed(31)
+        # With probability .05 will cooperate
+        self.assertEqual(P1.strategy(P2), 'C')
+        # But otherwise will defect
+        self.assertEqual(P1.strategy(P2), 'D')
+        self.assertEqual(P1.strategy(P2), 'D')
+        self.assertEqual(P1.strategy(P2), 'D')
+
+        P1.history = ['D']
+        P2.history = ['C']
+        random.seed(2)
+        # With probability .05 will defect
+        self.assertEqual(P1.strategy(P2), 'D')
+        # But otherwise will cooperate
+        self.assertEqual(P1.strategy(P2), 'C')
+        self.assertEqual(P1.strategy(P2), 'C')
+        self.assertEqual(P1.strategy(P2), 'C')
+
+        P1.history = ['D']
+        P2.history = ['D']
+        random.seed(31)
+        # With probability .05 will cooperate
+        self.assertEqual(P1.strategy(P2), 'C')
+        # But otherwise will defect
+        self.assertEqual(P1.strategy(P2), 'D')
+        self.assertEqual(P1.strategy(P2), 'D')
+        self.assertEqual(P1.strategy(P2), 'D')
