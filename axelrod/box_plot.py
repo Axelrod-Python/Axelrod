ccb675f9df706ce28d7f81ac3a647001b13f4e37
Owen Campbell <owen.campbell@tanti.org.uk>
2015-03-08 12:53:49 +0000

    fixes PEP errors

--- axelrod/box_plot.py
+++ axelrod/box_plot.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 
+
 class BoxPlot(object):
 
     def __init__(self, result_set):
@@ -7,7 +8,8 @@ class BoxPlot(object):
 
     def dataset(self):
         return [
-            s / (self.result_set.turns * float((len(self.result_set.ranking) - 1)))
+            s / (self.result_set.turns *
+                 float((len(self.result_set.ranking) - 1)))
             for s in self.result_set.scores[self.result_set.ranking]]
 
     def xticks_locations(self):
@@ -17,7 +19,8 @@ class BoxPlot(object):
         return [str(n) for n in self.result_set.ranked_names]
 
     def title(self):
-        return 'Mean score per stage game over {} rounds repeated {} times ({} strategies)'.format(
+        return ("Mean score per stage game over {} "
+                "rounds repeated {} times ({} strategies)").format(
             self.result_set.turns,
             self.result_set.repetitions,
             len(self.result_set.ranking))
