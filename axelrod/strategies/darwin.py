49f40910073167906be7f57ac6fb9569f04237f2
slavinp <slavinp@cs.man.ac.uk>
2015-03-10 13:35:38 +0000

    Added tests for Darwin strategy.

--- axelrod/strategies/darwin.py
+++ axelrod/strategies/darwin.py
@@ -16,44 +16,43 @@ class Darwin(Player):
     def __init__(self):
         super(self.__class__, self).__init__()
         self.trial = 0
-        self.default = 'C'
-        self.splice = self.default
+        self.response = self.__class__.genome[0]
 
 
     def strategy(self, opponent):
-        # Lie to psychics...
+        # Frustrate psychics...
         if inspect.stack()[1][3] not in self.__class__.valid_callers:
             return 'C'
 
         if self.trial > 0:
             outcome = self.__class__.outcomes[(self.history[-1], opponent.history[-1])]
             self.mutate(outcome)
-            self.__class__.genome[self.trial-1] = self.splice
+            self.__class__.genome[self.trial-1] = self.response
 
         if self.trial < len(self.__class__.genome):
             current = self.__class__.genome[self.trial]
         else:
-            self.__class__.genome.append(self.default)
-            current = self.default
+            self.__class__.genome.append(opponent.history[-1])
+            current = opponent.history[-1]
 
         self.trial += 1
         return current
 
 
     def reset(self):
-        self.history = []
-        self.splice = 'C'
-        self.trial = 0
-        self.__class__.genome[0] = 'C'
-        """
+        """ 
         print("C: {0}\tD: {1}".format(  self.__class__.genome.count('C'),
                                         self.__class__.genome.count('D')
                                       ))
         """
+        self.history = []
+        self.response = 'C'
+        self.trial = 0
+        self.__class__.genome[0] = 'C'
 
 
     def mutate(self, outcome):
         """Modify genome if undesirable outcome"""
         if outcome < 0:
-            self.splice = 'D' if self.splice == 'C' else 'C'
+            self.response = 'D' if self.response == 'C' else 'C'
 
---
+++ axelrod/tests/test_darwin.py
@@ -0,0 +1,35 @@
+import sys
+import axelrod
+from test_player import TestPlayer
+
+class TestDarwin(TestPlayer):
+
+    name = "Darwin"
+    player = axelrod.Darwin
+
+    def test_strategy(self):
+        p1 = self.player()
+        p2 = axelrod.Player()
+        self.assertEqual(p1.strategy(p2), 'C') # Always cooperate first.
+
+    def test_play(self):
+        self.play()
+
+    def play(self):
+        """We need this to circumvent the agent's anti-inspection measure"""
+        p1 = self.player()
+        p2 = axelrod.Player()
+        p1.reset()
+        p1.strategy(p2)
+        # Genome contains only valid responses.
+        self.assertEqual(p1.genome.count('C') + p1.genome.count('D'), len(p1.genome))
+
+
+    def test_reset(self):
+        p1 = self.player()
+        p1.reset()
+        self.assertEqual(p1.history, [])
+        self.assertEqual(p1.response, 'C')
+        self.assertEqual(p1.trial, 0)
+        self.assertEqual(p1.genome[0], 'C')
+
