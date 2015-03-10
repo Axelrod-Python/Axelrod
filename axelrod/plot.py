3f3a13a95b456eb1cdf19b0bbdbba6438a4dea24
Owen Campbell <owen.campbell@tanti.org.uk>
2015-03-10 09:01:04 +0000

    renames Plot methods to include boxplot prefix

--- axelrod/plot.py
+++ axelrod/plot.py
@@ -11,32 +11,32 @@ class Plot(object):
         self.result_set = result_set
         self.matplotlib_installed = matplotlib_installed
 
-    def dataset(self):
+    def boxplot_dataset(self):
         return [self.result_set.scores[ir] for ir in self.result_set.ranking]
 
-    def xticks_locations(self):
+    def boxplot_xticks_locations(self):
         return range(1, len(self.result_set.ranked_names) + 2)
 
-    def xticks_labels(self):
+    def boxplot_xticks_labels(self):
         return [str(n) for n in self.result_set.ranked_names]
 
-    def title(self):
+    def boxplot_title(self):
         return ("Mean score per stage game over {} "
                 "rounds repeated {} times ({} strategies)").format(
             self.result_set.turns,
             self.result_set.repetitions,
             len(self.result_set.ranking))
 
-    def figure(self):
+    def boxplot(self):
         if self.matplotlib_installed:
             figure = plt.figure()
-            plt.boxplot(self.dataset())
+            plt.boxplot(self.boxplot_dataset())
             plt.xticks(
-                self.xticks_locations(),
-                self.xticks_labels(),
+                self.boxplot_xticks_locations(),
+                self.boxplot_xticks_labels(),
                 rotation=90)
             plt.tick_params(axis='both', which='both', labelsize=8)
-            plt.title(self.title())
+            plt.title(self.boxplot_title())
             return figure
         else:
             return None
--- axelrod/tests/test_plot.py
+++ axelrod/tests/test_plot.py
@@ -22,35 +22,35 @@ class TestPlot(unittest.TestCase):
         cls.test_result_set.results = results
         cls.test_result_set.init_output()
 
-        cls.expected_dataset = [[2.6, 2.8], [3.1, 3.1], [3.2, 3.2]]
-        cls.expected_xticks_locations = [1, 2, 3, 4]
-        cls.expected_xticks_labels = ['Player2', 'Player1', 'Player3']
-        cls.expected_title = 'Mean score per stage game over 5 rounds repeated 2 times (3 strategies)'
+        cls.expected_boxplot_dataset = [[2.6, 2.8], [3.1, 3.1], [3.2, 3.2]]
+        cls.expected_boxplot_xticks_locations = [1, 2, 3, 4]
+        cls.expected_boxplot_xticks_labels = ['Player2', 'Player1', 'Player3']
+        cls.expected_boxplot_title = 'Mean score per stage game over 5 rounds repeated 2 times (3 strategies)'
 
     def test_init(self):
         bp = axelrod.Plot(self.test_result_set)
         self.assertEquals(bp.result_set, self.test_result_set)
 
-    def test_dataset(self):
+    def test_boxplot_dataset(self):
         bp = axelrod.Plot(self.test_result_set)
-        self.assertTrue(numpy.allclose(bp.dataset(), self.expected_dataset))
+        self.assertTrue(numpy.allclose(bp.boxplot_dataset(), self.expected_boxplot_dataset))
 
-    def test_xticks_locations(self):
+    def test_boxplot_xticks_locations(self):
         bp = axelrod.Plot(self.test_result_set)
-        self.assertEquals(bp.xticks_locations(), self.expected_xticks_locations)
+        self.assertEquals(bp.boxplot_xticks_locations(), self.expected_boxplot_xticks_locations)
 
-    def test_xticks_labels(self):
+    def test_boxplot_xticks_labels(self):
         bp = axelrod.Plot(self.test_result_set)
-        self.assertEquals(bp.xticks_labels(), self.expected_xticks_labels)
+        self.assertEquals(bp.boxplot_xticks_labels(), self.expected_boxplot_xticks_labels)
 
-    def test_title(self):
+    def test_boxplot_title(self):
         bp = axelrod.Plot(self.test_result_set)
-        self.assertEquals(bp.title(), self.expected_title)
+        self.assertEquals(bp.boxplot_title(), self.expected_boxplot_title)
 
-    def test_figure(self):
+    def test_boxplot(self):
         if matplotlib_installed:
             bp = axelrod.Plot(self.test_result_set)
-            self.assertIsInstance(bp.figure(), matplotlib.pyplot.Figure)
+            self.assertIsInstance(bp.boxplot(), matplotlib.pyplot.Figure)
         else:
             self.skipTest('matplotlib not installed')
 
