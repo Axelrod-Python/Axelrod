aab905d4e43cdcaff70b95b6cfc86fa247429d3a
Owen Campbell <owen.campbell@tanti.org.uk>
2015-03-19 09:16:57 +0000

    fixes PEP errors

--- axelrod/tournament.py
+++ axelrod/tournament.py
@@ -5,8 +5,6 @@ from round_robin import *
 
 
 class Tournament(object):
-    """Reproduce Prof. Axelrod's tournament"""
-
     game = Game()
 
     def __init__(self, players, name='axelrod', game=None,
@@ -26,7 +24,7 @@ class Tournament(object):
             turns=turns,
             repetitions=repetitions)
 
-        # The cache doesn't need to be an instance level variable at the moment,
+        # The cache doesn't need to be an instance level variable,
         # it could just be internal to the play method.
         # However, there's no reason why the cache couldn't be passed between
         # tournaments by the tournament manager and so it sits here waiting for
@@ -36,10 +34,11 @@ class Tournament(object):
     def play(self):
         payoffs_list = []
 
-        # We play the first repetition in order to build the deterministic cache.
-        # It's done separately so that only further repetions have any chance of
-        # running in parallel. This allows the cache to be made available to processes
-        # running in parallel without the problems of cross-process communication.
+        # We play the first repetition in order to build the deterministic
+        # cache. It's done separately so that only further repetions have
+        # any chance of running in parallel. This allows the cache to be made
+        # available to processes running in parallel without the problems of
+        # cross-process communication.
         payoffs, cache = self.play_round_robin(self.deterministic_cache)
         payoffs_list.append(payoffs)
         self.deterministic_cache = cache
@@ -106,8 +105,9 @@ class Tournament(object):
         cache = round_robin.deterministic_cache
         return payoffs, cache
 
-    # It's not right that this method directly updates an attribute of the RoundRobin
-    # Class. It should be moved into RoundRobin and simply called from here.
+    # It's not right that this method directly updates an attribute of the
+    # RoundRobin Class. It should be moved into RoundRobin and simply called
+    # from here.
     def update_result_set(self, payoffs_list):
         for index, payoffs in enumerate(payoffs_list):
             for i in range(len(self.players)):
