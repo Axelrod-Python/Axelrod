ffce2174b43f81b8a0ff441b8b751a84e9711a71
vince <vincent.knight@gmail.com>
2015-03-22 19:18:52 +0000

    Have very basic setup.py file written

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(
-    name='Axelrod'
-    version='0.1dev'
-    packages=['axelrod'],
+    name='Axelrod',
+    version='0.1dev',
+    packages=['axelrod', 'axelrod.strategies', 'axelrod.tests'],
     scripts=['run_tournament.py'],
-    license='The MIT License (MIT)'
-    long_description=open(’README.md’).read(),
+    license='The MIT License (MIT)',
+    long_description=open('README.md').read(),
 )
