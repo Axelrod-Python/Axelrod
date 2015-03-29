81e7fed4bfee525bf462feef1c716e4119852e93
vince <vincent.knight@gmail.com>
2015-03-29 12:46:35 +0100

    Fixing version

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='Axelrod',
-    version='0.0.1',
+    version='0.0.2dev',
     author='Vince Knight',
     author_email='vincent.knight@gmail.com',
     packages=['axelrod', 'axelrod.strategies', 'axelrod.tests'],
-    scripts=['run_tournament.py'],
+    scripts=['run_tournament.py', 'README.rst'],
     url='http://axelrod.readthedocs.org/',
     license='The MIT License (MIT)',
     description='Reproduce the Axelrod iterated prisoners dilemma tournament',
