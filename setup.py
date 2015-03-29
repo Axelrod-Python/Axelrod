5f00b3a342380aa1c81befce3185b0ac58b9aaad
vince <vincent.knight@gmail.com>
2015-03-29 16:14:41 +0100

    Tidying things

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Axelrod',
-    version='0.0.2',
+    version='0.0.3',
     author='Vince Knight',
     author_email='vincent.knight@gmail.com',
     packages=['axelrod', 'axelrod.strategies', 'axelrod.tests'],
