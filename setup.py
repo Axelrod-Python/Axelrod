59d688fa10364c469df0be6317948a66f84df7df
vince <vincent.knight@gmail.com>
2015-03-29 16:01:31 +0100

    Removing long description

--- setup.py
+++ setup.py
@@ -6,11 +6,10 @@ setup(
     author='Vince Knight',
     author_email='vincent.knight@gmail.com',
     packages=['axelrod', 'axelrod.strategies', 'axelrod.tests'],
-    scripts=['run_axelrod', 'README.rst'],
+    scripts=['run_axelrod'],
     url='http://axelrod.readthedocs.org/',
     license='The MIT License (MIT)',
     description='Reproduce the Axelrod iterated prisoners dilemma tournament',
-    long_description=open('README.rst').read(),
     install_requires=[
         "matplotlib >= 1.4.2",
     ],
