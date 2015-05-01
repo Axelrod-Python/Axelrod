5f0c6b6f77355f0fe83f1b48e2e434ae2886553c
Owen Campbell <owen.campbell@tanti.org.uk>
2015-05-01 13:24:31 +0100

    renames internal methods in plot class

--- axelrod/plot.py
+++ axelrod/plot.py
@@ -14,24 +14,29 @@ class Plot(object):
         else:
             raise AttributeError(
                 "No payoffs list has been passed to this ResultSet object.")
-        self.nplayers = self.result_set.nplayers
-        self.matplotlib_installed = matplotlib_installed
+        # self._nplayers = self.result_set.nplayers
+        self._matplotlib_installed = matplotlib_installed
 
-    def boxplot_dataset(self):
+    @property
+    def _boxplot_dataset(self):
         return [self.result_set.scores[ir] for ir in self.result_set.ranking]
 
-    def payoff_dataset(self):
+    @property
+    def _payoff_dataset(self):
         return [[self.result_set.payoff_matrix[r1][r2]
                 for r2 in self.result_set.ranking]
                 for r1 in self.result_set.ranking]
 
-    def boxplot_xticks_locations(self):
+    @property
+    def _boxplot_xticks_locations(self):
         return range(1, len(self.result_set.ranked_names) + 2)
 
-    def boxplot_xticks_labels(self):
+    @property
+    def _boxplot_xticks_labels(self):
         return [str(n) for n in self.result_set.ranked_names]
 
-    def boxplot_title(self):
+    @property
+    def _boxplot_title(self):
         return ("Mean score per stage game over {} "
                 "rounds repeated {} times ({} strategies)").format(
             self.result_set.turns,
@@ -40,26 +45,26 @@ class Plot(object):
 
     def boxplot(self):
 
-        if not self.matplotlib_installed:
+        if not self._matplotlib_installed:
             return None
 
         figure = plt.figure()
-        plt.boxplot(self.boxplot_dataset())
+        plt.boxplot(self._boxplot_dataset)
         plt.xticks(
-            self.boxplot_xticks_locations(),
-            self.boxplot_xticks_labels(),
+            self._boxplot_xticks_locations,
+            self._boxplot_xticks_labels,
             rotation=90)
         plt.tick_params(axis='both', which='both', labelsize=8)
-        plt.title(self.boxplot_title())
+        plt.title(self._boxplot_title)
         return figure
 
     def payoff(self):
 
-        if not self.matplotlib_installed:
+        if not self._matplotlib_installed:
             return None
 
         figure, ax = plt.subplots()
-        mat = ax.matshow(self.payoff_dataset())
+        mat = ax.matshow(self._payoff_dataset)
         plt.xticks(range(self.result_set.nplayers))
         plt.yticks(range(self.result_set.nplayers))
         ax.set_xticklabels(self.result_set.ranked_names, rotation=90)
@@ -70,7 +75,7 @@ class Plot(object):
 
     def stackplot(self, populations):
 
-        if not self.matplotlib_installed:
+        if not self._matplotlib_installed:
             return None
 
         figure, ax = plt.subplots()
@@ -87,12 +92,11 @@ class Plot(object):
         plt.xlabel('Turn')
         plt.title("Strategy population dynamics based on average payoffs")
 
-        ax2 = ax.twinx()
         trans = transforms.blended_transform_factory(ax.transAxes, ax.transData)
         ticks = []
         for i, n in enumerate(self.result_set.ranked_names):
             x = -0.02
-            y = (i + 0.5) * 1.0 / self.nplayers
+            y = (i + 0.5) * 1.0 / self.result_set.nplayers
             ax.annotate(n, xy=(x, y), xycoords=trans, clip_on=False, va='center', ha='right', fontsize=8)
             ticks.append(y)
         ax.set_yticks(ticks)
