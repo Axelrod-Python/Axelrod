83808d1b4057ec29917dabed8ce08e70857093ce
Owen Campbell <owen.campbell@tanti.org.uk>
2015-05-04 16:09:01 +0100

    fixes parallel processing bug

--- axelrod/tournament.py
+++ axelrod/tournament.py
@@ -36,7 +36,7 @@ class Tournament(object):
         if self._processes is None:
             self._run_serial_repetitions(payoffs_list)
         else:
-            if len(self._deterministic_cache) == 0 or not self._prebuilt_cache:
+            if len(self.deterministic_cache) == 0 or not self.prebuilt_cache:
                 self._logger.debug('Playing first round robin to build cache')
                 payoffs = self.play_round_robin()
                 payoffs_list.append(payoffs)
