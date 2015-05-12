aad2901561986773211d0596b894cfcfed53e0d7
Owen Campbell <owen.campbell@tanti.org.uk>
2015-05-12 11:11:08 +0100

    refactors test_tournament

--- axelrod/tests/test_tournament.py
+++ axelrod/tests/test_tournament.py
@@ -10,16 +10,31 @@ class TestTournament(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.game = axelrod.Game()
-        cls.players = [axelrod.Defector(), axelrod.Defector(), axelrod.Defector()]
+        cls.players = [
+            axelrod.Cooperator(),
+            axelrod.TitForTat(),
+            axelrod.Defector(),
+            axelrod.Grudger(),
+            axelrod.GoByMajority()]
+        cls.player_names = [str(p) for p in cls.players]
         cls.test_name = 'test'
+
+        cls.expected_outcome = [
+            ('Cooperator', [1800, 1800, 1800, 1800, 1800]),
+            ('Defector', [1612, 1612, 1612, 1612, 1612]),
+            ('Go By Majority', [1999, 1999, 1999, 1999, 1999]),
+            ('Grudger', [1999, 1999, 1999, 1999, 1999]),
+            ('Tit For Tat', [1999, 1999, 1999, 1999, 1999])]
+        cls.expected_outcome.sort()
 
     def test_init(self):
         tournament = axelrod.Tournament(
             name=self.test_name,
             players=self.players,
+            game=self.game,
             processes=4,
             noise=0.2)
-        self.assertEqual([str(s) for s in tournament.players], ['Defector', 'Defector', 'Defector'])
+        self.assertEqual(tournament.players, self.players)
         self.assertEqual(tournament.game.score(('C', 'C')), (3, 3))
         self.assertEqual(tournament.turns, 200)
         self.assertEqual(tournament.repetitions, 10)
@@ -38,6 +53,7 @@ class TestTournament(unittest.TestCase):
         tournament = axelrod.Tournament(
             name=self.test_name,
             players=self.players,
+            game=self.game,
             processes=4,
             noise=0.2,
             prebuilt_cache=False)
@@ -56,6 +72,7 @@ class TestTournament(unittest.TestCase):
         tournament = axelrod.Tournament(
             name=self.test_name,
             players=self.players,
+            game=self.game,
             processes=4,
             prebuilt_cache=True)
         tournament.deterministic_cache = {'test': 100}
@@ -65,6 +82,7 @@ class TestTournament(unittest.TestCase):
         tournament = axelrod.Tournament(
             name=self.test_name,
             players=self.players,
+            game=self.game,
             processes=4,
             prebuilt_cache=False)
         tournament.deterministic_cache = {'test': 100}
@@ -74,6 +92,7 @@ class TestTournament(unittest.TestCase):
         tournament = axelrod.Tournament(
             name=self.test_name,
             players=self.players,
+            game=self.game,
             processes=4,
             prebuilt_cache=True)
         self.assertTrue(tournament.build_cache_required)
@@ -82,6 +101,7 @@ class TestTournament(unittest.TestCase):
         tournament = axelrod.Tournament(
             name=self.test_name,
             players=self.players,
+            game=self.game,
             processes=4,
             prebuilt_cache=False)
         self.assertTrue(tournament.build_cache_required)
@@ -92,28 +112,17 @@ class TestTournament(unittest.TestCase):
     def test_parallel_play(self):
         pass
 
-
     def test_tournament(self):
-        """Test tournament."""
-
-        outcome = [('Cooperator', [1800, 1800, 1800, 1800, 1800]),
-                   ('Defector', [1612, 1612, 1612, 1612, 1612]),
-                   ('Go By Majority', [1999, 1999, 1999, 1999, 1999]),
-                   ('Grudger', [1999, 1999, 1999, 1999, 1999]),
-                   ('Tit For Tat', [1999, 1999, 1999, 1999, 1999])]
-
-        outcome.sort()
-
-        P1 = axelrod.Cooperator()
-        P2 = axelrod.TitForTat()
-        P3 = axelrod.Defector()
-        P4 = axelrod.Grudger()
-        P5 = axelrod.GoByMajority()
-        tournament = axelrod.Tournament(name='test', players=[P1, P2, P3, P4, P5], game=self.game, turns=200, repetitions=5)
-        names = [str(p) for p in tournament.players]
-        results = tournament.play().results
-        scores = [[sum([r[i] for ir,r in enumerate(res) if ir != ires]) for i in range(5)] for ires,res in enumerate(results)]
-        self.assertEqual(sorted(zip(names, scores)), outcome)
+        tournament = axelrod.Tournament(
+            name=self.test_name,
+            players=self.players,
+            game=self.game,
+            turns=200,
+            repetitions=5)
+        scores = tournament.play().scores
+        # scores = [[sum([r[i] for ir,sorted(zip(self.player_names, scores))r in enumerate(res) if ir != ires]) for i in range(5)] for ires,res in enumerate(results)]
+        actual_outcome = sorted(zip(self.player_names, scores))
+        self.assertEqual(actual_outcome, self.expected_outcome)
 
 
 if __name__ == '__main__':
