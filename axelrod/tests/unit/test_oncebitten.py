d0fbd90ca85a00d8f68a2fad6a4634838d6ce75a
Thomas Campbell <thomas.campbell@tanti.org.uk>
2015-07-19 16:19:53 +0100

    Creates testing for BackStabber

--- axelrod/tests/unit/test_oncebitten.py
+++ axelrod/tests/unit/test_oncebitten.py
@@ -144,3 +144,41 @@ class TestForgetfulFoolMeOnce(TestPlayer):
         self.assertEqual(P1.D_count, 0)
         self.assertEqual(P1.history, [])
 
+
+class TestBackStabber(TestPlayer):
+
+    name = "BackStabber"
+    player = axelrod.BackStabber
+    stochastic = False
+
+    def test_initial(self):
+        P1 = axelrod.BackStabber()
+        P2 = axelrod.Player()
+        self.assertEqual(P1.strategy(P2), P1._initial)
+
+    def test_strategy(self):
+        """
+        If opponent defects at any point then the player will defect forever
+        """
+        P1 = axelrod.BackStabber()
+        P2 = axelrod.Defector()
+        P1.history = ['C']
+        P2.history = ['D']
+        self.assertEqual(P1.strategy(P2), 'C')
+        P1.history = ['C']
+        P2.history = ['D']
+        self.assertEqual(P1.strategy(P2), 'D')
+        P2.history = ['C']*197
+        self.assertEqual(P1.strategy(P2), 'D')
+
+    def test_reset(self):
+        """Check that count gets reset properly"""
+        P1 = self.player()
+        P1.history = ['C', 'D']
+        P2 = axelrod.Player()
+        P2.history = ['D']
+        self.assertEqual(P1.strategy(P2), 'C')
+        self.assertEqual(P1.D_count, 1)
+        P1.reset()
+        self.assertEqual(P1.D_count, 0)
+        self.assertEqual(P1.history, [])
