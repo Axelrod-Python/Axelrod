476017fe00b40fcdf1d00e7d8d872f79e464ce33
Marc Harper <marc.harper@gmail.com>
2015-07-20 12:31:09 -0700

    Set memory_depth = 0 for ALLC ALLD and Random

--- axelrod/strategies/rand.py
+++ axelrod/strategies/rand.py
@@ -6,7 +6,7 @@ class Random(Player):
     """A player who randomly chooses between cooperating and defecting."""
 
     name = 'Random'
-    memory_depth = 1  # Four-Vector = (0.5, 0.5, 0.5, 0.5)
+    memory_depth = 0  # Memory-one Four-Vector = (0.5, 0.5, 0.5, 0.5)
 
     def strategy(self, opponent):
         return random.choice(['C', 'D'])
