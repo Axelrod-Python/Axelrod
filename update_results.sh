e632db554cd17bd8204b38c8c422018b399205c9
Vince Knight <vincent.knight@gmail.com>
2015-07-25 15:56:18 +0100

    Updating the results script.
    No files are written to root of directory.

--- update_results.sh
+++ update_results.sh
@@ -2,10 +2,10 @@
 # Update the noisy tournaments
 echo "-- Regenerating noisy tournament figures"
 # Run the noisy tournament
-./run_axelrod --noise 0.05 --xc -p 0
+./run_axelrod --noise 0.05 --xc -p 0 -o assets
 # Copy these figures over, renaming as appropriate
-cp strategies_payoff.png assets/strategies_payoff_noise_5.png
-cp strategies_boxplot.png assets/strategies_boxplot_noise_5.png
+mv assets/strategies_payoff.png assets/strategies_payoff_noise_5.png
+mv assets/strategies_boxplot.png assets/strategies_boxplot_noise_5.png
 
 # Running noiseless tournament
 echo "-- Running Axelrod, regenerating cache, results, and figures"
