55f602383d9a9d2ca3c336be1a179b393898d39c
Marc Harper <marc.harper@gmail.com>
2015-07-27 12:49:59 -0700

    Move TESTER to axelrod_tournaments.py

--- axelrod/strategies/axelrod_tournaments.py
+++ axelrod/strategies/axelrod_tournaments.py
@@ -6,32 +6,6 @@ from axelrod import Player
 
 import random
 
-
-class Eatherley(Player):
-    """
-    Strategy submitted to Axelrod's second tournament by Graham Eatherley.
-    """
-
-    name = "Eatherley"
-    memory_depth = float('inf')
-
-    def strategy(self, opponent):
-        # Cooperate on the first move
-        if not len(self.history):
-            return 'C'
-        # Reciprocate cooperation
-        if opponent.history[-1] == 'C':
-            return 'C'
-        # Respond to defections with probability equal to opponent's total
-        # proportion of defections
-        opponent_defections = len([x for x in opponent.history if x == 'D'])
-        defection_prop = float(opponent_defections) / len(opponent.history)
-        r = random.random()
-        if r < defection_prop:
-            return 'D'
-        return 'C'
-
-
 class Champion(Player):
     """
     Strategy submitted to Axelrod's second tournament by Danny Champion.
@@ -61,3 +35,61 @@ class Champion(Player):
             if defection_prop > max(0.4, r):
                 return 'D'
         return 'C'
+
+
+class Eatherley(Player):
+    """
+    Strategy submitted to Axelrod's second tournament by Graham Eatherley.
+    """
+
+    name = "Eatherley"
+    memory_depth = float('inf')
+
+    def strategy(self, opponent):
+        # Cooperate on the first move
+        if not len(self.history):
+            return 'C'
+        # Reciprocate cooperation
+        if opponent.history[-1] == 'C':
+            return 'C'
+        # Respond to defections with probability equal to opponent's total
+        # proportion of defections
+        opponent_defections = len([x for x in opponent.history if x == 'D'])
+        defection_prop = float(opponent_defections) / len(opponent.history)
+        r = random.random()
+        if r < defection_prop:
+            return 'D'
+        return 'C'
+
+
+class Tester(Player):
+    """
+    Submitted to Axelrod's second tournament by David Gladstein.
+
+    Defects on the first move and plays TFT if the opponent ever defects (after
+    one apology cooperation round). Otherwise alternate cooperation and defection.
+    """
+
+    name = "Tester"
+    memory_depth = float('inf')
+
+    def __init__(self):
+        Player.__init__(self)
+        self.is_TFT = False
+
+    def strategy(self, opponent):
+        # Defect on the first move
+        if not opponent.history:
+            return 'D'
+        # Am I TFT?
+        if self.is_TFT:
+            return 'D' if opponent.history[-1:] == ['D'] else 'C'
+        else:
+            # Did opponent defect?
+            if opponent.history[-1] == 'D':
+                self.is_TFT = True
+                return 'C'
+            if len(self.history) in [1, 2]:
+                return 'C'
+            # Alternate C and D
+            return flip_dict[self.history[-1]]
