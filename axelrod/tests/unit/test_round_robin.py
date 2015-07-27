fbb4010b827c558770150f0d9f403ac2496410e2
Owen Campbell <owen.campbell@tanti.org.uk>
2015-07-27 17:57:26 +0100

    adds tests for stochastic interaction

--- axelrod/tests/unit/test_round_robin.py
+++ axelrod/tests/unit/test_round_robin.py
@@ -18,8 +18,17 @@ class TestRoundRobin(unittest.TestCase):
         self.assertEquals(rr.game.score(('C', 'C')), (3, 3))
         self.assertEqual(rr.turns, 20)
         self.assertEqual(rr.deterministic_cache, {})
-        self.assertEqual(rr.cache_mutable, True)
+        self.assertTrue(rr.cache_mutable)
         self.assertEqual(rr._noise, 0.2)
+
+    def test_stochastic_interaction(self):
+        p1, p2 = axelrod.Player(), axelrod.Player()
+        rr = axelrod.RoundRobin(
+            players=[p1, p2], game=self.game, turns=20, noise=0.2)
+        self.assertTrue(rr._stochastic_interaction(p1, p2))
+        rr = axelrod.RoundRobin(
+            players=[p1, p2], game=self.game, turns=20)
+        self.assertFalse(rr._stochastic_interaction(p1, p2))
 
     def test_deterministic_cache(self):
         p1, p2, p3 = axelrod.Cooperator(), axelrod.Defector(), axelrod.Random()
