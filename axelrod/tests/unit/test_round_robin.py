1bad8647e4cc500134fb635b46195cbef935ea0c
Owen Campbell <owen.campbell@tanti.org.uk>
2015-07-27 20:23:28 +0100

    refactors play method to use _pair_of_players

--- axelrod/tests/unit/test_round_robin.py
+++ axelrod/tests/unit/test_round_robin.py
@@ -84,6 +84,10 @@ class TestRoundRobin(unittest.TestCase):
         self.assertEqual(pair['instances'][1].name, 'Tit For Tat')
         self.assertEqual(pair['classes'][0], axelrod.Cooperator)
         self.assertEqual(pair['classes'][1], axelrod.TitForTat)
+        pair = rr._pair_of_players(0, 0)
+        self.assertEqual(pair['instances'][0].name, pair['instances'][1].name)
+        self.assertEqual(pair['classes'][0], pair['classes'][1])
+        self.assertNotEqual(pair['instances'][0], pair['instances'][1])
 
     def test_deterministic_cache(self):
         p1, p2, p3 = axelrod.Cooperator(), axelrod.Defector(), axelrod.Random()
