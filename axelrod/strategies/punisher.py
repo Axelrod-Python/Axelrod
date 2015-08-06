b90fad5dc927584d921d69cc10677f7197d59ba8
Marc Harper <marc.harper@gmail.com>
2015-08-05 21:28:07 -0700

    Update punisher and inverse (and tests)

--- axelrod/strategies/punisher.py
+++ axelrod/strategies/punisher.py
@@ -3,9 +3,10 @@ from axelrod import Player
 
 class Punisher(Player):
     """
-    A player starts by cooperating however will defect if at any point the opponent has defected,
-    but forgets after meme_length matches, with 1<=mem_length<=20 proportional to the amount
-    of time the opponent has played 'D', punishing that player for playing 'D' too often
+    A player starts by cooperating however will defect if at any point the
+    opponent has defected, but forgets after meme_length matches, with
+    1<=mem_length<=20 proportional to the amount of time the opponent has
+    played 'D', punishing that player for playing 'D' too often.
     """
 
     name = 'Punisher'
@@ -35,7 +36,7 @@ class Punisher(Player):
             self.grudge_memory += 1
             return 'D'
         elif 'D' in opponent.history[-1:]:
-            self.mem_length = (sum([i == 'D' for i in opponent.history])*20) / len(opponent.history)
+            self.mem_length = (opponent.defections * 20) / len(opponent.history)
             self.grudged = True
             return 'D'
         return 'C'
@@ -84,7 +85,7 @@ class InversePunisher(Player):
             self.grudge_memory += 1
             return 'D'
         elif 'D' in opponent.history[-1:]:
-            self.mem_length = ((sum([i == 'C' for i in opponent.history]))*20) / len(opponent.history)
+            self.mem_length = (opponent.cooperations * 20) / len(opponent.history)
             if self.mem_length == 0:
                 self.mem_length += 1
             self.grudged = True
