d3bff085a57a4030807572fe475fc1fccabc3d67
Marc Harper <marc.harper@gmail.com>
2015-08-10 14:14:25 -0700

    Deprecated assertEquals to assertEqual

--- axelrod/tests/unit/test_game.py
+++ axelrod/tests/unit/test_game.py
@@ -14,14 +14,14 @@ class TestGame(unittest.TestCase):
     def test_init(self):
         expected_scores = {(C, D): (0, 5), (D, C): (5, 0),
                            (D, D): (1, 1), (C, C): (3, 3)}
-        self.assertEquals(self.game.scores, expected_scores)
+        self.assertEqual(self.game.scores, expected_scores)
 
     def test_RPST(self):
         expected_values = (3, 1, 0, 5)
-        self.assertEquals(self.game.RPST(), expected_values)
+        self.assertEqual(self.game.RPST(), expected_values)
 
     def test_score(self):
-        self.assertEquals(self.game.score((C, C)), (3, 3))
-        self.assertEquals(self.game.score((D, D)), (1, 1))
-        self.assertEquals(self.game.score((C, D)), (0, 5))
-        self.assertEquals(self.game.score((D, C)), (5, 0))
+        self.assertEqual(self.game.score((C, C)), (3, 3))
+        self.assertEqual(self.game.score((D, D)), (1, 1))
+        self.assertEqual(self.game.score((C, D)), (0, 5))
+        self.assertEqual(self.game.score((D, C)), (5, 0))
