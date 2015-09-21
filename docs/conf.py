818432e9b5657440d30853d89082bbcf0cb0d6a0
Vince Knight <vincent.knight@gmail.com>
2015-09-21 15:15:12 +0100

    Revert "Trying with sphinx set to build with py3"
    This reverts commit 347a030f9a84d7e7cf01f50a38c08917588f2b41.

--- docs/conf.py
+++ docs/conf.py
@@ -15,13 +15,13 @@
 import sys
 import os
 
-import mock
+#import mock
 
-MOCK_MODULES = ['numpy', 'matplotlib.pyplot', 'matplotlib',
-                'matplotlib.transforms', 'mpl_toolkits.axes_grid1', 'scipy',
-                'scipy.interpolate']
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
+#MOCK_MODULES = ['numpy', 'matplotlib.pyplot', 'matplotlib',
+                #'matplotlib.transforms', 'mpl_toolkits.axes_grid1', 'scipy',
+                #'scipy.interpolate']
+#for mod_name in MOCK_MODULES:
+    #sys.modules[mod_name] = mock.Mock()
 
 # Adds absolute path to axelrod module
 sys.path.insert(0, os.path.abspath('../'))  # Adding path to module
