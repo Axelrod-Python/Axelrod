6ed5d6455a60d8483aec8edff18108b20d2164b4
Vince Knight <vincent.knight@gmail.com>
2015-09-21 15:15:14 +0100

    Revert "Adding scipy also for readthedocs"
    This reverts commit 3b8a1359c823742a6ece86c2196d2d083ffe77ba.

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,7 @@ import os
 
 import mock
 
-MOCK_MODULES = ['numpy', 'matplotlib.pyplot', 'matplotlib',
-                'matplotlib.transforms', 'mpl_toolkits.axes_grid1', 'scipy',
-                'scipy.interpolate']
+MOCK_MODULES = ['numpy', 'matplotlib.pyplot', 'matplotlib', 'matplotlib.transforms', 'mpl_toolkits.axes_grid1']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
