6e4cc6a0376dbe409cb4c45fab00ea36308db713
Vince Knight <vincent.knight@gmail.com>
2015-09-21 15:37:01 +0100

    Removing mock

--- docs/conf.py
+++ docs/conf.py
@@ -15,11 +15,11 @@
 import sys
 import os
 
-import mock
+#import mock
 
-MOCK_MODULES = ['numpy', 'matplotlib.pyplot', 'matplotlib', 'matplotlib.transforms', 'mpl_toolkits.axes_grid1']
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
+#MOCK_MODULES = ['numpy', 'matplotlib.pyplot', 'matplotlib', 'matplotlib.transforms', 'mpl_toolkits.axes_grid1']
+#for mod_name in MOCK_MODULES:
+    #sys.modules[mod_name] = mock.Mock()
 
 # Adds absolute path to axelrod module
 #sys.path.insert(0, os.path.abspath('../'))  # Adding path to module
