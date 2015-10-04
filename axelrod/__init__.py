342d19b3f381412ecae07b730d303e1abdb4aa5b
Owen Campbell <owen.campbell@tanti.org.uk>
2015-10-04 12:27:36 +0100

    adds ranked_names function and test

--- axelrod/__init__.py
+++ axelrod/__init__.py
@@ -11,6 +11,6 @@ from .strategies import *
 from .tournament import Tournament
 from .tournament_manager import TournamentManager
 from .tournament_manager_factory import TournamentManagerFactory
-from .payoff import scores, normalised_scores, median, ranking
+from .payoff import scores, normalised_scores, median, ranking, ranked_names
 from .result_set import ResultSet
 from .ecosystem import Ecosystem
