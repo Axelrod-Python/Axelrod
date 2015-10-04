edd9ab37c36cfe1c9f8d14e9997a6e96c5332a89
Owen Campbell <owen.campbell@tanti.org.uk>
2015-10-04 19:13:03 +0100

    uses alias for axelrod.payoff import

--- axelrod/tests/unit/test_payoff.py
+++ axelrod/tests/unit/test_payoff.py
@@ -1,5 +1,5 @@
 import unittest
-import axelrod.payoff
+import axelrod.payoff as ap
 
 
 class TestPayoff(unittest.TestCase):
@@ -44,27 +44,27 @@ class TestPayoff(unittest.TestCase):
         cls.expected_wins = [[2, 0], [0, 0], [0, 2]]
 
     def test_scores(self):
-        scores = axelrod.payoff.scores(self.expected_payoff, 3, 2)
+        scores = ap.scores(self.expected_payoff, 3, 2)
         self.assertEqual(scores, self.expected_scores)
 
     def test_normalised_scores(self):
-        scores = axelrod.payoff.normalised_scores(self.expected_scores, 3, 5)
+        scores = ap.normalised_scores(self.expected_scores, 3, 5)
         self.assertEqual(scores, self.expected_normalised_scores)
 
     def test_median(self):
-        median = axelrod.payoff.median([])
+        median = ap.median([])
         self.assertEqual(median, None)
-        median = axelrod.payoff.median([1, 2, 4, 6])
+        median = ap.median([1, 2, 4, 6])
         self.assertEqual(median, 3.0)
-        median = axelrod.payoff.median(self.expected_scores)
+        median = ap.median(self.expected_scores)
         self.assertEqual(median, [27, 20])
 
     def test_ranking(self):
-        ranking = axelrod.payoff.ranking(self.expected_scores, 3)
+        ranking = ap.ranking(self.expected_scores, 3)
         self.assertEqual(ranking, self.expected_ranking)
 
     def test_ranked_names(self):
-        ranked_names = axelrod.payoff.ranked_names(
+        ranked_names = ap.ranked_names(
             self.players, self.expected_ranking,)
         self.assertEqual(ranked_names, self.expected_ranked_names)
 
@@ -73,7 +73,7 @@ class TestPayoff(unittest.TestCase):
         return [[round(x, precision) for x in row] for row in matrix]
 
     def test_payoff_matrix(self):
-        averages, stddevs = axelrod.payoff.payoff_matrix(self.expected_payoff, 5, 2)
+        averages, stddevs = ap.payoff_matrix(self.expected_payoff, 5, 2)
         self.assertEqual(
             self.round_matrix(averages, 2), self.expected_payoff_matrix)
         self.assertEqual(
@@ -82,15 +82,15 @@ class TestPayoff(unittest.TestCase):
     def test_winning_player(self):
         test_players = (8, 4)
         test_payoffs = (34, 44)
-        winner = axelrod.payoff.winning_player(test_players, test_payoffs)
+        winner = ap.winning_player(test_players, test_payoffs)
         self.assertEqual(winner, 4)
         test_payoffs = (54, 44)
-        winner = axelrod.payoff.winning_player(test_players, test_payoffs)
+        winner = ap.winning_player(test_players, test_payoffs)
         self.assertEqual(winner, 8)
         test_payoffs = (34, 34)
-        winner = axelrod.payoff.winning_player(test_players, test_payoffs)
+        winner = ap.winning_player(test_players, test_payoffs)
         self.assertEqual(winner, None)
 
     def test_wins(self):
-        wins = axelrod.payoff.wins(self.expected_payoff, 3, 2)
+        wins = ap.wins(self.expected_payoff, 3, 2)
         self.assertEqual(wins, self.expected_wins)
