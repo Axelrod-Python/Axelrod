5748ed8f3a5568aa85e62a9a8ca84ac42972c0c9
Marc Harper <marc.harper@gmail.com>
2015-10-04 09:53:11 -0700

    New Strategy: CycleHunter

--- axelrod/tests/unit/test_hunter.py
+++ axelrod/tests/unit/test_hunter.py
@@ -27,6 +27,7 @@ class TestDefectorHunter(TestPlayer):
             self.responses_test([C] * i, [D] * i, [C])
         self.responses_test([C] * 4, [D] * 4, [D])
 
+
 class TestCooperatorHunter(TestPlayer):
 
     name = "Cooperator Hunter"
@@ -44,6 +45,7 @@ class TestCooperatorHunter(TestPlayer):
         for i in range(3):
             self.responses_test([C] * i, [C] * i, [C])
         self.responses_test([C] * 4, [C] * 4, [D])
+
 
 class TestAlternatorHunter(TestPlayer):
 
@@ -65,6 +67,35 @@ class TestAlternatorHunter(TestPlayer):
         self.responses_test([C] * 5, [C, D] * 2 + [C], [C])
         self.responses_test([C] * 6, [C, D] * 3, [D])
         self.responses_test([C] * 7, [C, D] * 3 + [C], [D])
+
+
+class TestCycleHunter(TestPlayer):
+
+    name = "Cycle Hunter"
+    player = axelrod.CycleHunter
+    expected_classifier = {
+        'memory_depth': float('inf'),  # Long memory
+        'stochastic' : False,
+        'inspects_source': False,
+        'manipulates_source': False,
+        'manipulates_state': False
+    }
+
+    def test_strategy(self):
+        self.first_play_test(C)
+        player = self.player()
+        # Test against cyclers
+        for opponent in [axelrod.CyclerCCD(), axelrod.CyclerCCCD(), axelrod.CyclerCCCCCD(), axelrod.Cooperator(), axelrod.Defector(), axelrod.Alternator()]:
+            player.reset()
+            for i in range(30):
+                player.play(opponent)
+            self.assertEqual(player.history[-1], 'D')
+        # Test against non-cyclers
+        for opponent in [axelrod.Random(), axelrod.AntiCycler()]:
+            player.reset()
+            for i in range(30):
+                player.play(opponent)
+            self.assertEqual(player.history[-1], 'C')
 
 
 class TestMathConstantHunter(TestPlayer):
@@ -107,3 +138,31 @@ class TestRandomHunter(TestPlayer):
         P1.history = [C] * 100
         P2.history = [random.choice([C, D]) for i in range(100)]
         self.assertEqual(P1.strategy(P2), D)
+
+
+class TestMetaHunter(TestPlayer):
+
+    name = "Meta Hunter"
+    player = axelrod.MetaHunter
+    expected_classifier = {
+        'memory_depth': float('inf'),  # Long memory
+        'stochastic' : False,
+        'inspects_source': False,
+        'manipulates_source': False,
+        'manipulates_state': False
+    }
+
+    def test_strategy(self):
+        self.first_play_test(C)
+
+        # We are not using the Cooperator Hunter here, so this should lead to cooperation.
+        self.responses_test([C, C, C, C], [C, C, C, C], [C])
+
+        # After long histories tit-for-tat should come into play.
+        self.responses_test([C] * 101, [C] * 100 + [D], [D])
+
+        # All these others, however, should trigger a defection for the hunter.
+        self.responses_test([C] * 4, [D] * 4, [D])
+        self.responses_test([C] * 6, [C, D] * 3, [D])
+        self.responses_test([C] * 8, [C, C, C, D, C, C, C, D], [D])
+        self.responses_test([C] * 100, [random.choice([C, D]) for i in range(100)],[D])
