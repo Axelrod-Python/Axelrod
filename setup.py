6d2458336e80580ce6de7b77baa35ea8ab7c5d4e
Vince Knight <vincent.knight@gmail.com>
2015-10-18 17:57:00 +0100

    Merge conflict.

--- setup.py
+++ setup.py
@@ -3,11 +3,7 @@ from setuptools import setup
 
 setup(
     name='Axelrod',
-<<<<<<< HEAD
-    version='0.0.13',
-=======
     version='0.0.15',
->>>>>>> master
     author='Vince Knight, Owen Campbell, Karol Langner, Marc Harper',
     author_email=('axelrod-python@googlegroups.com'),
     packages=['axelrod', 'axelrod.strategies', 'axelrod.tests'],
