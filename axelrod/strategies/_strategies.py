c47a86802d5620927b4ff6f4a4f49ed0d4a4b0f9
Martin <martin@pythonforbiologists.com>
2015-10-27 16:25:28 +0000

    added lookerup strategy

--- axelrod/strategies/_strategies.py
+++ axelrod/strategies/_strategies.py
@@ -22,6 +22,7 @@ from .hunter import (
     DefectorHunter, CooperatorHunter, CycleHunter, AlternatorHunter,
     MathConstantHunter, RandomHunter, EventualCycleHunter)
 from .inverse import Inverse
+from .lookerup import EvolvedLookerUp
 from .mathematicalconstants import Golden, Pi, e
 from .memoryone import (
     WinStayLoseShift,GTFT, StochasticCooperator, StochasticWSLS, ZDGTFT2,
@@ -100,6 +101,7 @@ strategies = [
     LimitedRetaliate,
     LimitedRetaliate2,
     LimitedRetaliate3,
+    EvolvedLookerUp,
     MathConstantHunter,
     MindBender,
     MindController,
