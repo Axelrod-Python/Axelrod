f27419f863badd974f54834598389375030b30a8
Vince Knight <vincent.knight@gmail.com>
2015-10-30 10:00:08 +0000

    New release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='Axelrod',
-    version='0.0.16',
+    version='0.0.17',
     author='Vince Knight, Owen Campbell, Karol Langner, Marc Harper',
     author_email=('axelrod-python@googlegroups.com'),
     packages=['axelrod', 'axelrod.strategies', 'axelrod.tests'],
