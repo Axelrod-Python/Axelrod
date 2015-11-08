0191d88957d66eb51683bb5645887900025225cf
Owen Campbell <owen.campbell@tanti.org.uk>
2015-11-08 14:28:44 +0000

    refactors results and play method

--- axelrod/tests/unit/test_match.py
+++ axelrod/tests/unit/test_match.py
@@ -12,8 +12,15 @@ class TestMatch(unittest.TestCase):
     def test_init(self):
         p1, p2 = axelrod.Cooperator(), axelrod.Cooperator()
         match = axelrod.Match((p1, p2), 5)
+        self.assertEqual(match.result, [])
         self.assertEqual(match._player1, p1)
-        self.assertEqual(match._classes, (axelrod.Cooperator, axelrod.Cooperator))
+        self.assertEqual(match._player2, p2)
+        self.assertEqual(
+            match._classes, (axelrod.Cooperator, axelrod.Cooperator))
+        self.assertEqual(match._turns, 5)
+        self.assertEqual(match._cache, {})
+        self.assertEqual(match._cache_mutable, True)
+        self.assertEqual(match._noise, 0)
 
     def test_stochastic(self):
         p1, p2 = axelrod.Cooperator(), axelrod.Cooperator()
@@ -42,29 +49,24 @@ class TestMatch(unittest.TestCase):
         match = axelrod.Match((p1, p2), 5)
         self.assertFalse(match._cache_update_required)
 
-    def test_result(self):
-        players = (axelrod.Cooperator(), axelrod.Defector())
-        match = axelrod.Match(players, 3)
-        expected_result = [(C, D), (C, D), (C, D)]
-        self.assertEqual(match.result, expected_result)
-
-        # a deliberately incorrect result so we can tell it came from the cache
-        expected_result = [(C, C), (D, D), (D, C)]
-        cache = {(axelrod.Cooperator, axelrod.Defector): expected_result}
-        match = axelrod.Match(players, 3, cache)
-        self.assertEqual(match.result, expected_result)
-
     def test_play(self):
         cache = {}
         players = (axelrod.Cooperator(), axelrod.Defector())
         match = axelrod.Match(players, 3, cache)
         expected_result = [(C, D), (C, D), (C, D)]
-        self.assertEqual(match._play(), expected_result)
+        self.assertEqual(match.play(), expected_result)
         self.assertEqual(
             cache[(axelrod.Cooperator, axelrod.Defector)], expected_result)
+
+        # a deliberately incorrect result so we can tell it came from the cache
+        expected_result = [(C, C), (D, D), (D, C)]
+        cache = {(axelrod.Cooperator, axelrod.Defector): expected_result}
+        match = axelrod.Match(players, 3, cache)
+        self.assertEqual(match.play(), expected_result)
 
     def test_sparklines(self):
         players = (axelrod.Cooperator(), axelrod.Alternator())
         match = axelrod.Match(players, 4)
+        match.play()
         expected_sparklines = u'████\n█ █ '
         self.assertEqual(match.sparklines, expected_sparklines)
