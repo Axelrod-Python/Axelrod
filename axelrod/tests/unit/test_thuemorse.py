72be2c4dfa003127c20d30278939b8a7a4b1c8cb
Marc Harper <marc.harper@gmail.com>
2016-02-19 13:08:01 -0800

    SequencePlayer class and ThueMorse refactor

--- axelrod/tests/unit/test_thuemorse.py
+++ axelrod/tests/unit/test_thuemorse.py
@@ -1,9 +1,18 @@
 """Test for the Thue-Morse strategies."""
+import unittest
 
 import axelrod
 from .test_player import TestPlayer
+from axelrod.strategies.thuemorse import recursive_thue_morse
+
 
 C, D = axelrod.Actions.C, axelrod.Actions.D
+
+class TestThueMoreGenerator(unittest.TestCase):
+    def test_sequence(self):
+        expected = [0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0]
+        for i, e in enumerate(expected):
+            self.assertEqual(recursive_thue_morse(i), e)
 
 
 class TestThueMorse(TestPlayer):
@@ -22,16 +31,17 @@ class TestThueMorse(TestPlayer):
     def test_strategy(self):
         """Test that strategy is randomly picked (not affected by history)."""
         self.first_play_test(D)
-        self.first_play_test(D)
 
     def test_effect_of_strategy(self):
-        self.markov_test(['C', 'C', 'C', 'C'])
-        self.responses_test([], [], [D, C, C, D, C, D, D, C, C, D, D, C, D, C, C, D])
-        self.responses_test([C], [C], [C, C, D, C, D, D, C, C, D, D, C, D, C, C, D])
-        self.responses_test([D], [D], [C, C, D, C, D, D, C, C, D, D, C, D, C, C, D])
-        self.responses_test([C, C, C, D], [C, C, C, D], [C, D, D, C, C, D, D, C, D, C, C, D])
-
-
+        self.markov_test([C, C, C, C])
+        self.responses_test([], [], [D, C, C, D, C, D, D, C, C, D, D, C, D, C,
+                                     C, D])
+        self.responses_test([C], [C], [C, C, D, C, D, D, C, C, D, D, C, D, C, C,
+                                       D])
+        self.responses_test([D], [D], [C, C, D, C, D, D, C, C, D, D, C, D, C, C,
+                                       D])
+        self.responses_test([C, C, C, D], [C, C, C, D], [C, D, D, C, C, D, D, C,
+                                                         D, C, C, D])
 
 
 class TestThueMorseInverse(TestPlayer):
@@ -53,8 +63,12 @@ class TestThueMorseInverse(TestPlayer):
         self.first_play_test(C)
 
     def test_effect_of_strategy(self):
-        self.markov_test(['D', 'D', 'D', 'D'])
-        self.responses_test([], [], [C, D, D, C, D, C, C, D, D, C, C, D, C, D, D, C])
-        self.responses_test([C], [C], [D, D, C, D, C, C, D, D, C, C, D, C, D, D, C])
-        self.responses_test([D], [D], [D, D, C, D, C, C, D, D, C, C, D, C, D, D, C])
-        self.responses_test([C, C, C, D], [C, C, C, D], [D, C, C, D, D, C, C, D, C, D, D, C])
+        self.markov_test([D, D, D, D])
+        self.responses_test([], [], [C, D, D, C, D, C, C, D, D, C, C, D, C, D,
+                                     D, C])
+        self.responses_test([C], [C], [D, D, C, D, C, C, D, D, C, C, D, C, D, D,
+                                       C])
+        self.responses_test([D], [D], [D, D, C, D, C, C, D, D, C, C, D, C, D, D,
+                                       C])
+        self.responses_test([C, C, C, D], [C, C, C, D], [D, C, C, D, D, C, C, D,
+                                                         C, D, D, C])
