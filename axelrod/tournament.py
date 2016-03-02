8def5a870e832839c5265a1267a3f095a26cab7e
Owen Campbell <owen.campbell@empiria.co.uk>
2016-03-02 18:26:32 +0000

    Add docstrings to Tournament class methods

--- axelrod/tournament.py
+++ axelrod/tournament.py
@@ -60,15 +60,24 @@ class Tournament(object):
 
     @players.setter
     def players(self, players):
+        """Ensure that players are passed the tournament attributes"""
         newplayers = []
         for player in players:
-            player.set_tournament_attributes(length=self.turns,
-                                             game=self.game,
-                                             noise=self.noise)
+            player.set_tournament_attributes(
+                length=self.turns,
+                 game=self.game,
+                 noise=self.noise)
             newplayers.append(player)
         self._players = newplayers
 
     def play(self):
+        """
+        Plays the tournament and passes the results to the ResultSet class
+
+        Returns
+        -------
+        axelrod.ResultSet
+        """
         if self._processes is None:
             self._run_serial_repetitions(self._outcome)
         else:
@@ -85,28 +94,46 @@ class Tournament(object):
         return self.result_set
 
     def _build_cache_required(self):
+        """
+        A boolean to indicate whether it is necessary to build the
+        deterministic cache.
+        """
         return (
             not self.noise and (
                 len(self.deterministic_cache) == 0 or
                 not self.prebuilt_cache))
 
     def _build_cache(self, outcome):
+        """
+        For parallel processing, this runs a single round robin in order to
+        build the deterministic cache.
+        """
         self._logger.debug('Playing first round robin to build cache')
         self._run_single_repetition(outcome)
         self._parallel_repetitions -= 1
 
     def _run_single_repetition(self, outcome):
+        """
+        Runs a single round robin and updates the outcome dictionary.
+        """
         output = self._play_round_robin()
         outcome['payoff'].append(output['payoff'])
         outcome['cooperation'].append(output['cooperation'])
 
     def _run_serial_repetitions(self, outcome):
+        """
+        Runs all repetitions of the round robin in serial.
+        """
         self._logger.debug('Playing %d round robins' % self.repetitions)
         for repetition in range(self.repetitions):
             self._run_single_repetition(outcome)
         return True
 
     def _run_parallel_repetitions(self, outcome):
+        """
+        Runs a single round robin to build the deterministic cache and then
+        runs the remaining repetitions in parallel.
+        """
         # At first sight, it might seem simpler to use the multiprocessing Pool
         # Class rather than Processes and Queues. However, Pool can only accept
         # target functions which can be pickled and instance methods cannot.
@@ -126,6 +153,13 @@ class Tournament(object):
         return True
 
     def _n_workers(self):
+        """
+        Determines the number of parallel processes to use.
+
+        Returns
+        -------
+        integer
+        """
         if (2 <= self._processes <= multiprocessing.cpu_count()):
             n_workers = self._processes
         else:
@@ -133,6 +167,18 @@ class Tournament(object):
         return n_workers
 
     def _start_workers(self, workers, work_queue, done_queue):
+        """
+        Initiates the sub-processes to carry out parallel processing.
+
+        Parameters
+        ----------
+        workers : integer
+            The number of sub-processes to create
+        work_queue : multiprocessing.Queue
+            A queue containing an entry for each round robin to be processed
+        done_queue : multiprocessing.Queue
+            A queue containing the output dictionaries from each round robin
+        """
         for worker in range(workers):
             process = multiprocessing.Process(
                 target=self._worker, args=(work_queue, done_queue))
@@ -142,6 +188,18 @@ class Tournament(object):
 
     @staticmethod
     def _process_done_queue(workers, done_queue, outcome):
+        """
+        Retrieves the outcome dictionaries from the parallel sub-processes
+
+        Parameters
+        ----------
+        workers : integer
+            The number of sub-processes in existence
+        done_queue : multiprocessing.Queue
+            A queue containing the output dictionaries from each round robin
+        outcome : dictionary
+           The outcome dictionary to update
+        """
         stops = 0
         while stops < workers:
             output = done_queue.get()
@@ -153,6 +211,16 @@ class Tournament(object):
         return True
 
     def _worker(self, work_queue, done_queue):
+        """
+        The work for each parallel sub-process to execute.
+
+        Parameters
+        ----------
+        work_queue : multiprocessing.Queue
+            A queue containing an entry for each round robin to be processed
+        done_queue : multiprocessing.Queue
+            A queue containing the output dictionaries from each round robin
+        """
         for repetition in iter(work_queue.get, 'STOP'):
             output = self._play_round_robin(cache_mutable=False)
             done_queue.put(output)
@@ -160,6 +228,19 @@ class Tournament(object):
         return True
 
     def _play_round_robin(self, cache_mutable=True):
+        """
+        Create and play a single round robin of matches between all players.
+
+        Parameters
+        ----------
+        cache_mutable : boolean
+            Whether the deterministic cache should be updated
+
+        Returns
+        -------
+        dictionary
+            Containing the payoff and cooperation matrices
+        """
         interactions = {}
         for player1_index in range(self.nplayers):
             for player2_index in range(player1_index, self.nplayers):
