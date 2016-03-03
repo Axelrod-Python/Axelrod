80b41655acbb153ca4e3c2ffeb3340a29a767d37
Owen Campbell <owen.campbell@empiria.co.uk>
2016-03-03 12:33:56 +0000

    Fix PEP8 errors

--- axelrod/tournament_type.py
+++ axelrod/tournament_type.py
@@ -1,32 +1,33 @@
 from .match import Match
 
+
 def pair_of_players(players, player1_index, player2_index):
-        """
-        Create a pair of Player objects.
+    """
+    Create a pair of Player objects.
 
-        If the two index numbers are the same, the second player object is
-        created using the clone method of the first.
+    If the two index numbers are the same, the second player object is
+    created using the clone method of the first.
 
-        Parameters
-        ----------
-        players : list
-            A list of axelrod.Player objects
-        player_1_index : integer
-            index number of player 1 within self.players list
-        player_2_index : integer
-            index number of player 2 within self.players list
+    Parameters
+    ----------
+    players : list
+        A list of axelrod.Player objects
+    player_1_index : integer
+        index number of player 1 within self.players list
+    player_2_index : integer
+        index number of player 2 within self.players list
 
-        Returns
-        -------
-        tuple
-            A pair of axelrod.Player objects
-        """
-        player1 = players[player1_index]
-        if player1_index == player2_index:
-            player2 = player1.clone()
-        else:
-            player2 = players[player2_index]
-        return (player1, player2)
+    Returns
+    -------
+    tuple
+        A pair of axelrod.Player objects
+    """
+    player1 = players[player1_index]
+    if player1_index == player2_index:
+        player2 = player1.clone()
+    else:
+        player2 = players[player2_index]
+    return (player1, player2)
 
 
 def build_round_robin(players, turns, deterministic_cache,
