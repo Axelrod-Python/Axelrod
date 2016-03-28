6d5664a1ce48fa0a138559c5228d9fe49fb349da
Vince Knight <vincent.knight@gmail.com>
2016-03-28 15:49:00 +0100

    Addressing #503
    Moving module docstring to class for strategy.

--- axelrod/strategies/geller.py
+++ axelrod/strategies/geller.py
@@ -1,24 +1,5 @@
 # -*- coding: utf-8 -*-
 
-"""
-Geller - by Martin Chorley (@martinjc), heavily inspired by Matthew Williams (@voxmjw)
-
-This code is inspired by Matthew Williams' talk
-"Cheating at rock-paper-scissors — meta-programming in Python"
-given at Django Weekend Cardiff in February 2014.
-
-His code is here: https://github.com/mattjw/rps_metaprogramming
-and there's some more info here: http://www.mattjw.net/2014/02/rps-metaprogramming/
-
-This code is **way** simpler than Matt's, as in this exercise we already
-have access to the opponent instance, so don't need to go
-hunting for it in the stack. Instead we can just call it to
-see what it's going to play, and return a result based on that
-
-This is almost certainly cheating, and more than likely against the
-spirit of the 'competition' :-)
-"""
-
 import inspect
 
 from axelrod import Actions, Player, random_choice
@@ -29,6 +10,23 @@ class Geller(Player):
     """Observes what the player will do in the next round and adjust.
 
     If unable to do this: will play randomly.
+
+    Geller - by Martin Chorley (@martinjc), heavily inspired by Matthew Williams (@voxmjw)
+
+    This code is inspired by Matthew Williams' talk
+    "Cheating at rock-paper-scissors — meta-programming in Python"
+    given at Django Weekend Cardiff in February 2014.
+
+    His code is here: https://github.com/mattjw/rps_metaprogramming
+    and there's some more info here: http://www.mattjw.net/2014/02/rps-metaprogramming/
+
+    This code is **way** simpler than Matt's, as in this exercise we already
+    have access to the opponent instance, so don't need to go
+    hunting for it in the stack. Instead we can just call it to
+    see what it's going to play, and return a result based on that
+
+    This is almost certainly cheating, and more than likely against the
+    spirit of the 'competition' :-)
     """
 
     name = 'Geller'
