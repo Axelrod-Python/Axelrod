cad9a9863a1c8f2445c3f5bcd1b15606f574352c
Vince Knight <vincent.knight@gmail.com>
2016-03-29 08:25:02 +0100

    Removing commented out code.

--- axelrod/tournament.py
+++ axelrod/tournament.py
@@ -4,7 +4,7 @@ import logging
 import multiprocessing
 
 from .game import Game
-from .result_set import ResultSet#, ProbEndResultSet
+from .result_set import ResultSet
 from .tournament_type import RoundRobin, ProbEndRoundRobin
 from .payoff import payoff_matrix
 from .cooperation import cooperation_matrix
