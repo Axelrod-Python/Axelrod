f3c4cb70d1f9fab3b7cb497faa2cbe2350e2f116
Owen Campbell <owen.campbell@empiria.co.uk>
2016-04-10 11:33:20 +0100

    Add stubs for deterministic cache tests

---
+++ axelrod/tests/unit/test_deterministic_cache.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class TestDeterministicCache(unittest.TestCase):
+
+    def test_init(self):
+        pass
+
+    def test_setitem(self):
+        pass
+
+    def test_save(self):
+        pass
+
+    def test_load(self):
+        pass
