37c755c13f904a22bae710ff8f479bfffe4700d0
Owen Campbell <owen.campbell@empiria.co.uk>
2016-04-12 10:32:06 +0100

    Add dill to docs mocks

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,9 @@ import os
 
 import mock
 
-MOCK_MODULES = ['numpy', 'numpy.linalg', 'numpy.random', 'matplotlib.pyplot', 'matplotlib', 'matplotlib.transforms', 'mpl_toolkits.axes_grid1', 'cloudpickle', 'multiprocess']
+MOCK_MODULES = [
+    'numpy', 'numpy.linalg', 'numpy.random', 'matplotlib.pyplot', 'matplotlib',
+    'matplotlib.transforms', 'mpl_toolkits.axes_grid1', 'dill', 'multiprocess']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
