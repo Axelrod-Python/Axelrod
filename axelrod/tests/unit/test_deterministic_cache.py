25af26bae082667cf98cb03fe0ab2daba7bd55b4
Owen Campbell <owen.campbell@empiria.co.uk>
2016-04-16 17:14:49 +0100

    Alter test for new key format

--- axelrod/tests/unit/test_deterministic_cache.py
+++ axelrod/tests/unit/test_deterministic_cache.py
@@ -8,16 +8,16 @@ class TestDeterministicCache(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.test_key = (TitForTat, Defector)
+        cls.test_key = (TitForTat, Defector, 3)
         cls.test_value = [('C', 'D'), ('D', 'D'), ('D', 'D')]
         cls.test_save_file = 'test_cache_save.txt'
         cls.test_load_file = 'test_cache_load.txt'
         if sys.version_info[0] == 2:
             # Python 2.x
-            cls.test_pickle = b'\x80\x02}q\x00caxelrod.strategies.titfortat\nTitForTat\nq\x01caxelrod.strategies.defector\nDefector\nq\x02\x86q\x03]q\x04(U\x01Cq\x05U\x01Dq\x06\x86q\x07h\x06h\x06\x86q\x08h\x06h\x06\x86q\tes.'
+            cls.test_pickle = '\x80\x02}q\x00caxelrod.strategies.titfortat\nTitForTat\nq\x01caxelrod.strategies.defector\nDefector\nq\x02K\x03\x87q\x03]q\x04(U\x01Cq\x05U\x01Dq\x06\x86q\x07h\x06h\x06\x86q\x08h\x06h\x06\x86q\tes.'
         else:
             # Python 3.x
-            cls.test_pickle = b'\x80\x03}q\x00caxelrod.strategies.titfortat\nTitForTat\nq\x01caxelrod.strategies.defector\nDefector\nq\x02\x86q\x03]q\x04(X\x01\x00\x00\x00Cq\x05X\x01\x00\x00\x00Dq\x06\x86q\x07h\x06h\x06\x86q\x08h\x06h\x06\x86q\tes.'
+            cls.test_pickle = b'\x80\x03}q\x00caxelrod.strategies.titfortat\nTitForTat\nq\x01caxelrod.strategies.defector\nDefector\nq\x02K\x03\x87q\x03]q\x04(X\x01\x00\x00\x00Cq\x05X\x01\x00\x00\x00Dq\x06\x86q\x07h\x06h\x06\x86q\x08h\x06h\x06\x86q\tes.'
         with open(cls.test_load_file, 'wb') as f:
             f.write(cls.test_pickle)
 
@@ -29,19 +29,15 @@ class TestDeterministicCache(unittest.TestCase):
     def test_basic_init(self):
         cache = DeterministicCache()
         self.assertTrue(cache.mutable)
-        self.assertEqual(cache.turns, None)
 
     def test_init_from_file(self):
         cache = DeterministicCache(file_name=self.test_load_file)
         self.assertEqual(cache[self.test_key], self.test_value)
-        self.assertEqual(cache.turns, 3)
 
     def test_setitem(self):
         cache = DeterministicCache()
         cache[self.test_key] = self.test_value
         self.assertEqual(cache[self.test_key], self.test_value)
-        # The first cached entry should set the turns attribute
-        self.assertEqual(cache.turns, 3)
 
     def test_set_immutable_cache(self):
         cache = DeterministicCache()
@@ -54,24 +50,22 @@ class TestDeterministicCache(unittest.TestCase):
         self.assertTrue(cache._is_valid_key(self.test_key))
         # Should return false if key is not a tuple
         self.assertFalse(cache._is_valid_key('test'))
-        # Should return false if tuple is not a pair
-        self.assertFalse(cache._is_valid_key(('test', 'test', 'test')))
-        # Should return false if contents of tuple are not axelrod Players
+        # Should return false if tuple is not a triplet
         self.assertFalse(cache._is_valid_key(('test', 'test')))
-        self.assertFalse(cache._is_valid_key((TitForTat, 'test')))
-        self.assertFalse(cache._is_valid_key(('test', TitForTat)))
+        # Should return false if contents of tuple are not axelrod Players
+        # and an integer
+        self.assertFalse(cache._is_valid_key(('test', 'test', 'test')))
+        self.assertFalse(cache._is_valid_key((TitForTat, 'test', 2)))
+        self.assertFalse(cache._is_valid_key(('test', TitForTat, 2)))
         # Should return false if either player class is stochastic
-        self.assertFalse(cache._is_valid_key((Random, TitForTat)))
-        self.assertFalse(cache._is_valid_key((TitForTat, Random)))
+        self.assertFalse(cache._is_valid_key((Random, TitForTat, 2)))
+        self.assertFalse(cache._is_valid_key((TitForTat, Random, 2)))
 
     def test_is_valid_value(self):
         cache = DeterministicCache()
         self.assertTrue(cache._is_valid_value(self.test_value))
         # Should return false if value is not a list
         self.assertFalse(cache._is_valid_value('test'))
-        # Should return false if length does not match turns attribute
-        cache.turns = 20
-        self.assertFalse(cache._is_valid_value(self.test_value))
 
     def test_save(self):
         cache = DeterministicCache()
@@ -85,4 +79,3 @@ class TestDeterministicCache(unittest.TestCase):
         cache = DeterministicCache()
         cache.load(self.test_load_file)
         self.assertEqual(cache[self.test_key], self.test_value)
-        self.assertEqual(cache.turns, 3)
