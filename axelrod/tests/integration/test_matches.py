a84a98b6b7a3b9578a7e5607dde061af928c8d0e
Vince Knight <vincent.knight@gmail.com>
2016-07-08 16:52:41 +0100

    Add a test for seed giving consistent results
    Note: we need to have set in a seed in both stdlib and numpy.
    
    The weird upper value in the hypothesis test is based on an upper bound
    on the seed function.

--- axelrod/tests/integration/test_matches.py
+++ axelrod/tests/integration/test_matches.py
@@ -2,6 +2,9 @@
 import unittest
 import axelrod
 
+import random
+import numpy
+
 from hypothesis import given
 from hypothesis.strategies import integers
 from axelrod.tests.property import strategy_lists
@@ -10,6 +13,9 @@ C, D = axelrod.Actions.C, axelrod.Actions.D
 
 deterministic_strategies = [s for s in axelrod.ordinary_strategies
                             if not s().classifier['stochastic']]  # Well behaved strategies
+
+stochastic_strategies = [s for s in axelrod.ordinary_strategies
+                            if s().classifier['stochastic']]
 
 class TestMatchOutcomes(unittest.TestCase):
 
@@ -23,3 +29,20 @@ class TestMatchOutcomes(unittest.TestCase):
         matches = [axelrod.Match(players, turns) for _ in range(3)]
         self.assertEqual(matches[0].play(), matches[1].play())
         self.assertEqual(matches[1].play(), matches[2].play())
+
+    @given(strategies=strategy_lists(strategies=stochastic_strategies,
+                                     min_size=2, max_size=2),
+           turns=integers(min_value=1, max_value=20),
+           seed=integers(min_value=0, max_value=4294967295))
+    def test_outcome_repeats_stochastic(self, strategies, turns, seed):
+        """a test to check that if a seed is set stochastic strategies give the
+        same result"""
+        results = []
+        for _ in range(3):
+            random.seed(seed)
+            numpy.random.seed(seed)
+            players = [s() for s in strategies]
+            results.append(axelrod.Match(players, turns).play())
+
+        self.assertEqual(results[0], results[1])
+        self.assertEqual(results[1], results[2])
