47283a05b86009267be9990b40c5225e539af9e4
Ranjini Das <das22r@mtholyoke.edu>
2016-08-07 20:32:31 -0400

    Added strategies Desperate, Willing, Hopeless, Grim
    Adding tests Desperate, Hopeless, Willing and Grim in mutual.py

---
+++ axelrod/strategies/mutual.py
@@ -0,0 +1,60 @@
+from axelrod import Actions, Player, init_args
+
+C, D = Actions.C, Actions.D
+
+class Desperate(Player):
+    """A player that only cooperates after mutual defection"""
+    name = "Desperate"
+    classifier = {
+        'memory_depth': 1,
+        'stochastic': False,
+        'makes_use_of': set(),
+        'inspects_source': False,
+        'manipulates_source': False,
+        'manipulates_state': False
+    }
+
+    def strategy(self, opponent):
+        if not opponent.history:
+            return D
+        if self.history[-1] == D and opponent.history[-1] == D:
+            return C
+        return D
+
+class Hopeless(Player):
+    """A player that only defects after mutual cooperation"""
+    name = "Hopeless"
+    classifier = {
+        'memory_depth': 1,
+        'stochastic': False,
+        'makes_use_of': set(),
+        'inspects_source': False,
+        'manipulates_source': False,
+        'manipulates_state': False
+    }
+
+    def strategy(self, opponent):
+        if not opponent.history:
+            return C
+        if self.history[-1] == C and opponent.history[-1] == C:
+            return D
+        return C
+
+class Willing(Player):
+    """A player that only defects after mutual defection"""
+    name = "Willing"
+    classifier = {
+        'memory_depth': 1,
+        'stochastic': False,
+        'makes_use_of': set(),
+        'inspects_source': False,
+        'manipulates_source': False,
+        'manipulates_state': False
+    }
+
+    def strategy(self, opponent):
+        if not opponent.history:
+            return C
+        if self.history[-1] == D and opponent.history[-1] == D:
+            return D
+        return C
