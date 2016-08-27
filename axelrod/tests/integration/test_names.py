e0a9fce93e51b1281063f52dea13dc68ecbf533e
Owen Campbell <owen.campbell@tanti.org.uk>
2016-08-27 14:11:56 +0100

    Simplify filter in list comprehension

--- axelrod/tests/integration/test_names.py
+++ axelrod/tests/integration/test_names.py
@@ -5,7 +5,7 @@ from axelrod import all_strategies
 class TestNames(unittest.TestCase):
 
     def test_all_strategies_have_names(self):
-        names = [s.name for s in all_strategies if s.name is not None]
+        names = [s.name for s in all_strategies if s.name]
         self.assertEqual(len(names), len(all_strategies))
 
     def test_all_names_are_unique(self):
