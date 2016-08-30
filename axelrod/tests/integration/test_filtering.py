e9accd331c5cb78f93d3e57ee801bd3d8ef1ccee
Owen Campbell <owen.campbell@tanti.org.uk>
2016-08-30 11:10:51 +0100

    [#696] Add test for all boolean classifiers

--- axelrod/tests/integration/test_filtering.py
+++ axelrod/tests/integration/test_filtering.py
@@ -3,13 +3,26 @@ from axelrod import all_strategies, filtered_strategies
 
 
 class TestFiltersAgainstComprehensions(unittest.TestCase):
+    """
+    Test that the results of filtering strategies via a filterset dict
+    match the results from using a list comprehension.
+    """
 
-    def test_long_run_time(self):
-        comprehension = [
-            s for s in all_strategies if
-            s().classifier['long_run_time']]
-        filterset = {
-            'long_run_time': True
-        }
-        filtered = filtered_strategies(filterset)
+    def test_boolean_filtering(self):
+
+        classifiers = [
+            'stochastic',
+            'long_run_time',
+            'manipulates_state',
+            'manipulates_source',
+            'inspects_source']
+
+        for classifier in classifiers:
+            comprehension = set([
+                s for s in all_strategies if
+                s.classifier[classifier]])
+            filterset = {
+                classifier: True
+            }
+        filtered = set(filtered_strategies(filterset))
         self.assertEqual(comprehension, filtered)
