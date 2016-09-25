e0a4fda4923fb0c52bb2a64ca4dbb4f6510f3775
Adam Pohl <adam.pohl9999@gmail.com>
2016-09-25 23:07:22 +0100

    Fixed python 2.7 issue YAAAY!!! :smiling_imp:

--- axelrod/tests/unit/test_worse_and_worse.py
+++ axelrod/tests/unit/test_worse_and_worse.py
@@ -1,5 +1,6 @@
 """Test for the Worse and Worse strategy."""
 
+import sys
 import axelrod
 
 from .test_player import TestPlayer
@@ -26,8 +27,20 @@ class TestWorseAndWorse(TestPlayer):
         Test that the stratergy chooses to defect according to the correct
         probability.
         """
-        self.responses_test([], [], [C, C, D, D, D], random_seed=1,
-        tournament_length=5)
+        if sys.version_info[0] == 2:
+            # Python 2.x
 
-        self.responses_test([], [], [D, D, D, D, D], random_seed=2,
-        tournament_length=5)
+            self.responses_test([], [], [D, C, C, D, D], random_seed=1,
+            tournament_length=5)
+
+            self.responses_test([], [], [C, C, D, D, C], random_seed=2,
+            tournament_length=5)
+
+        elif sys.version_info[0] == 3:
+            # Python 3.x
+
+            self.responses_test([], [], [C, C, D, D, D], random_seed=1,
+            tournament_length=5)
+
+            self.responses_test([], [], [D, D, D, D, D], random_seed=2,
+            tournament_length=5)
