21ec8382b98110053737921e558a1c05a3f1fd01
Vince Knight <vincent.knight@gmail.com>
2016-09-30 08:56:27 +0100

    Test that version exists.

--- axelrod/tests/unit/test_version.py
+++ axelrod/tests/unit/test_version.py
@@ -4,8 +4,6 @@ Tests the version number
 from axelrod import __version__
 import unittest
 
-expected_version = "1.11.0"
-
 class TestVersion(unittest.TestCase):
     def test_version(self):
-        self.assertEqual(__version__, expected_version)
+        self.assertIsInstance(__version__, str)
