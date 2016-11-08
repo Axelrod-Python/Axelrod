e9ad550efb465ea6398945f66eb8878a28b2ea97
James Campbell <james.campbell@tanti.org.uk>
2016-11-08 14:31:18 +0000

    Add tests for two Ashlock Fingerprint functions
    Creats tests for `create_probe_coords` and `create_probes`. Forces some
    refactoring of `create_probe_coords` by expecting only a list to be
    returned instead of a dictionary.

--- axelrod/tests/unit/test_fingerprint.py
+++ axelrod/tests/unit/test_fingerprint.py
@@ -1,10 +1,24 @@
 import unittest
 import axelrod as axl
 from axelrod.fingerprint import *
+from axelrod.strategy_transformers import JossAnnTransformer
+
+
+strategy = axl.WinStayLoseShift
+probe = axl.TitForTat
+coordinates = [(0.0, 0.0), (0.0, 0.5), (0.5, 0.0), (0.5, 0.5)]
+probes = [JossAnnTransformer(c)(probe)() for c in coordinates]
 
 
 class TestFingerprint(unittest.TestCase):
     """Some stuff"""
+
+    @classmethod
+    def setUpClass(cls):
+        cls.strategy = strategy
+        cls.probe = probe
+        cls.expected_probes = probes
+        cls.expected_coordinates = coordinates
 
     def test_init(self):
         strategy = axl.Cooperator()
@@ -13,11 +27,18 @@ class TestFingerprint(unittest.TestCase):
         self.assertEqual(fingerprint.strategy, strategy)
         self.assertEqual(fingerprint.probe, probe)
 
-    def test_create_probe_coords(test):
-        pass
+    def test_create_probe_coords(self):
+        AF = AshlockFingerprint(self.strategy, self.probe)
+        probe_coords = AF.create_probe_coords(0.5)
+        self.assertEqual(probe_coords, self.expected_coordinates)
+        # self.assertEqual(list(probe_coords.values()), [None, None, None, None])
 
     def test_create_probes(self):
-        pass
+        AF = AshlockFingerprint(self.strategy, self.probe)
+        coords = [(0.0, 0.0), (0.0, 0.5), (0.5, 0.0), (0.5, 0.5)]
+        probe_dict = AF.create_probes(self.probe, coords)
+        self.assertEqual(list(probe_dict.keys()), self.expected_coordinates)
+        # self.assertEqual(list(probe_dict.values()), self.expected_probes)
 
     def test_create_edges(self):
         pass
