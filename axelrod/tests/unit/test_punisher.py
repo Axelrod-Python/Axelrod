135a2c76a259a09000abebbfd481bf70858d53dc
Sourav Singh <souravsingh@users.noreply.github.com>
2017-01-10 00:09:21 +0530

    Add corresponding unit test

--- axelrod/tests/unit/test_punisher.py
+++ axelrod/tests/unit/test_punisher.py
@@ -113,3 +113,56 @@ class TestInversePunisher(TestPlayer):
         self.assertEqual(P1.history, [])
         self.assertEqual(P1.grudged, False)
         self.assertEqual(P1.grudge_memory, 0)
+
+class TestLevelPunisher(TestPlayer):
+
+    name = "Level Punisher"
+    player = axelrod.LevelPunisher
+    expected_classifier = {
+        'memory_depth': float('inf'),  # Long memory
+        'stochastic': False,
+        'makes_use_of': set(),
+        'long_run_time': False,
+        'inspects_source': False,
+        'manipulates_source': False,
+        'manipulates_state': False
+    }
+
+    def test_init(self):
+        """Tests for the __init__ method."""
+        P1 = axelrod.LevelPunisher()
+        self.assertEqual(P1.history, [])
+        self.assertEqual(P1.mem_length, 1)
+        self.assertEqual(P1.grudged, False)
+        self.assertEqual(P1.grudge_memory, 1)
+
+    def test_strategy(self):
+        self.responses_test([], [], [C], attrs={"grudged": False})
+        self.responses_test([C], [C], [C], attrs={"grudged": False})
+        self.responses_test([C], [D], [D], attrs={"grudged": True})
+        for i in range(10):
+            self.responses_test([C, C] + [D] * i, [C, D] + [C] * i, [D],
+                                attrs={"grudged": True, "grudge_memory": i,
+                                       "mem_length": 10})
+        # Eventually the grudge is dropped
+        i = 11
+        self.responses_test([C, C] + [D] * i, [C, D] + [C] * i, [C],
+                            attrs={"grudged": False, "grudge_memory": 0,
+                                    "mem_length": 10})
+
+        # Grudged again on opponent's D
+        self.responses_test([C, C] + [D] * i + [C], [C, D] + [C] * i + [D], [D],
+                            attrs={"grudged": True, "grudge_memory": 0,
+                                   "mem_length": 2})
+
+    def test_reset_method(self):
+        """Tests the reset method."""
+        P1 = axelrod.LevelPunisher()
+        P1.history = [C, D, D, D]
+        P1.grudged = True
+        P1.grudge_memory = 4
+        P1.reset()
+        self.assertEqual(P1.history, [])
+        self.assertEqual(P1.grudged, False)
+        self.assertEqual(P1.grudge_memory, 0)
+
