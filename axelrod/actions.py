13891c1cba5960783dc105fedfa1f4d4ea18584b
Sourav Singh <souravsingh@users.noreply.github.com>
2017-01-13 22:34:26 +0530

    Add type hints for action

--- axelrod/actions.py
+++ axelrod/actions.py
@@ -1,10 +1,14 @@
+from typing import NewType
+
+C = NewType('Action', str)
+D = NewType('Action', str)
 
 class Actions(object):
     C = 'C'
     D = 'D'
 
 
-def flip_action(action):
+def flip_action(action: Action) -> Action:
     if action == Actions.C:
         return Actions.D
     elif action == Actions.D:
