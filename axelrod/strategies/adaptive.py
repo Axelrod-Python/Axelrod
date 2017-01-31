a6cd147edff6447ad86d5d09ff2b620cff15d9e8
janga <janga1997@gmail.com>
2017-01-31 13:16:38 +0530

    Add None to __init__ as return type

--- axelrod/strategies/adaptive.py
+++ axelrod/strategies/adaptive.py
@@ -27,7 +27,7 @@ class Adaptive(Player):
         'manipulates_state': False
     }
 
-    def __init__(self, initial_plays: List[axelrod.actions.Action] =None):
+    def __init__(self, initial_plays: List[axelrod.actions.Action] =None) -> None:
         super().__init__()
         if not initial_plays:
             initial_plays = [C] * 6 + [D] * 5
