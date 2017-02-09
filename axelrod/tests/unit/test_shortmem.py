b1f93b8e7bb83736538de577fbbb1e64caeeb411
janga <janga1997@gmail.com>
2017-02-09 15:45:03 +0530

    Add tests for ShortMem

---
+++ axelrod/tests/unit/test_shortmem.py
@@ -0,0 +1,34 @@
+"""Tests for the Cooperator strategy."""
+
+import axelrod
+from .test_player import TestPlayer
+
+C, D = axelrod.Actions.C, axelrod.Actions.D
+
+class TestCooperator(TestPlayer):
+
+    name = "ShortMem"
+    player = axelrod.ShortMem
+    expected_classifier = {
+        'memory_depth': 10,
+        'stochastic': False,
+        'makes_use_of': set(),
+        'inspects_source': False,
+        'manipulates_source': False,
+        'manipulates_state': False
+    }
+
+    def test_strategy(self):
+        # Starts by cooperating for the first ten moves.
+        self.responses_test([C], [C] * 4, [D] * 4)
+        self.responses_test([C], [C] * 4 + [D] *6, [D] * 4 + [C] * 6)
+        
+        #Cooperate if in the last ten moves, Cooperations - Defections >= 30
+        self.responses_test([C], [C] * 13 + [D] * 7, [D] * 16 + [C] * 7)
+            
+        #Defect if in the last ten moves, Defections - Cooperations >= 30
+        self.responses_test([D], [D] * 13 + [C] * 7, [C] * 12 + [D] * 8)
+        
+        #If Defections - Cooperations <30 and vice-versa, apply TitForTat 
+        self.responses_test([D], [C] * 13 + [D] * 7, [C] * 15 + [D] * 5)
+        self.responses_test([C], [C] * 17 + [D] * 7, [D] * 18 + [C] * 6)
