3e831d8b1317aa5252f5bcaff6006f9cb4c4de52
Sourav Singh <souravsingh@users.noreply.github.com>
2017-02-17 00:30:30 +0530

    Add test for Resurrection

---
+++ axelrod/tests/unit/test_resurrection.py
@@ -0,0 +1,28 @@
+"""Test for the Resurrection strategy."""
+
+import axelrod
+from .test_player import TestPlayer
+
+C, D = axelrod.Actions.C, axelrod.Actions.D
+class Resurrection(TestPlayer):
+
+    name = "Resurrection"
+    player = axelrod.Resurrection
+    expected_classifier = {
+        'memory_depth': 1,
+        'stochastic': False,
+        'makes_use_of': set(),
+        'long_run_time': False,
+        'inspects_source': False,
+        'manipulates_source': False,
+        'manipulates_state': False
+    }
+
+    def test_strategy(self):
+        # Starts by Cooperating
+        self.first_play_test(C)
+        
+        # Check if the turns played are greater than 5 
+        self.responses_test([D], [C, D, D, D, D, D, C, D] , [C] * 8)
+        
+        
