55b8c2af6686fc9b187bd0370cc7691737fe9934
Sourav Singh <souravsingh@users.noreply.github.com>
2017-03-01 05:23:13 +0530

    Fixes to strategy

--- axelrod/strategies/resurrection.py
+++ axelrod/strategies/resurrection.py
@@ -7,7 +7,7 @@ C, D = Actions.C, Actions.D
 class Resurrection(Player):
     """
     A player starts by cooperating and defects if the number of rounds 
-    played by the player is greater than five and last five rounds are defections.
+    played by the player is greater than five and the last five rounds are defections.
     Otherwise, the strategy plays like Tit-for-tat.
     
     Names:
@@ -29,7 +29,7 @@ class Resurrection(Player):
     def strategy(self, opponent: Player) -> Action:
         if len(self.history) == 0:
             return C
-        if len(self.history) >= 5 and self.history[-5:] == [D,D,D,D,D]:
+        if len(self.history) >= 5 and self.history[-5:] == [D, D, D, D, D]:
             return D
         else:
             return opponent.history[-1]
