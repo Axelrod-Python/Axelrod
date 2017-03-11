766df5d6e2f106986ada38a557d7055dcabc7da0
Owen Campbell <owen.campbell@tanti.org.uk>
2017-03-11 14:08:33 +0000

    [#894] Fix PEP8 errors and remove __main__ condition

--- axelrod/tests/unit/test_plot.py
+++ axelrod/tests/unit/test_plot.py
@@ -16,17 +16,19 @@ class TestPlot(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.players = (axelrod.Alternator(), axelrod.TitForTat(), axelrod.Defector())
+        cls.players = (
+            axelrod.Alternator(), axelrod.TitForTat(), axelrod.Defector())
         cls.turns = 5
         cls.matches = {
-                        (0, 1): [axelrod.Match((cls.players[0], cls.players[1]),
-                        turns=cls.turns) for _ in range(3)],
-                        (0, 2): [axelrod.Match((cls.players[0], cls.players[2]),
-                        turns=cls.turns) for _ in range(3)],
-                        (1, 2): [axelrod.Match((cls.players[1], cls.players[2]),
-                        turns=cls.turns) for _ in range(3)]}
-                        # This would not actually be a round robin tournament
-                        # (no cloned matches)
+            (0, 1): [axelrod.Match((cls.players[0], cls.players[1]),
+            turns=cls.turns) for _ in range(3)],
+            (0, 2): [axelrod.Match((cls.players[0], cls.players[2]),
+            turns=cls.turns) for _ in range(3)],
+            (1, 2): [axelrod.Match((cls.players[1], cls.players[2]),
+            turns=cls.turns) for _ in range(3)]
+        }
+        # This would not actually be a round robin tournament
+        # (no cloned matches)
 
         cls.interactions = {}
         for index_pair, matches in cls.matches.items():
@@ -39,27 +41,34 @@ class TestPlot(unittest.TestCase):
 
         cls.test_result_set = axelrod.ResultSet(cls.players, cls.interactions)
         cls.expected_boxplot_dataset = [
-               [(17 / 5 + 9 / 5) / 2 for _ in range(3)],
-               [(13 / 5 + 4 / 5) / 2 for _ in range(3)],
-               [3 / 2 for _ in range(3)]
-               ]
+            [(17 / 5 + 9 / 5) / 2 for _ in range(3)],
+            [(13 / 5 + 4 / 5) / 2 for _ in range(3)],
+            [3 / 2 for _ in range(3)]
+        ]
         cls.expected_boxplot_xticks_locations = [1, 2, 3, 4]
-        cls.expected_boxplot_xticks_labels = ['Defector', 'Tit For Tat', 'Alternator']
+        cls.expected_boxplot_xticks_labels = [
+            'Defector', 'Tit For Tat', 'Alternator']
 
         cls.expected_lengthplot_dataset = [
-               [cls.turns for _ in range(3)],
-               [cls.turns for _ in range(3)],
-               [cls.turns for _ in range(3)],
-               ]
+            [cls.turns for _ in range(3)],
+            [cls.turns for _ in range(3)],
+            [cls.turns for _ in range(3)],
+        ]
 
         cls.expected_payoff_dataset = [
-            [0, mean([9/5 for _ in range(3)]), mean([17/5 for _ in range(3)])],
-            [mean([4/5 for _ in range(3)]), 0, mean([13/5 for _ in range(3)])],
-            [mean([2/5 for _ in range(3)]), mean([13/5 for _ in range(3)]), 0]
+            [0, mean(
+                [9 / 5 for _ in range(3)]),
+                mean([17 / 5 for _ in range(3)])],
+            [mean(
+                [4 / 5 for _ in range(3)]), 0,
+                mean([13 / 5 for _ in range(3)])],
+            [mean(
+                [2 / 5 for _ in range(3)]),
+                mean([13 / 5 for _ in range(3)]), 0]
         ]
-        cls.expected_winplot_dataset = ([[2, 2, 2], [0, 0, 0], [0, 0, 0]],
-                                        ['Defector', 'Tit For Tat', 'Alternator'])
-
+        cls.expected_winplot_dataset = (
+            [[2, 2, 2], [0, 0, 0], [0, 0, 0]],
+            ['Defector', 'Tit For Tat', 'Alternator'])
 
     def test_default_cmap(self):
         cmap = axelrod.plot.default_cmap()
@@ -76,7 +85,6 @@ class TestPlot(unittest.TestCase):
 
         cmap = axelrod.plot.default_cmap('2.0')
         self.assertEqual(cmap, 'viridis')
-
 
     def test_init(self):
         plot = axelrod.Plot(self.test_result_set)
@@ -238,7 +246,3 @@ class TestPlot(unittest.TestCase):
                                     progress_bar=False))
         else:  # pragma: no cover
             self.skipTest('matplotlib not installed')
-
-
-if __name__ == '__main__':
-    unittest.main()
