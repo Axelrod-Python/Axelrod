c1f66b4e53917d5a4d1f0fa3b89ba50931d19c13
E Shaw <shaweric01@gmail.com>
2017-03-25 13:18:04 +0800

    issues: new actions.py method - make list?, line in test_player, TrickyCooperator, testing problems for cycler

--- axelrod/tests/strategies/test_better_and_better.py
+++ axelrod/tests/strategies/test_better_and_better.py
@@ -22,7 +22,9 @@ class TestBetterAndBetter(TestPlayer):
 
     def test_strategy(self):
         """Tests that the strategy gives expected behaviour."""
-        self.first_play_test(D, seed=3)  # D is very unlikely
+
+        self.first_play_test(D, seed=3)  # C is very unlikely
+        self.first_play_test(C, seed=1514)  # first seed (starting from seed=0) that cooperates on first round
         self.versus_test(axelrod.Defector(), [(D, D), (D, D), (D, D), (D, D), (C, D), (D, D), (D, D), (D, D), (D, D)],
                          seed=6)
         self.versus_test(axelrod.Cooperator(), [(D, C), (D, C), (D, C), (D, C), (D, C), (D, C), (D, C), (D, C), (D, C)],
--- axelrod/tests/unit/test_actions.py
+++ axelrod/tests/unit/test_actions.py
@@ -1,6 +1,7 @@
 import unittest
 from axelrod import Actions, flip_action
-
+from axelrod.actions import str_to_actions
+from types import GeneratorType
 
 C, D = Actions.C, Actions.D
 
@@ -22,3 +23,20 @@ class TestAction(unittest.TestCase):
 
     def test_error(self):
         self.assertRaises(ValueError, flip_action, 'R')
+
+    def test_str_to_actions_is_generator(self):
+        to_test = str_to_actions('c')
+        self.assertIsInstance(to_test, GeneratorType)
+
+    def test_str_to_actions_upper_and_lower(self):
+        to_test = str_to_actions('cDdC')
+        self.assertEqual(next(to_test), Actions.C)
+        self.assertEqual(next(to_test), Actions.D)
+        self.assertEqual(next(to_test), Actions.D)
+        self.assertEqual(next(to_test), Actions.C)
+        self.assertRaises(StopIteration, next, to_test)
+
+    def test_str_to_actions_only_raises_value_error_once_offending_letter_is_reached(self):
+        to_test = str_to_actions('cAc')
+        self.assertEqual(next(to_test), Actions.C)
+        self.assertRaises(ValueError, next, to_test)
