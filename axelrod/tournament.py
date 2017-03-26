287abd905ae0f372269ca24462d19da150b3ffd2
Areeb Ahmed <areeb.ahmed04@gmail.com>
2017-03-27 00:07:52 +0530

    Fix type error in tournament

--- axelrod/tournament.py
+++ axelrod/tournament.py
@@ -8,11 +8,14 @@ import warnings
 import tqdm
 
 from axelrod import on_windows
+from axelrod.player import Player
+from axelrod.MatchGenerator import MatchGenerator
 from .game import Game
 from .match import Match
 from .match_generator import RoundRobinMatches, ProbEndRoundRobinMatches, SpatialMatches, ProbEndSpatialMatches
-from .result_set import ResultSetFromFile, ResultSet
+from .result_set import ResultSetFromFile, ResultSet, BigResultSet
 
+from typing import List
 
 class Tournament(object):
 
