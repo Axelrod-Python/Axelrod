dc9ad2709a5d3e7ad50a25df277e842680af707d
alajara <armando.lajara@mail.citytech.cuny.edu>
2017-03-30 00:28:45 -0400

    Update test_stalker.py

--- axelrod/tests/strategies/test_stalker.py
+++ axelrod/tests/strategies/test_stalker.py
@@ -26,9 +26,11 @@ class TestStalker(TestPlayer):
         self.first_play_test(C)
 
         # current_average_score > very_good_score
-        actions = [(C, C), (C, C), (C, D), (C, D), (D, C), (C, C),
-                   (C, D), (C, C), (C, C), (D, D)]
-        self.versus_test(opponent=axelrod.MockPlayer([C,C,D,D,C,C,D,C,C,D]), expected_actions=actions)
+        actions = [(C, D)] * 2 + [(D, C)] * 4 + [(D, D)]
+        self.versus_test(opponent=axelrod.MockPlayer([D] * 2 + [C] * 4), expected_actions=actions)
+        
+        actions = [(D, C), (D, C)] + [(C, C)] * 4 + [(D, C)]
+        self.versus_test(opponent=axelrod.MockPlayer([C] * 6), expected_actions=actions)
         
         # wish_score < current_average_score < very_good_score
         actions = [(C, C)] * 7 + [(C, D), (C, D), (C, C), (C, C), (D, C)]
