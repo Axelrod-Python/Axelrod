e50d2e1752dd7ee3d5aa5176f23033c557fd0a4f
Vince Knight <vincent.knight@gmail.com>
2017-04-01 14:23:26 +0100

    Add a test for average_score > good score.
    This requires tricking the player to defect before the end of the game.

--- axelrod/tests/strategies/test_stalker.py
+++ axelrod/tests/strategies/test_stalker.py
@@ -1,10 +1,10 @@
 """Tests for the Stalker strategy."""
 
-import axelrod
+import axl as axl
 import random
 from .test_player import TestPlayer
 
-C, D = axelrod.Actions.C, axelrod.Actions.D
+C, D = axl.Actions.C, axl.Actions.D
 
 class TestStalker(TestPlayer):
 
@@ -21,41 +21,53 @@ class TestStalker(TestPlayer):
     }
 
     def test_strategy(self):
-        
+
         # Start with cooperation
         self.first_play_test(C)
 
-        # current_average_score > very_good_score
-        actions = [(C, D)] * 2 + [(C, C), (D, C), (C, C), (C, C), (D, D)] 
-        self.versus_test(opponent=axelrod.MockPlayer([D] * 2 + [C] * 4), expected_actions=actions)
-        
-        actions = [(C, C)] * 6 + [(D, C)]
-        self.versus_test(opponent=axelrod.MockPlayer([C] * 6), expected_actions=actions)
-        
+
+        actions = [(C, D)] * 2 + [(C, C), (D, C), (C, C), (C, C), (D, D)]
+        self.versus_test(opponent=axl.MockPlayer([D] * 2 + [C] * 4),
+                         expected_actions=actions)
+
+        actions = [(C, C)] * 3 + [(D, C)]
+        self.versus_test(opponent=axl.Cooperator(), expected_actions=actions)
+
         # wish_score < current_average_score < very_good_score
         actions = [(C, C)] * 7 + [(C, D), (C, D), (C, C), (C, C), (D, C)]
-        self.versus_test(opponent=axelrod.MockPlayer([C] * 7 + [D] * 2), expected_actions=actions)
-        
+        self.versus_test(opponent=axl.MockPlayer(actions=[C] * 7 + [D] * 2),
+                         expected_actions=actions)
+
         actions = [(C, C)] * 7 + [(C, D), (C, C), (D, C)]
-        self.versus_test(opponent=axelrod.MockPlayer([C] * 7 + [D]), expected_actions=actions)
-       
+        self.versus_test(opponent=axl.MockPlayer(actions=[C] * 7 + [D]),
+                         expected_actions=actions)
+
         # current_average_score > 2
         actions = [(C, C)] * 9 + [(D, C)]
-        self.versus_test(axelrod.Cooperator(), expected_actions=actions)
-    
+        self.versus_test(axl.Cooperator(), expected_actions=actions)
+
         # 1 < current_average_score < 2
         actions = [(C, C)] * 7 + [(C, D)] * 4 + [(D, D)]
-        self.versus_test(opponent=axelrod.MockPlayer([C] * 7 + [D] * 5), expected_actions=actions)
+        self.versus_test(opponent=axl.MockPlayer(actions=[C] * 7 + [D] * 5),
+                         expected_actions=actions)
 
         # current_average_score < 1
         actions = [(C, D)] + [(D, D)] * 2 + [(C, D)] * 3 + [(D, D),
                    (C, D), (D, D), (C, D), (D, D), (C, D), (D, D)]
-        self.versus_test(axelrod.Defector(), expected_actions=actions, seed=6)
-        
+        self.versus_test(axl.Defector(), expected_actions=actions, seed=6)
+
         actions = [(C, D)] * 3 + [(D, D), (C, D), (D, D), (C, D),
                    (C, D), (D, D), (C, D), (C, D), (C, D), (D, D)]
-        self.versus_test(axelrod.Defector(), expected_actions=actions, seed=7)
-       
+        self.versus_test(axl.Defector(), expected_actions=actions, seed=7)
+
         # defect in last round
         actions = [(C, C)] * 199 + [(D, C)]
-        self.versus_test(axelrod.Cooperator(), expected_actions=actions, match_attributes={"length": 200})
+        self.versus_test(axl.Cooperator(), expected_actions=actions,
+                         match_attributes={"length": 200})
+
+        # Force a defection before the end of the actual match which ensures
+        # that current_average_score > very_good_score
+        actions = [(C, C)] * 3 + [(D, C)] * 3
+        self.versus_test(opponent=axl.Cooperator(),
+                         expected_actions=actions,
+                         match_attributes={"length": 4})
