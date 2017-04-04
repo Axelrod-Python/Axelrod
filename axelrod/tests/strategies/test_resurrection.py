1394c93eacdea9b0d0665d850df80015e636879c
Sourav Singh <souravsingh@users.noreply.github.com>
2017-04-05 00:34:41 +0530

    Add tests for DoubleResurrection

--- axelrod/tests/strategies/test_resurrection.py
+++ axelrod/tests/strategies/test_resurrection.py
@@ -33,3 +33,29 @@ class Resurrection(TestPlayer):
         
         # Check for four defections and a cooperation by opponent
         self.responses_test([D], [C] * 5, [D, D, D, D, D, C])
+        
+class TestDoubleResurrection(TestPlayer):
+
+    name = "DoubleResurrection"
+    player = axelrod.DoubleResurrection
+    expected_classifier = {
+        'memory_depth': 1,
+        'stochastic': False,
+        'makes_use_of': set(),
+        'long_run_time': False,
+        'inspects_source': False,
+        'manipulates_source': False,
+        'manipulates_state': False
+    }
+
+    def test_strategy(self):
+        # Starts by Cooperating
+        self.first_play_test(C)
+        
+        # Check if the turns played are greater than 5 
+        self.responses_test([C], [D, C, C, D, D, D, D, D] , [C] * 8)
+        
+        self.responses_test([D], [C, D, D, C, C, C, C, C] , [C] * 8)
+        
+        #Check if turns played are less than 5.
+        self.responses_test([C], [D, C, D, C], [C] * 4)
