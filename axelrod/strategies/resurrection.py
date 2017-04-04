e7c805f8d1b574cbae7a3d550263b2074d7c6ead
Sourav Singh <souravsingh@users.noreply.github.com>
2017-04-05 00:31:41 +0530

    Add DoubleResurrection

--- axelrod/strategies/resurrection.py
+++ axelrod/strategies/resurrection.py
@@ -33,4 +33,34 @@ class Resurrection(Player):
             return D
         else:
             return opponent.history[-1]
+        
+class DoubleResurrection(Player):
+    """
+    A player starts by cooperating and defects if the number of rounds 
+    played by the player is greater than five and the last five rounds are cooperations.
+    If the last five rounds were defections, the player cooperates.
+    
+    Names:
+    - DoubleResurrection: Name from CoopSim https://github.com/jecki/CoopSim     
+    """
 
+    name = 'DoubleResurrection'
+    classifier = {
+        'memory_depth': 1,  
+        'stochastic': False,
+        'makes_use_of': set(),
+        'long_run_time': False,
+        'inspects_source': False,
+        'manipulates_source': False,
+        'manipulates_state': False
+    }
+
+    def strategy(self, opponent: Player) -> Action:
+        if len(self.history) == 0:
+            return C
+        if len(self.history) >= 5 and self.history[-5:] == [C, C, C, C, C]:
+            return D
+        elif len(self.history) >= 5 and self.history[-5:] == [D, D, D, D, D]:
+            return C
+        else:
+            return opponent.history[-1]
