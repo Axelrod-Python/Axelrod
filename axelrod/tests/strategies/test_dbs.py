218251290d3cbc4f641e27339081e3aac2895382
edouard_argenson <edouard.argenson@gmail.com>
2017-04-17 15:19:08 +0200

    apply PEP8 recommendations to code

--- axelrod/tests/strategies/test_dbs.py
+++ axelrod/tests/strategies/test_dbs.py
@@ -24,27 +24,49 @@ class TestDBS(TestPlayer):
         # start by cooperating
         self.first_play_test(C)
 
-        default_init_kwargs = {'discount_factor':.75, 'promotion_threshold':3, 'violation_threshold':4, 'reject_threshold':4,'tree_depth':5}
+        default_init_kwargs = {
+                'discount_factor':.75, 'promotion_threshold':3,
+                'violation_threshold':4, 'reject_threshold':4, 
+                'tree_depth':5
+                }
 
+        # test that DBS always cooperate against Cooperator
         actions = [(C, C)] * 7
-        self.versus_test(opponent=axelrod.Cooperator(), expected_actions=actions)
+        self.versus_test(
+                opponent=axelrod.Cooperator(), 
+                expected_actions=actions,
+                init_kwargs = default_init_kwargs
+                )
 
         # test if it correctly learns Alternator strategy
-        actions = [(C,C),(C,D)]*3 + [(D,C),(C,D)]*3 
-        self.versus_test(opponent=axelrod.Alternator(), expected_actions=actions,init_kwargs = default_init_kwargs)
-
+        actions = [(C, C), (C, D)] * 3 + [(D, C), (C, D)] * 3 
+        self.versus_test(
+                opponent=axelrod.Alternator(),
+                expected_actions=actions,
+                init_kwargs = default_init_kwargs
+                )
 
         # check that algorithms take into account a change in 
         # opponent's strategy
-        mock_actions = [C,C,C,D,D,D,D,D,D,D]
-        exp_actions = [(C,C)]*3 + [(C,D)]*4 + [(D,D)]*3 
-        self.versus_test(opponent=axelrod.MockPlayer(actions=mock_actions),
-                                 expected_actions=exp_actions, seed=None,init_kwargs = default_init_kwargs)
+        mock_actions = [C, C, C, D, D, D, D, D, D, D]
+        exp_actions = [(C, C)] * 3 + [(C, D)] * 4 + [(D, D)] * 3 
+        self.versus_test(
+                opponent=axelrod.MockPlayer(actions=mock_actions),
+                expected_actions=exp_actions, 
+                init_kwargs=default_init_kwargs
+                )
 
         # check that adaptation is faster if diminishing promotion_threshold
-        init_kwargs_2 = {'discount_factor':.75, 'promotion_threshold':2, 'violation_threshold':4, 'reject_threshold':4,'tree_depth':5}
-        mock_actions = [C,C,C,D,D,D,D,D,D,D]
-        exp_actions = [(C,C)]*3 + [(C,D)]*3 + [(D,D)]*4 
-        self.versus_test(opponent=axelrod.MockPlayer(actions=mock_actions),
-                                 expected_actions=exp_actions, seed=None,init_kwargs = init_kwargs_2)
+        init_kwargs_2 = {
+                'discount_factor':.75, 'promotion_threshold':2, 
+                'violation_threshold':4, 'reject_threshold':4,
+                'tree_depth':5
+                }
+        mock_actions = [C, C, C, D, D, D, D, D, D, D]
+        exp_actions = [(C, C)] * 3 + [(C, D)] * 3 + [(D, D)] * 4 
+        self.versus_test(
+                opponent=axelrod.MockPlayer(actions=mock_actions),
+                expected_actions=exp_actions, 
+                init_kwargs = init_kwargs_2
+                )
 
