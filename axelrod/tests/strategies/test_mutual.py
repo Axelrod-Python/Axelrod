fbc12aed6aef62f1e93408e1a64dafc2580471cd
Marios Zoulias <t8140029@dias.aueb.gr>
2017-04-17 18:39:52 +0300

    PEP8 changes

--- axelrod/tests/strategies/test_mutual.py
+++ axelrod/tests/strategies/test_mutual.py
@@ -20,39 +20,39 @@ class TestDesperate(TestPlayer):
     }
 
     def test_strategy(self):
-        self.first_play_test(C, seed = 1)
-        self.first_play_test(D, seed = 2)
+        self.first_play_test(C, seed=1)
+        self.first_play_test(D, seed=2)
 
         # Our Player (Desperate) vs Cooperator SEED --> 1
         opponent = axelrod.Cooperator()
         opponent_actions = [C] * 5
         actions = [(C, C), (D, C), (D, C), (D, C), (D, C)]
-        self.versus_test(opponent, expected_actions = actions, seed = 1)
+        self.versus_test(opponent, expected_actions=actions, seed=1)
 
         # Our Player (Desperate) vs Cooperator SEED --> 2
         opponent = axelrod.Cooperator()
         actions = [(D, C), (D, C), (D, C), (D, C), (D, C)]
-        self.versus_test(opponent, expected_actions = actions, seed = 2)
+        self.versus_test(opponent, expected_actions=actions, seed=2)
 
         # Our Player (Desperate) vs Defector SEED --> 1
         opponent = axelrod.Defector()
         actions = [(C, D), (D, D), (C, D), (D, D), (C, D)]
-        self.versus_test(opponent, expected_actions = actions, seed = 1)
+        self.versus_test(opponent, expected_actions=actions, seed=1)
 
         # Our Player (Desperate) vs Defector SEED --> 2
         opponent = axelrod.Defector()
         actions = [(D, D), (C, D), (D, D), (C, D), (D, D)]
-        self.versus_test(opponent, expected_actions = actions, seed = 2)
+        self.versus_test(opponent, expected_actions=actions, seed=2)
 
         # Our Player (Desperate) vs Alternator SEED --> 1
         opponent = axelrod.Alternator()
         actions = [(C, C), (D, D), (C, C), (D, D), (C, C)]
-        self.versus_test(opponent, expected_actions = actions, seed = 1)
+        self.versus_test(opponent, expected_actions=actions, seed=1)
 
         # Our Player (Desperate) vs Alternator SEED --> 2
         opponent = axelrod.Alternator()
         actions = [(D, C), (D, D), (C, C), (D, D), (C, C)]
-        self.versus_test(opponent, expected_actions = actions, seed = 2)
+        self.versus_test(opponent, expected_actions=actions, seed=2)
 
 class TestHopeless(TestPlayer):
 
@@ -69,39 +69,39 @@ class TestHopeless(TestPlayer):
     }
 
     def test_strategy(self):
-        self.first_play_test(C, seed = 1)
-        self.first_play_test(D, seed = 2)
+        self.first_play_test(C, seed=1)
+        self.first_play_test(D, seed=2)
 
         # Our Player (Hopeless) vs Cooperator SEED --> 1
         opponent = axelrod.Cooperator()
         opponent_actions = [C] * 5
         actions = [(C, C), (D, C), (C, C), (D, C), (C, C)]
-        self.versus_test(opponent, expected_actions = actions, seed = 1)
+        self.versus_test(opponent, expected_actions=actions, seed=1)
 
         # Our Player (Hopeless) vs Cooperator SEED --> 2
         opponent = axelrod.Cooperator()
         actions = [(D, C), (C, C), (D, C), (C, C), (D, C)]
-        self.versus_test(opponent, expected_actions = actions, seed = 2)
+        self.versus_test(opponent, expected_actions=actions, seed=2)
 
         # Our Player (Hopeless) vs Defector SEED --> 1
         opponent = axelrod.Defector()
         actions = [(C, D), (C, D), (C, D), (C, D), (C, D)]
-        self.versus_test(opponent, expected_actions = actions, seed = 1)
+        self.versus_test(opponent, expected_actions=actions, seed=1)
 
         # Our Player (Hopeless) vs Defector SEED --> 2
         opponent = axelrod.Defector()
         actions = [(D, D), (C, D), (C, D), (C, D), (C, D)]
-        self.versus_test(opponent, expected_actions = actions, seed = 2)
+        self.versus_test(opponent, expected_actions=actions, seed=2)
 
         # Our Player (Hopeless) vs Alternator SEED --> 1
         opponent = axelrod.Alternator()
         actions = [(C, C), (D, D), (C, C), (D, D), (C, C)]
-        self.versus_test(opponent, expected_actions = actions, seed = 1)
+        self.versus_test(opponent, expected_actions=actions, seed=1)
 
         # Our Player (Hopeless) vs Alternator SEED --> 2
         opponent = axelrod.Alternator()
         actions = [(D, C), (C, D), (C, C), (D, D), (C, C)]
-        self.versus_test(opponent, expected_actions = actions, seed = 2)
+        self.versus_test(opponent, expected_actions=actions, seed=2)
 
 
 class TestWilling(TestPlayer):
@@ -119,36 +119,36 @@ class TestWilling(TestPlayer):
     }
 
     def test_strategy(self):
-        self.first_play_test(C, seed = 1)
-        self.first_play_test(D, seed = 2)
+        self.first_play_test(C, seed=1)
+        self.first_play_test(D, seed=2)
 
         # Our Player (Willing) vs Cooperator SEED --> 1
         opponent = axelrod.Cooperator()
         opponent_actions = [C] * 5
         actions = [(C, C), (C, C), (C, C), (C, C), (C, C)]
-        self.versus_test(opponent, expected_actions = actions, seed = 1)
+        self.versus_test(opponent, expected_actions=actions, seed=1)
 
         # Our Player (Willing) vs Cooperator SEED --> 2
         opponent = axelrod.Cooperator()
         actions = [(D, C), (C, C), (C, C), (C, C), (C, C)]
-        self.versus_test(opponent, expected_actions = actions, seed = 2)
+        self.versus_test(opponent, expected_actions=actions, seed=2)
 
         # Our Player (Willing) vs Defector SEED --> 1
         opponent = axelrod.Defector()
         actions = [(C, D), (C, D), (C, D), (C, D), (C, D)]
-        self.versus_test(opponent, expected_actions = actions, seed = 1)
+        self.versus_test(opponent, expected_actions=actions, seed=1)
 
         # Our Player (Willing) vs Defector SEED --> 2
         opponent = axelrod.Defector()
         actions = [(D, D), (D, D), (D, D), (D, D), (D, D)]
-        self.versus_test(opponent, expected_actions=actions, seed = 2)
+        self.versus_test(opponent, expected_actions=actions, seed=2)
 
         # Our Player (Willing) vs Alternator SEED --> 1
         opponent = axelrod.Alternator()
         actions = [(C, C), (C, D), (C, C), (C, D), (C, C)]
-        self.versus_test(opponent, expected_actions = actions, seed = 1)
+        self.versus_test(opponent, expected_actions=actions, seed=1)
 
         # Our Player (Willing) vs Alternator SEED --> 2
         opponent = axelrod.Alternator()
         actions = [(D, C), (C, D), (C, C), (C, D), (C, C)]
-        self.versus_test(opponent, expected_actions = actions, seed = 2)
+        self.versus_test(opponent, expected_actions=actions, seed=2)
