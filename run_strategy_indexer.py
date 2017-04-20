d415f1a36eec2518f3ff7409cfbb0f698c559b3e
Vince Knight <vincent.knight@gmail.com>
2017-04-20 12:38:33 +0100

    Use pathlib with strategy indexer.
    This actually cleans up a lot of the code as well.

--- run_strategy_indexer.py
+++ run_strategy_indexer.py
@@ -2,80 +2,45 @@
 A script to check that all strategy modules have been included in
 `./docs/reference/all_strategies.rst`
 """
-import glob
 import sys
-import os
+import pathlib
 
-def read_index(index_path: str) -> str:
-    """
-    Read the index of strategies
+default_index_path = pathlib.Path("./docs/reference/all_strategies.rst")
+excluded_modules = ("_strategies", "__init__", "_filters", "human")
 
-    Parameters
-    ----------
-    index_path : str
-        A file path for the index file where all strategies are auto documented
-
-    Returns
-    -------
-    strategies_reference : str
-        The string value of the contents of the index file.
-    """
-    with open(index_path, "r") as f:
-        strategies_reference = f.read()
-    return strategies_reference
-
-def get_module_name(module_path: str) -> str:
-    """
-    Take string of the form `./axelrod/strategies/titfortat.py` and returns
-    `titfortat`.
-
-    Parameters
-    ----------
-    module_path : str
-        A file path for a module file.
-
-    Returns
-    -------
-    module_name : str
-        The name of the module
-    """
-    filename = os.path.basename(module_path)
-    module_name = os.path.splitext(filename)[0]
-    return module_name
-
-def check_module(module_path: str,
-                 index_path: str="./docs/reference/all_strategies.rst",
-                 excluded: tuple=("_strategies", "__init__",
-                                  "_filters", "human")) -> bool:
+def check_module(module_path: pathlib.Path,
+                 index_path: pathlib.Path=default_index_path,
+                 excluded: tuple=excluded_modules) -> bool:
     """
     Check if a module name is written in the index of strategies.
 
     Parameters
     ----------
-    module_path : str
+    module_path :
         A file path for a module file.
-    index_path : str
+    index_path :
         A file path for the index file where all strategies are auto documented
-    excluded : tuple
+    excluded :
         A collection of module names to be ignored
 
     Returns
     -------
-    boolean : bool
+    boolean :
         True/False if module is referenced.
 
     """
-    strategies_index = read_index(index_path)
-    module_name = get_module_name(module_path)
+    strategies_index = index_path.read_text()
+    module_name = module_path.stem
     if module_name not in excluded and module_name not in strategies_index:
-            print("{} not in index".format(module_name))
-            return False
+        print("{} not in index".format(module_name))
+        return False
     return True
 
 
 if __name__ == "__main__":
 
-    modules = glob.glob("./axelrod/strategies/*.py")
+    p = pathlib.Path('.')
+    modules = p.glob("./axelrod/strategies/*.py")
     exit_codes = []
 
     for module_path in modules:
