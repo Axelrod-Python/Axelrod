cbc1bba590e068162e39b5bc8de2a35e535754cf
Gaëtan GOUSSEAUD <gousseaud.gaetan.pro@gmail.com>
2017-05-01 17:44:04 +0200

    Bush Mosteller(BM) player added
    Added Bush Mosteller test player and player

--- axelrod/strategies/_strategies.py
+++ axelrod/strategies/_strategies.py
@@ -10,6 +10,7 @@ from .axelrod_first import (
 from .axelrod_second import Champion, Eatherley, Tester, Gladstein, Tranquilizer, MoreGrofman
 from .backstabber import BackStabber, DoubleCrosser
 from .better_and_better import BetterAndBetter
+from .bm import BM
 from .calculator import Calculator
 from .cooperator import Cooperator, TrickyCooperator
 from .cycler import (
