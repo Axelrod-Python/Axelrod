2d0e2c63e3a6bf70f65b431c231c24443fd418b9
Vince Knight <vincent.knight@gmail.com>
2017-07-29 21:18:50 +0100

    Use defector and cooperator for test_selfsteem

--- axelrod/tests/strategies/test_selfsteem.py
+++ axelrod/tests/strategies/test_selfsteem.py
@@ -30,31 +30,31 @@ class TestSelfSteem(TestPlayer):
         # Check for f < -0.95, cooperate
         actions = [(D, C), (C, C), (D, C), (D, C),
                    (C, C), (D, C), (C, C), (C, C)]
-        self.versus_test(opponent=axelrod.MockPlayer(actions=[C]),
+        self.versus_test(opponent=axelrod.Cooperator(),
                          expected_actions=actions, seed=0)
 
         actions = [(D, D)] + [(D, D)] * 5 + [(D, D), (C, D), (C, D)]
-        self.versus_test(opponent=axelrod.MockPlayer(actions=[D]),
+        self.versus_test(opponent=axelrod.Defector(),
                          expected_actions=actions, seed=0)
 
         # Check for -0.3 < f < 0.3, random
         actions = [(D, C), (C, C), (D, C), (D, C),
                    (C, C), (D, C)] + [(C, C)] * 6 + [(D, C),(D, C)] + [(C, C)] * 7
-        self.versus_test(opponent=axelrod.MockPlayer(actions=[C]),
+        self.versus_test(opponent=axelrod.Cooperator(),
                          expected_actions=actions, seed=6)
 
         actions = [(D, D)] * 7 + [(C, D), (C, D)] + [(D, D)] * 8 + [(C, D),
                    (C, D), (D, D), (D, D), (D, D)]
-        self.versus_test(opponent=axelrod.MockPlayer(actions=[D]),
+        self.versus_test(opponent=axelrod.Defector(),
                          expected_actions=actions, seed=5)
 
         # Check for 0.95 > abs(f) > 0.3, follows TitForTat
         actions = [(D, D)] * 5 + [(C, D), (D, D), (C, D),
                    (C, D), (D, D), (C, D)] + [(D, D)] * 5
-        self.versus_test(opponent=axelrod.MockPlayer(actions=[D]),
+        self.versus_test(opponent=axelrod.Defector(),
                          expected_actions=actions)
 
         actions = [(D, C), (C, C), (D, C), (D, C),
                    (C, C), (D, C), (C, C), (C, C), (C, C), (C, C)]
-        self.versus_test(opponent=axelrod.MockPlayer(actions=[C]),
+        self.versus_test(opponent=axelrod.Cooperator(),
                          expected_actions=actions)
