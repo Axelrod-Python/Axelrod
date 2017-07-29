8704d527d9ba8790bd098c46c0b51b52db06871c
Vince Knight <vincent.knight@gmail.com>
2017-07-29 19:20:54 +0100

    Fix test in selfsteem.
    It is a random strategy, that will act randomly if `-1<f<1`, `f` is
    calculated as the sin(...), at the start of that game, it is sin(0)
    which means it acts randomly from the start.

--- axelrod/tests/strategies/test_selfsteem.py
+++ axelrod/tests/strategies/test_selfsteem.py
@@ -22,36 +22,39 @@ class TestSelfSteem(TestPlayer):
     def test_strategy(self):
 
         # Check for f > 0.95, defect
-        actions = [(C, C), (C, C), (D, C), (D, C)]
-        self.versus_test(axelrod.Cooperator(), expected_actions=actions)
-
         actions = [(C, C), (C, C), (D, C),
-                   (D, C), (C, C), (D, C)] + [(C, C)] * 6 + [(D, C)]
-        self.versus_test(axelrod.Cooperator(), expected_actions=actions)
+                   (D, C), (C, C), (D, C)] + [(C, C)] * 4 + [(D, C)]
+        self.versus_test(axelrod.Cooperator(), expected_actions=actions,
+                         seed=1)
 
         # Check for f < -0.95, cooperate
-        actions = [(D, C), (C, C), (D, C),(D, C),
+        actions = [(D, C), (C, C), (D, C), (D, C),
                    (C, C), (D, C), (C, C), (C, C)]
-        self.versus_test(opponent=axelrod.MockPlayer(actions=[C]), expected_actions=actions)
+        self.versus_test(opponent=axelrod.MockPlayer(actions=[C]),
+                         expected_actions=actions, seed=0)
 
-        actions = [(C, D)] + [(D, D)] * 6 + [(C, D),
-                   (C, D)] + [(D, D)] * 6 + [(C, D)]
-        self.versus_test(opponent=axelrod.MockPlayer(actions=[D]), expected_actions=actions)
+        actions = [(D, D)] + [(D, D)] * 5 + [(D, D), (C, D), (C, D)]
+        self.versus_test(opponent=axelrod.MockPlayer(actions=[D]),
+                         expected_actions=actions, seed=0)
 
         # Check for -0.3 < f < 0.3, random
         actions = [(D, C), (C, C), (D, C), (D, C),
                    (C, C), (D, C)] + [(C, C)] * 6 + [(D, C),(D, C)] + [(C, C)] * 7
-        self.versus_test(opponent=axelrod.MockPlayer(actions=[C]), expected_actions=actions, seed=6)
+        self.versus_test(opponent=axelrod.MockPlayer(actions=[C]),
+                         expected_actions=actions, seed=6)
 
         actions = [(D, D)] * 7 + [(C, D), (C, D)] + [(D, D)] * 8 + [(C, D),
                    (C, D), (D, D), (D, D), (D, D)]
-        self.versus_test(opponent=axelrod.MockPlayer(actions=[D]), expected_actions=actions, seed=5)
+        self.versus_test(opponent=axelrod.MockPlayer(actions=[D]),
+                         expected_actions=actions, seed=5)
 
         # Check for 0.95 > abs(f) > 0.3, follows TitForTat
         actions = [(D, D)] * 5 + [(C, D), (D, D), (C, D),
                    (C, D), (D, D), (C, D)] + [(D, D)] * 5
-        self.versus_test(opponent=axelrod.MockPlayer(actions=[D]), expected_actions=actions)
+        self.versus_test(opponent=axelrod.MockPlayer(actions=[D]),
+                         expected_actions=actions)
 
         actions = [(D, C), (C, C), (D, C), (D, C),
                    (C, C), (D, C), (C, C), (C, C), (C, C), (C, C)]
-        self.versus_test(opponent=axelrod.MockPlayer(actions=[C]), expected_actions=actions)
+        self.versus_test(opponent=axelrod.MockPlayer(actions=[C]),
+                         expected_actions=actions)
