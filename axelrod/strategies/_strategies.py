32d406043f75343315b0b19db6d9cc759cd6ff69
Anurag Sinha <anuragsinhab@gmail.com>
2017-10-29 12:34:35 +0530

    Added Tricky level punisher strategy.

--- axelrod/strategies/_strategies.py
+++ axelrod/strategies/_strategies.py
@@ -65,7 +65,7 @@ from .negation import Negation
 from .oncebitten import OnceBitten, FoolMeOnce, ForgetfulFoolMeOnce, FoolMeForever
 from .prober import (CollectiveStrategy, Prober, Prober2, Prober3, Prober4,
                      HardProber, NaiveProber, RemorsefulProber)
-from .punisher import Punisher, InversePunisher, LevelPunisher
+from .punisher import Punisher, InversePunisher, LevelPunisher, TrickyLevelPunisher
 from .qlearner import (
     RiskyQLearner, ArrogantQLearner, HesitantQLearner, CautiousQLearner)
 from .rand import Random
@@ -279,6 +279,7 @@ all_strategies = [
     Tranquilizer,
     TrickyCooperator,
     TrickyDefector,
+    TrickyLevelPunisher
     Tullock,
     TwoTitsForTat,
     VeryBad,
