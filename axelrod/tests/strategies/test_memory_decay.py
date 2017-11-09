e05efafd34b47fb7653f5de9432d64b904f63e47
Denis <denis@bilogora.lan>
2017-11-09 13:21:26 +0100

    added mem_decay to meta.py, expanded tests

--- axelrod/tests/strategies/test_memory_decay.py
+++ axelrod/tests/strategies/test_memory_decay.py
@@ -1,15 +1,14 @@
 """Tests for the Memory Decay strategy"""
 
-from inspect import getargspec
-import axelrod
+import axelrod as axe
 from axelrod.tests.strategies.test_player import TestPlayer
 
-C, D = axelrod.Action.C, axelrod.Action.D
+C, D = axe.Action.C, axe.Action.D
 
 class TestMemoryDecay(TestPlayer):
 
     name = 'MemoryDecay'
-    player = axelrod.MemoryDecay
+    player = axe.meta.MemoryDecay
     expected_classifier = {
         'memory_depth': float('inf'),
         'stochastic': True,
@@ -21,14 +20,53 @@ class TestMemoryDecay(TestPlayer):
     }
 
     def test_strategy(self):
-        """Test TitForTat behavior in first 15 turns"""
-        opponent = axelrod.MockPlayer(actions = [C, C])
-        actions = [(C, C), (C, C), (C, C), (C, C)]
+        #Test TitForTat behavior in first 15 turns
+        opponent = axe.Cooperator()
+        actions = list([(C, C)]) * 15
         self.versus_test(opponent, expected_actions = actions)
 
-        opponent = axelrod.MockPlayer(actions = [D, D])
-        actions = [(C, D), (D, D), (D, D), (D, D)]
+        opponent = axe.Defector()
+        actions = [(C, D)] + list([(D, D)]) * 14
         self.versus_test(opponent, expected_actions = actions)
 
-t = TestMemoryDecay()
-t.test_strategy()
+        opponent = axe.Alternator()
+        actions = [(C, C)] + [(C, D), (D, C)] * 7
+        self.versus_test(opponent, expected_actions = actions)
+
+        opponent_actions = [C, D, D, C, D, C, C, D, C, D, D, C, C, D, D]
+        opponent = axe.MockPlayer(actions =opponent_actions)
+        mem_actions = [C, C, D, D, C, D, C, C, D, C, D, D, C, C, D]
+        actions = list(zip(mem_actions, opponent_actions))
+        self.versus_test(opponent, expected_actions = actions)
+
+        opponent = axe.Random()
+        actions = [(C, D), (D, D), (D, C), (C, C), (C, D), (D, C)]
+        self.versus_test(opponent, expected_actions = actions, seed = 0)
+
+        #Test alternative starting strategies
+        opponent = axe.Cooperator()
+        actions = list([(D, C)]) * 15
+        self.versus_test(opponent, expected_actions = actions,
+                         init_kwargs = {'start_strategy' : 'Defector'})
+
+        opponent = axe.Cooperator()
+        actions = list([(C, C)]) * 15
+        self.versus_test(opponent, expected_actions = actions,
+                         init_kwargs = {'start_strategy' : 'Cooperator'})
+
+        opponent = axe.Cooperator()
+        actions = [(C, C)] + list([(D, C), (C, C)]) * 7
+        self.versus_test(opponent, expected_actions = actions,
+                         init_kwargs = {'start_strategy' : 'Alternator'})
+
+        #Test net-cooperation-score (NCS) based decisions in subsequent turns
+        opponent = axe.Cooperator()
+        actions = [(C, C)] * 15 + [(C, C)]
+        self.versus_test(opponent, expected_actions = actions, seed = 1,
+                         init_kwargs = {'memory' : [D] * 5 + [C] * 10})
+
+        # nešto ne valja
+        opponent = axe.Cooperator()
+        actions = [(C, C)] * 15 + [(D, C)]
+        self.versus_test(opponent, expected_actions = actions, seed = 1,
+                         init_kwargs = {'memory' : [D] * 6 + [C] * 9})
