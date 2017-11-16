5374c0a5e8a391cbb7b2917da98279411276dfed
gaffney2010 <gaffney.tj@gmail.com>
2017-11-16 05:26:37 -0500

    Added references to Borufsen

--- axelrod/strategies/_strategies.py
+++ axelrod/strategies/_strategies.py
@@ -7,7 +7,9 @@ from .averagecopier import AverageCopier, NiceAverageCopier
 from .axelrod_first import (
     Davis, RevisedDowning, Feld, Grofman, Nydegger, Joss, Shubik, Tullock,
     UnnamedStrategy, SteinAndRapoport, TidemanAndChieruzzi)
-from .axelrod_second import Champion, Eatherley, Tester, Gladstein, Tranquilizer, MoreGrofman, Kluepfel
+from .axelrod_second import (
+    Champion, Eatherley, Tester, Gladstein, Tranquilizer, MoreGrofman,
+    Kluepfel, Borufsen)
 from .backstabber import BackStabber, DoubleCrosser
 from .better_and_better import BetterAndBetter
 from .bush_mosteller import BushMosteller
@@ -105,6 +107,7 @@ all_strategies = [
     AverageCopier,
     BetterAndBetter,
     BackStabber,
+    Borufsen,
     Bully,
     BushMosteller,
     Calculator,
