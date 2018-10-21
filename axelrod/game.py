36aa63f00e20e1f4550a2305223b59ef6be752b3
Karol M. Langner <karol.langner@gmail.com>
2018-10-20 18:00:04 -0700

    Spruce up game and its tests

--- axelrod/game.py
+++ axelrod/game.py
@@ -1,6 +1,6 @@
 from typing import Tuple, Union
 
-from .action import Action
+from axelrod import Action
 
 C, D = Action.C, Action.D
 
@@ -8,14 +8,36 @@ Score = Union[int, float]
 
 
 class Game(object):
-    """A class to hold the game matrix and to score a game accordingly."""
+    """Container for the game matrix and scoring logic.
+
+    Attributes
+    ----------
+    scores: dict
+        The numerical score attribute to all combinations of action pairs.
+    """
 
     def __init__(self, r: Score = 3, s: Score = 0, t: Score = 5, p: Score = 1) -> None:
-        self.scores = {(C, C): (r, r), (D, D): (p, p), (C, D): (s, t), (D, C): (t, s)}
+        """Create a new game object.
+        
+        Parameters
+        ----------
+        r: int or float
+            Score obtained by both players for mutual cooperation.
+        s: int or float
+            Score obtained by a player for cooperating against a defector.
+        t: int or float
+            Score obtained by a player for defecting against a cooperator.
+        p: int or float
+            Score obtained by both player for mutual defection.
+        """
+        self.scores = {
+                (C, C): (r, r),
+                (D, D): (p, p),
+                (C, D): (s, t),
+                (D, C): (t, s)}
 
     def RPST(self) -> Tuple[Score, Score, Score, Score]:
-        """Return the values in the game matrix in the Press and Dyson
-        notation."""
+        """Returns game matrix values in Press and Dyson notation."""
         R = self.scores[(C, C)][0]
         P = self.scores[(D, D)][0]
         S = self.scores[(C, D)][0]
@@ -23,11 +45,17 @@ class Game(object):
         return (R, P, S, T)
 
     def score(self, pair: Tuple[Action, Action]) -> Tuple[Score, Score]:
-        """Return the appropriate score for decision pair.
+        """Returns the appropriate score for a decision pair.
 
-        Returns the appropriate score (as a tuple) from the scores dictionary
-        for a given pair of plays (passed in as a tuple).
-        e.g. score((C, C)) returns (2, 2)
+        Parameters
+        ----------
+        pair: tuple(Action, Action)
+            A pair actions for two players, for example (C, C).
+
+        Returns
+        -------
+        tuple of int or float
+            Scores for two player resulting from their actions.
         """
         return self.scores[pair]
 
