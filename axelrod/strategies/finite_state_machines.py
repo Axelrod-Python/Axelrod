0822b5c38f5eb853b125409883eae15aa1cc0ade
gaffney2010 <gaffneytj@google.com>
2019-01-06 05:59:55 -0800

    Added a get_memory_from_transitions function for FSM.

--- axelrod/strategies/finite_state_machines.py
+++ axelrod/strategies/finite_state_machines.py
@@ -1,7 +1,345 @@
 from axelrod.action import Action
 from axelrod.player import Player
+from collections import defaultdict, namedtuple
 
 C, D = Action.C, Action.D
+
+
+def get_accessible_transitions(transitions, initial_state):
+  """Gets all transitions from the list that can be reached from the
+  initial_state.
+  """
+  edge_dict = defaultdict(list)
+  visited = dict()
+  for trans in transitions:
+      visited[trans[0]] = False
+      edge_dict[trans[0]].append(trans[2])
+  accessible_edges = [initial_state]
+
+  edge_queue = [initial_state]
+  visited[initial_state] = True
+  while len(edge_queue) > 0:
+      edge = edge_queue.pop()
+      for next_edge in edge_dict[edge]:
+          if not visited[next_edge]:
+              visited[next_edge] = True
+              edge_queue.append(next_edge)
+              accessible_edges.append(next_edge)
+
+  accessible_transitions = list()
+  for trans in transitions:
+      if trans[0] in accessible_edges:
+          accessible_transitions.append(trans)
+
+  return accessible_transitions
+
+
+def get_memory_from_transitions(transitions, initial_state=None,
+                                print_trace=True):
+    """
+    This function calculates the memory of an FSM from the transitions.
+
+    Assume that transitions are a list with entries like
+     ((state, last_opponent_action, next_state, next_action), ...)
+
+    We look at all the next_actions for all the transitions.  If these aren't
+    all the same, then we attach 1 turn worth of memory (this strategy's
+    previous action and the opponent's previous action).  We can get the
+    opponent's previous strategy from the transition, but to get this strategy's
+    previous action, we need to consider all incoming transitions into the
+    current state.  [We call this walking backwards through the graph along the
+    path given by the incoming transition.]  There may be zero or one or
+    multiple incoming transitions into each state, creating multiple paths we
+    could walk along.  We call these branches, and keep track of all branches.
+
+    Along each branch there is a chain of actions.  After 1 step, it may be CC,
+    CD, DC, or DD.  [However we write "CC" like "_/C, C/_" though to establish
+    that letters to the left of the "/" are the opponent's moves, while the
+    letters to the right are this strategy's moves.]  For each chain of actions,
+    we gather the branches that match that chain.  If all these branches have
+    the same next_action, then we know what to do following that chain of
+    actions.  In that case we call these branches decided.  With undecided
+    branches, we continue to walk back.  We repeat until all branches are
+    decided.  The number of steps that this takes is the memory of the FSM.
+
+    If however, there are still undecided branches after E*(E-1) steps (where E
+    is the number of transitions), then the memory must be infinite.  This is
+    shown elsewhere.
+
+
+    As an example, we show how the Fortress3 (defined below) strategy would
+    work.
+
+    Fortress3 is given by the transitions:
+    transitions = (
+        (1, C, 1, D),
+        (1, D, 2, D),
+        (2, C, 1, D),
+        (2, D, 3, C),
+        (3, C, 3, C),
+        (3, D, 1, D),
+    )
+
+    In the first step, we just check transitions' next-actions.
+    We list transtions as state:prev_opponent_action;next_action:
+    1:C;D | Back-trace: _ | On-state: 1
+    1:D;D | Back-trace: _ | On-state: 1
+    2:C;D | Back-trace: _ | On-state: 2
+    2:D;C | Back-trace: _ | On-state: 2
+    3:C;C | Back-trace: _ | On-state: 3
+    3:D;D | Back-trace: _ | On-state: 3
+
+    In the second step, we walk backwards along incoming transitions.
+    We continue to label each branch by its ending
+    state:previous_opponent_action;next_action, but we list the state that
+    we're on at this point in time:
+    1:C;D | Back-trace: _/D, C/_ | On-state: 1
+    1:C;D | Back-trace: _/D, C/_ | On-state: 2
+    1:C;D | Back-trace: _/D, C/_ | On-state: 3
+    2:C;D | Back-trace: _/D, C/_ | On-state: 1
+    1:D;D | Back-trace: _/D, D/_ | On-state: 1
+    1:D;D | Back-trace: _/D, D/_ | On-state: 2
+    1:D;D | Back-trace: _/D, D/_ | On-state: 3
+    2:D;C | Back-trace: _/D, D/_ | On-state: 1
+    3:C;C | Back-trace: _/C, C/_ | On-state: 2
+    3:C;C | Back-trace: _/C, C/_ | On-state: 3
+    3:D;D | Back-trace: _/C, D/_ | On-state: 2
+    3:D;D | Back-trace: _/C, D/_ | On-state: 3
+
+    From here we can conclude that:
+    If _/D, C/_, then D
+    If _/C, C/_, then C
+    If _/C, D/_, then D
+
+    We remove the branches that correspond to those action chains.  But we
+    continue to walk back the _/D, D/_ branches:
+    1:D;D | Back-trace: _/D, C/D, D/_ | On-state: 1
+    1:D;D | Back-trace: _/D, C/D, D/_ | On-state: 2
+    1:D;D | Back-trace: _/D, C/D, D/_ | On-state: 3
+    1:D;D | Back-trace: _/D, C/D, D/_ | On-state: 1
+    1:D;D | Back-trace: _/C, D/D, D/_ | On-state: 2
+    1:D;D | Back-trace: _/C, D/D, D/_ | On-state: 3
+    2:D;C | Back-trace: _/D, D/D, D/_ | On-state: 1
+    2:D;C | Back-trace: _/D, D/D, D/_ | On-state: 2
+    2:D;C | Back-trace: _/D, D/D, D/_ | On-state: 3
+
+    From here we conclude that:
+    If _/D, C/D, D/_, then D
+    If _/C, D/D, D/_, then D
+    If _/D, D/D, D/_, then C
+
+    There are no more undecided branches, so we stop and say that the memory
+    is 2.
+    """
+    # If initial_state is set, use this to determine which transitions are
+    # reachable from the initial_state and restrict to those.
+    if initial_state is not None:
+        transitions = get_accessible_transitions(transitions, initial_state)
+
+    # First make a back_transitions dict from transitions.  This is keyed on
+    # states, and a list of "BackTrans" (one for each transition incoming to
+    # that state) as values.
+    back_transitions = defaultdict(list)
+    # A "BackTrans" has the previous state and previous action/reaction pair.
+    BackTrans = namedtuple("BackTrans", ["prev_state", "prev_reaction",
+                                         "prev_opp_action"])
+    for trans in transitions:
+        state = trans[0]
+        last_opponent_action = trans[1]
+        next_state = trans[2]
+        next_action = trans[3]
+
+        back_transitions[next_state].append(BackTrans(state,
+                                                      next_action,
+                                                      last_opponent_action))
+
+    class ActionChain(object):
+        """A list of actions.  Made a class so that we can hash."""
+        def __init__(self, initial_list=None):
+            if initial_list is None:
+                initial_list = list()
+            self.actions = initial_list[:]
+
+        def __eq__(self, other):
+            return self.actions == other.actions
+
+        def __repr__(self):
+            """
+            This is a way to represent a memory of a certain length.  We
+            represent history as a opponent_action/this_player_reaction
+            seperated by commas, with the most recent pair listed last.
+
+            Because knowing the left half of the _/_ action-reaction requires
+            more memory than knowing the right half, we will have a blank on the
+            oldest pair.
+            """
+            if len(self.actions) == 0:
+                return "_"
+
+            # The first action on the list will be the opponent's previous
+            # action.  We don't know yet how we will respond, so we leave a
+            # blank (_).
+            action_str = "{}/_".format(self.actions[0])
+            # Then we go backwards.  The next actions on the list are the
+            # opponent's previous actions, our previous actions, alternatively.
+            i = 1
+            while i < len(self.actions)-2:
+                action_str = "{}/{}, {}".format(self.actions[i+1],
+                                                self.actions[i], action_str)
+                i += 2
+            # The oldest action we'll have will be our response to an unknown
+            # opponent action.
+            action_str = "_/{}, {}".format(self.actions[-1], action_str)
+
+            return action_str
+
+        def __hash__(self):
+            return hash(repr(self))
+
+        def append(self, action):
+            self.actions.append(action)
+
+    class Branch(object):
+        """A chain of previous actions.  With other information captured, like
+        state, so that we can continue to walk backwards.
+        """
+        def __init__(self, trans=None):
+            if trans is None:
+                return
+
+            state = trans[0]
+            last_opponent_action = trans[1]
+            next_state = trans[2]
+            next_action = trans[3]
+
+            self.num_moves_recorded = 0
+            self.action_chain = ActionChain([])
+            self.next_action = next_action
+            self.on_state = state
+            # The information that we have available at any step will be half of
+            # next step's history.  So we keep this in a buffer.
+            self.buffer = last_opponent_action
+
+            # For debugging
+            self.initial_trans = "{}:{}".format(state, last_opponent_action)
+
+        def step(self, backtrans):
+            """Continues to walk (or branch) backwards from where the branch
+            leaves off, given a path (backtrans) to walk backwards along.  This
+            will return a Branch instance.
+            """
+            new_branch = Branch()
+            new_branch.num_moves_recorded = self.num_moves_recorded + 1
+            new_branch.action_chain = ActionChain(self.action_chain.actions)
+            new_branch.action_chain.append(self.buffer)
+            new_branch.action_chain.append(backtrans.prev_reaction)
+            new_branch.next_action = self.next_action
+            new_branch.on_state = backtrans.prev_state
+            # This needs one more memory to know.
+            new_branch.buffer = backtrans.prev_opp_action
+
+            new_branch.initial_trans = self.initial_trans
+
+            return new_branch
+
+        def debug_str(self):
+            return "{};{} | Back-trace: {} | On-state: {}".format(
+                    self.initial_trans, self.next_action,
+                    repr(self.action_chain), self.on_state)
+
+    BranchList = namedtuple("BranchList", ("branch_list", "next_actions"))
+
+    class BranchPool(object):
+        """We keep branches in the branch_pool, grouped by common-end
+        ActionChains.  A common-end ActionChain is a chain of N actions
+        occurring most-recently that is common to all branches in the group.
+        Specifically branch_pool is a dict with keys given by common-end
+        ActionChains, and with dict-values given by a list of branches and the
+        set of possible next_actions for these branches.
+
+        The set of possible next_actions is the set of actions that this FSM may
+        choose to do following the chain of actions given in the key.  When
+        there is a single action, we know that the strategy will make that
+        action; we call the branches with that chain of actions "decided" at
+        this point.
+        """
+        def __init__(self):
+            self.clear()
+
+        def push(self, branch):
+            """Just adds a branch to the branch_pool."""
+            common_branches = self.branch_pool[branch.action_chain]
+            common_branches.next_actions.add(branch.next_action)
+            common_branches.branch_list.append(branch)
+
+        def branches(self):
+            """An iterator that loops through all the branches in the
+            branch_pool.
+            """
+            for k, v in self.branch_pool.items():
+                for branch in v.branch_list:
+                    yield branch
+
+        def clear(self):
+            """Empty the branch_pool."""
+            self.branch_pool = defaultdict(lambda: BranchList(list(), set()))
+
+        def remove_decided_branches(self):
+            """We call a branch "decided" if all branches with that common-end
+            (end of ActionChain) give the same next_action.  This function
+            removes those from the branch_pool, and returns these as a dict
+            keyed by common-end ActionChain, and with dict-values given by the
+            common next_action.
+            """
+            decided_branches = dict()
+            for k, v in self.branch_pool.items():
+                if len(v.next_actions) == 1:
+                  decided_branches[k] = list(v.next_actions)[0]
+            for k in decided_branches.keys():
+                del self.branch_pool[k]
+            return decided_branches
+
+        def __bool__(self):
+            return len(self.branch_pool) > 0
+
+    # Set up variables
+    num_edges = len(transitions)
+    waiting, processed = BranchPool(), BranchPool()
+    if print_trace:
+        print("STEP 0")
+        print("===============")
+    for trans in transitions:
+        trans_branch = Branch(trans)
+        processed.push(trans_branch)
+        if print_trace:
+            print(trans_branch.debug_str())
+    processed.remove_decided_branches()
+
+    steps = 0
+    while processed:
+        steps += 1
+        if print_trace:
+            print("STEP {}".format(steps))
+            print("===============")
+        if steps > num_edges*(num_edges-1):
+            return float("inf")
+        # Move processed to waiting
+        for branch in processed.branches():
+            waiting.push(branch)
+        processed.clear()
+        # Now process the waiting list.
+        for branch in waiting.branches():
+            for backtrans in back_transitions[branch.on_state]:
+                processed.push(branch.step(backtrans))
+        if print_trace:
+          for branch in processed.branches():
+                print(branch.debug_str())
+        waiting.clear()
+        # And remove decided branches.
+        decided_branches = processed.remove_decided_branches()
+        for k, v in decided_branches.items():
+            print("If {}, then {}".format(k, v))
+    return steps
 
 
 class SimpleFSM(object):
--- axelrod/tests/strategies/test_finite_state_machines.py
+++ axelrod/tests/strategies/test_finite_state_machines.py
@@ -2,11 +2,211 @@
 import unittest
 
 import axelrod
-from axelrod.strategies.finite_state_machines import SimpleFSM
+from axelrod.strategies.finite_state_machines import SimpleFSM, get_memory_from_transitions
 
 from .test_player import TestPlayer
 
 C, D = axelrod.Action.C, axelrod.Action.D
+
+
+class TestGetMemoryFromTransitions(unittest.TestCase):
+    def test_cooperator(self):
+        transitions = (
+            (0, C, 0, C),
+            (0, D, 0, C)
+        )
+
+        self.assertEqual(get_memory_from_transitions(transitions), 0)
+
+    def test_tit_for_tat(self):
+        transitions = (
+            (0, C, 0, C),
+            (0, D, 0, D)
+        )
+
+        self.assertEqual(get_memory_from_transitions(transitions), 1)
+
+    def test_two_state_memory_two(self):
+        """If all D lead to state 0 and all C lead to state 1.  We make it so
+        that all paths out of state 0 plays Cooperator and state 1 plays
+        Defector.
+
+        In this case, we must know what state we're in to know how to respond to
+        the opponent's previou action, but we cannot determine from our own
+        previous action; we must look at opponent's action from two turns ago.
+        """
+        transitions = (
+            (0, C, 0, C),
+            (0, D, 1, C),
+            (1, C, 0, D),
+            (1, D, 1, D)
+        )
+
+        self.assertEqual(get_memory_from_transitions(transitions), 2)
+
+    def test_two_state_tft(self):
+        """Same case as above, but this time our own last action tells us which
+        state we're in.  In fact, this strategy is exactly TFT.
+        """
+        transitions = (
+            (0, C, 0, C),
+            (0, D, 1, D),
+            (1, C, 0, C),
+            (1, D, 1, D)
+        )
+
+        self.assertEqual(get_memory_from_transitions(transitions), 1)
+
+    def test_two_state_inf_memory(self):
+        """A C will cause the FSM to stay in the same state, and D causes to
+        change states.  Will always respond to a C with a C.   Will respond to a
+        D with a C in state 0, but with a D in state 1.
+
+        So we need to know the state to know how to respond to a D.  But since
+        an arbitarily long sequence of C/C may occur, we need infinite memory.
+        """
+        transitions = (
+            (0, C, 0, C),
+            (0, D, 1, C),
+            (1, C, 1, C),
+            (1, D, 0, D)
+        )
+
+        self.assertEqual(get_memory_from_transitions(transitions), float("inf"))
+
+    def test_four_state_memory_two(self):
+        """Same as the two_state_memory_two test above, but we use two copies,
+        stitched together.
+        """
+        transitions = (
+            (0, C, 0, C),
+            (0, D, 1, C),
+            (1, C, 2, D),
+            (1, D, 1, D),
+            (2, C, 2, C),
+            (2, D, 3, C),
+            (3, C, 0, D),
+            (3, D, 3, D)
+        )
+
+        self.assertEqual(get_memory_from_transitions(transitions), 2)
+
+    def test_fortress_3(self):
+        """Tests Fortress-3, which Defects unless the opponent D twice in a row.
+        In that case C, and continue to C for as long as the opponent does.
+
+        We know we're in state 3 if our own previous move was a C.  Otherwise, C
+        if and only if the opponent's previous two moves were D.  [Unless we
+        were in state 3 last turn, in which case we would have C'd two turns
+        ago.]
+
+        So the memory should be 2.
+        """
+        transitions = (
+            (1, C, 1, D),
+            (1, D, 2, D),
+            (2, C, 1, D),
+            (2, D, 3, C),
+            (3, C, 3, C),
+            (3, D, 1, D),
+        )
+
+        self.assertEqual(get_memory_from_transitions(transitions), 2)
+
+    def test_complex_cooperator(self):
+        """Tests a cooperator with lots of states and transitions.
+        """
+        transitions = (
+            (0, C, 0, C),
+            (0, D, 1, C),
+            (1, C, 2, C),
+            (1, D, 3, C),
+            (2, C, 4, C),
+            (2, D, 3, C),
+            (3, C, 5, C),
+            (3, D, 4, C),
+            (4, C, 2, C),
+            (4, D, 6, C),
+            (5, C, 7, C),
+            (5, D, 3, C),
+            (6, C, 7, C),
+            (6, D, 7, C),
+            (7, C, 8, C),
+            (7, D, 7, C),
+            (8, C, 8, C),
+            (8, D, 6, C)
+        )
+
+        self.assertEqual(get_memory_from_transitions(transitions), 0)
+
+    def test_disconnected_graph(self):
+        """Test two disjoint versions of Fortress3, with initial_state."""
+        transitions = (
+            (1, C, 1, D),
+            (1, D, 2, D),
+            (2, C, 1, D),
+            (2, D, 3, C),
+            (3, C, 3, C),
+            (3, D, 1, D),
+            (4, C, 4, D),
+            (4, D, 5, D),
+            (5, C, 4, D),
+            (5, D, 6, C),
+            (6, C, 6, C),
+            (6, D, 4, D)
+        )
+
+        self.assertEqual(get_memory_from_transitions(transitions,
+                                                     initial_state=1), 2)
+
+    def test_transient_state(self):
+        """Test a setup where we a transient state (no incoming transitions)
+        goes into a Fortress3 (and D) if the opponent D, and goes into a
+        Cooperator if the opponent C.
+
+        The transient state is state 0.  Fortress3 starts at state 1.  And
+        the Cooperator is state 4.
+        """
+        transitions = (
+            (0, C, 4, C),
+            (0, D, 1, D),
+            (1, C, 1, D),
+            (1, D, 2, D),
+            (2, C, 1, D),
+            (2, D, 3, C),
+            (3, C, 3, C),
+            (3, D, 1, D),
+            (4, C, 4, C),
+            (4, D, 4, C)
+        )
+
+        # If starting in state 4, then treat like Cooperator
+        self.assertEqual(get_memory_from_transitions(transitions,
+                                                     initial_state=4), 0)
+        # Start in state 1, then a Fortress3.
+        self.assertEqual(get_memory_from_transitions(transitions,
+                                                     initial_state=1), 2)
+
+    def test_infinite_memory_transient_state(self):
+        """A transient state at 0, which goes into either a Cooperator or a TFT.
+        Because an arbitrarily-long chain of C/C may exist, we would need a
+        infinite memory to determine which state we're in, so that we know how
+        to respond to a D.
+        """
+        transitions = (
+            (0, C, 1, C),
+            (0, D, 2, D),
+            (1, C, 1, C),
+            (1, D, 1, C),
+            (2, C, 2, C),
+            (2, D, 2, D)
+        )
+
+        self.assertEqual(get_memory_from_transitions(
+                transitions, initial_state=0), float("inf"))
+
+        self.assertEqual(get_memory_from_transitions(transitions,
+                                                     initial_state=2), 1)
 
 
 class TestSimpleFSM(unittest.TestCase):
