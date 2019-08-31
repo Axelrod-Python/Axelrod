2b597f2c44ccad50411acbf6051a18e085699f4d
Marc Harper <marc.harper@gmail.com>
2019-08-30 21:44:09 -0700

    Add particle swarm methods to Player class

--- axelrod/strategies/finite_state_machines.py
+++ axelrod/strategies/finite_state_machines.py
@@ -1,3 +1,4 @@
+import itertools
 import random
 from random import randrange, choice
 
@@ -201,6 +202,42 @@ class FSMPlayer(Player):
             initial_action=self.initial_action,
             mutation_probability=self.mutation_probability
         )
+
+    def receive_vector(self, vector):
+        """
+        Read a serialized vector into the set of FSM parameters (less initial
+        state).  Then assign those FSM parameters to this class instance.
+
+        The vector has three parts. The first is used to define the next state
+        (for each of the player's states - for each opponents action).
+
+        The second part is the player's next moves (for each state - for
+        each opponent's actions).
+
+        Finally, a probability to determine the player's first move.
+        """
+        state_scale = vector[:self.num_states * 2]
+        next_states = [int(s * (self.num_states - 1)) for s in state_scale]
+        actions = vector[self.num_states * 2: -1]
+
+        self.initial_action = C if round(vector[-1]) == 0 else D
+        self.initial_state = 1
+
+        transitions = []
+        for i, (initial_state, action) in enumerate(
+            itertools.product(range(self.num_states), [C, D])):
+            next_action = C if round(actions[i]) == 0 else D
+            transitions.append([initial_state, action, next_states[i], next_action])
+        self.fsm = SimpleFSM(transitions, self.initial_state)
+
+    def create_vector_bounds(self):
+        """Creates the bounds for the decision variables."""
+        size = len(self.fsm.transitions) * 2 + 1
+
+        lb = [0] * size
+        ub = [1] * size
+
+        return lb, ub
 
 
 class Fortress3(FSMPlayer):
