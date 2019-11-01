4a570ca9e1641454f27948bce321c3e941d4b676
Inviro <prelusiondilemma@gmail.com>
2019-11-01 00:19:05 -0700

    Added documentation to the doctests.py
    Added Numpy Style documentation to the file, and a small side-note about the ELLIPSIS option from doctest online documentation.

--- doctests.py
+++ doctests.py
@@ -6,11 +6,27 @@ import warnings
 
 # Note loader and ignore are required arguments for unittest even if unused.
 def load_tests(loader, tests, ignore):
+    """
+        Locates and returns a collection of unittests in a TestSuite object
+        Parameters
+        ----------
+        loader :
+            A required but unused parameter.
+        tests :
+            A unittest TestSuite object for collecting the needed test cases.
+        ignore :
+            A required but unused parameter.
+        Returns
+        -------
+        tests :
+            A unittest TestSuite object that holds test cases.
+    """
     for root, dirs, files in os.walk("."):
         for f in files:
             if f.endswith(".rst"):
                 tests.addTests(
                     doctest.DocFileSuite(
+                        # ELLIPSIS option tells doctest to ignore portions of the verification value.
                         os.path.join(root, f), optionflags=doctest.ELLIPSIS
                     )
                 )
