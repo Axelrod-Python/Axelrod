374627a9ad64ca8c9f89a1f9e5b2eee1ec2327d6
T.J. Gaffney <gaffney.tj@gmail.com>
2020-03-22 03:41:18 -0700

    Add template type and fix "def" -> "class"

--- axelrod/classifier.py
+++ axelrod/classifier.py
@@ -1,27 +1,31 @@
-from typing import Any, Callable, Text
+from typing import Any, Callable, Generic, Optional, Set, Text, TypeVar, Union
 
 
-# TJ: Template the value type
-def Classifier(object):
-    def __init__(self, name: Text, f: Callable[['Player'], Any]):
+T = TypeVar('T')
+
+class Classifier(Generic[T]):
+    def __init__(self, name: Text, f: Callable[['Player'], T]):
         self.name = name
         self.f = f
 
     # TJ: Who depends on whom, re: Player
-    def calc_for_player(self, player: 'Player'):
+    def calc_for_player(self, player: 'Player') -> T:
         if self.name in player.classifier:
             return player.classifier[self.name]
 
         return self.f(player)
 
 
-stochastic = Classifier("stochastic", lambda _: False)
-memory_depth = Classifier("memory_depth", lambda _: float("inf"))
-makes_use_of = Classifier("makes_use_of", lambda _: None)
-long_run_time = Classifier("long_run_time", lambda _: False)
-inspects_source = Classifier("inspects_source", lambda _: None)
-manipulates_source = Classifier("manipulates_source", lambda _: None)
-manipulates_state = Classifier("manipulates_state", lambda _: None)
+stochastic = Classifier[bool]("stochastic", lambda _: False)
+memory_depth = Classifier[Union[float, int]]("memory_depth",
+                                             lambda _: float("inf"))
+makes_use_of = Classifier[Optional[Set[Text]]]("makes_use_of", lambda _: None)
+long_run_time = Classifier[bool]("long_run_time", lambda _: False)
+inspects_source = Classifier[Optional[bool]]("inspects_source", lambda _: None)
+manipulates_source = Classifier[Optional[bool]]("manipulates_source",
+                                                lambda _: None)
+manipulates_state = Classifier[Optional[bool]]("manipulates_state",
+                                               lambda _: None)
 
 all_classifiers = [
     stochastic,
