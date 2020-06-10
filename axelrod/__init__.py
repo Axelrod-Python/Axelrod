83e6c541d495e440efff5639b09c8b4608835193
akash-suresh <akashsuresh36@gmail.com>
2020-06-10 11:02:41 +0530

    Maintained same order of imports in __init__.py

--- axelrod/__init__.py
+++ axelrod/__init__.py
@@ -1,24 +1,24 @@
 DEFAULT_TURNS = 200
 
-from axelrod import graph
-from axelrod.action import Action
-from axelrod.classifier import Classifiers
-from axelrod.deterministic_cache import DeterministicCache
-from axelrod.ecosystem import Ecosystem
-from axelrod.evolvable_player import EvolvablePlayer
-from axelrod.fingerprint import AshlockFingerprint, TransitiveFingerprint
-from axelrod.game import DefaultGame, Game
-from axelrod.history import History, LimitedHistory
-from axelrod.load_data_ import load_pso_tables, load_weights
-from axelrod.match import Match
-from axelrod.match_generator import *
-from axelrod.mock_player import MockPlayer
-from axelrod.moran import ApproximateMoranProcess, MoranProcess
-from axelrod.player import Player
-from axelrod.plot import Plot
-from axelrod.random_ import Pdf, random_choice, random_flip, seed
-from axelrod.result_set import ResultSet
-from axelrod.strategies import *
-from axelrod.tournament import Tournament
 # The order of imports matters!
 from axelrod.version import __version__
+from axelrod.load_data_ import load_pso_tables, load_weights
+from axelrod import graph
+from axelrod.action import Action
+from axelrod.random_ import random_choice, random_flip, seed, Pdf
+from axelrod.plot import Plot
+from axelrod.game import DefaultGame, Game
+from axelrod.history import History, LimitedHistory
+from axelrod.player import Player
+from axelrod.classifier import Classifiers
+from axelrod.evolvable_player import EvolvablePlayer
+from axelrod.mock_player import MockPlayer
+from axelrod.match import Match
+from axelrod.moran import MoranProcess, ApproximateMoranProcess
+from axelrod.strategies import *
+from axelrod.deterministic_cache import DeterministicCache
+from axelrod.match_generator import *
+from axelrod.tournament import Tournament
+from axelrod.result_set import ResultSet
+from axelrod.ecosystem import Ecosystem
+from axelrod.fingerprint import AshlockFingerprint, TransitiveFingerprint
