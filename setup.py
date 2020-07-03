1188387147adc2ebd58243cf40451be28923b716
Marc Harper <marc.harper@gmail.com>
2020-07-02 20:28:49 -0700

    Add python 3.8 to setup.py and github CI config

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3 :: Only",
     ],
     python_requires=">=3.6",
