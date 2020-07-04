f124448a63a31074202d2140ff0b9a2c2afc5bcf
Marc Harper <marc.harper@gmail.com>
2020-07-03 21:17:11 -0700

    Update seeds for test_worse_and_worse

--- axelrod/tests/strategies/test_worse_and_worse.py
+++ axelrod/tests/strategies/test_worse_and_worse.py
@@ -25,14 +25,15 @@ class TestWorseAndWorse(TestPlayer):
         """Test that the strategy gives expected behaviour."""
         # 6 Rounds Cooperate given seed
         actions = [(C, C)] * 6 + [(D, C)] + [(C, C)] * 3
-        self.versus_test(axl.Cooperator(), expected_actions=actions, seed=8)
+        self.versus_test(axl.Cooperator(), expected_actions=actions, seed=144)
 
-        # 6 Rounds Cooperate and Defect no matter oponent
+    def test_strategy2(self):
+        # 6 Rounds Cooperate and Defect no matter opponent
         actions = [(C, D)] * 6 + [(D, D)] + [(C, D)] * 3
-        self.versus_test(axl.Defector(), expected_actions=actions, seed=8)
+        self.versus_test(axl.Defector(), expected_actions=actions, seed=144)
 
 
-class TestWorseAndWorseRandom(TestPlayer):
+class TestKnowledgeableWorseAndWorse(TestPlayer):
 
     name = "Knowledgeable Worse and Worse"
     player = axl.KnowledgeableWorseAndWorse
@@ -56,6 +57,7 @@ class TestWorseAndWorseRandom(TestPlayer):
             seed=1,
         )
 
+    def test_strategy2(self):
         # Test that behaviour does not depend on opponent
         actions = [(C, D)] + [(D, D)] * 4
         self.versus_test(
@@ -65,6 +67,7 @@ class TestWorseAndWorseRandom(TestPlayer):
             seed=1,
         )
 
+    def test_strategy3(self):
         # Test that behaviour changes when does not know length.
         actions = [(C, C), (C, D), (C, C), (C, D), (C, C)]
         self.versus_test(
@@ -96,30 +99,34 @@ class TestWorseAndWorse2(TestPlayer):
         actions = [(C, C)] * 19
         self.versus_test(axl.Cooperator(), expected_actions=actions)
 
+    def test_strategy2(self):
         actions = [(C, C), (C, C), (C, D), (D, C)]
         self.versus_test(
             opponent=axl.MockPlayer(actions=[C, C, D, C]), expected_actions=actions
         )
 
+    def test_strategy3(self):
         actions = [(C, C)] * 18 + [(C, D), (D, C)]
         self.versus_test(
             opponent=axl.MockPlayer(actions=[C] * 18 + [D, C]),
             expected_actions=actions,
         )
 
+    def test_strategy4(self):
         # After round 20, strategy follows stochastic behavior given a seed
         actions = [(C, C)] * 20 + [(C, D), (D, C), (C, C), (C, D)]
         self.versus_test(
             opponent=axl.MockPlayer(actions=[C] * 20 + [D, C, C, D]),
             expected_actions=actions,
-            seed=8,
+            seed=20,
         )
 
+    def test_strategy5(self):
         actions = [(C, C)] * 20 + [(D, D), (D, C)] + [(C, C)] * 2 + [(D, C)]
         self.versus_test(
             opponent=axl.MockPlayer(actions=[C] * 20 + [D, C, C, C]),
             expected_actions=actions,
-            seed=2,
+            seed=338,
         )
 
 
@@ -143,15 +150,17 @@ class TestWorseAndWorse3(TestPlayer):
         actions = [(C, D)] + [(D, D)] * 4
         self.versus_test(axl.Defector(), expected_actions=actions)
 
+    def test_strategy2(self):
         # Test that if opponent only cooperates, strategy also cooperates
         actions = [(C, C)] * 5
         self.versus_test(axl.Cooperator(), expected_actions=actions)
 
+    def test_strategy3(self):
         # Test that given a non 0/1 probability of defecting, strategy follows
         # stochastic behaviour, given a seed
         actions = [(C, C), (C, D), (C, C), (D, D), (C, C), (D, C)]
         self.versus_test(
             axl.MockPlayer(actions=[C, D, C, D, C]),
             expected_actions=actions,
-            seed=8,
+            seed=18,
         )
