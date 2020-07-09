5897fdbe55278ed7882b1bb9dcee3d4acefdc011
Marc Harper <marc.harper@gmail.com>
2020-07-08 18:32:50 -0700

    Add more test for Random strategy

--- axelrod/tests/strategies/test_rand.py
+++ axelrod/tests/strategies/test_rand.py
@@ -21,23 +21,23 @@ class TestRandom(TestPlayer):
         "manipulates_state": False,
     }
 
-    def test_strategy(self):
+    def test_deterministic(self):
+        actions = [(D, C), (D, C), (D, C)]
+        self.versus_test(axl.Cooperator(), expected_actions=actions, init_kwargs={"p": 0})
+
+        actions = [(C, C), (C, C), (C, C)]
+        self.versus_test(axl.Cooperator(), expected_actions=actions, init_kwargs={"p": 1})
+
+    def test_stochastic_behavior1(self):
         """Test that strategy is randomly picked (not affected by history)."""
         opponent = axl.MockPlayer()
         actions = [(C, C), (D, C), (D, C), (C, C)]
         self.versus_test(opponent, expected_actions=actions, seed=1)
 
+    def test_stochastic_behavior1(self):
         opponent = axl.MockPlayer()
         actions = [(D, C), (C, C), (D, C)]
         self.versus_test(opponent, expected_actions=actions, seed=2)
-
-        opponent = axl.MockPlayer()
-        actions = [(D, C), (D, C), (D, C)]
-        self.versus_test(opponent, expected_actions=actions, init_kwargs={"p": 0})
-
-        opponent = axl.MockPlayer()
-        actions = [(C, C), (C, C), (C, C)]
-        self.versus_test(opponent, expected_actions=actions, init_kwargs={"p": 1})
 
     def test_deterministic_classification(self):
         """Test classification when p is 0 or 1"""
