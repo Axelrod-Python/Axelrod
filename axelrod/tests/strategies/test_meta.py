bcb50081c3017281adb14b20b976c9e2a85980a0
Vince Knight <vincent.knight@gmail.com>
2021-05-24 13:09:17 +0100

    Adjust meta test.

--- axelrod/tests/strategies/test_meta.py
+++ axelrod/tests/strategies/test_meta.py
@@ -632,7 +632,7 @@ class TestNMWEDeterministic(TestMetaPlayer):
         pass
 
     def test_strategy(self):
-        actions = [(C, C), (C, D), (C, C), (D, D), (D, C)]
+        actions = [(C, C), (C, D), (D, C), (C, D), (D, C)]
         self.versus_test(opponent=axl.Alternator(), expected_actions=actions, seed=11)
 
 
