e51a31041d7ac280d1997ae0ec79f4f451f55bab
alexhroom <alex.room@btinternet.com>
2023-05-02 14:17:01 +0100

    added attributes to mock player

--- axelrod/mock_player.py
+++ axelrod/mock_player.py
@@ -9,15 +9,26 @@ C, D = Action.C, Action.D
 
 class MockPlayer(Player):
     """Creates a mock player that plays a given sequence of actions. If
-    no actions are given, plays like Cooperator. Used for testing.
+    no actions are given, or it runs out of actions,
+    plays like Cooperator. Used for testing.
+
+    Parameters
+    ----------
+    actions: List[Action], default []
+        The sequence of actions played by the mock player.
+    attributes: dict, default {}
+        A dictionary of player attributes.
     """
 
     name = "Mock Player"
+    attributes = {}
 
-    def __init__(self, actions: List[Action] = None) -> None:
+    def __init__(self, actions: List[Action] = None, attributes: dict = None) -> None:
         super().__init__()
         if not actions:
             actions = []
+        if attributes:
+            self.attributes = attributes
         self.actions = cycle(actions)
 
     def strategy(self, opponent: Player) -> Action:
