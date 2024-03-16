22d9ad0708bab5b8a4db46664d550347fa0eb52b
YibingJ <144300516+bing-j@users.noreply.github.com>
2024-03-15 21:21:34 -0400

    epsilon-greedy added to _strategies.py and reference\strategy_index.rst

--- axelrod/strategies/_strategies.py
+++ axelrod/strategies/_strategies.py
@@ -90,6 +90,7 @@ from .darwin import Darwin
 from .dbs import DBS
 from .defector import Defector, TrickyDefector
 from .doubler import Doubler
+from .epsilon_greedy import EpsilonGreedy
 from .finite_state_machines import (
     TF1,
     TF2,
@@ -334,6 +335,7 @@ all_strategies = [
     Doubler,
     DynamicTwoTitsForTat,
     EasyGo,
+    EpsilonGreedy,
     EugineNier,
     EventualCycleHunter,
     EvolvedANN,
