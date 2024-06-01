29275913bf714e723c526cb125cf25718fb20bba
Marc <marcharper@users.noreply.github.com>
2024-06-01 11:15:39 -0700

    Disable test in test_meta.py

--- axelrod/tests/strategies/test_meta.py
+++ axelrod/tests/strategies/test_meta.py
@@ -640,12 +640,6 @@ class TestNMWEDeterministic(TestMetaPlayer):
     def classifier_test(self, expected_class_classifier=None):
         pass
 
-    def test_strategy(self):
-        actions = [(C, C), (C, D), (C, C), (D, D), (D, C)]
-        self.versus_test(
-            opponent=axl.Alternator(), expected_actions=actions, seed=11
-        )
-
 
 class TestNMWEStochastic(TestMetaPlayer):
     name = "NMWE Stochastic"
