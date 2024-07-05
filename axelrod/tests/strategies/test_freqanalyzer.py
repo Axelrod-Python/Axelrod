5ca735cc08245d876ad37b8131a9e36a9de27367
Ian Miller <imaxm7@gmail.com>
2024-07-05 18:53:43 -0400

    cleanup + wip

--- axelrod/tests/strategies/test_freqanalyzer.py
+++ axelrod/tests/strategies/test_freqanalyzer.py
@@ -1,4 +1,4 @@
-"""Tests for the Stalker strategy."""
+"""Tests for the FrequencyAnalyzer strategy."""
 
 import axelrod as axl
 
@@ -7,14 +7,13 @@ from .test_player import TestPlayer
 C, D = axl.Action.C, axl.Action.D
 
 
-class TestStalker(TestPlayer):
+class Test(TestPlayer):
 
-    name = "Stalker: (D,)"
-    player = axl.Stalker
+    name = "FrequencyAnalyzer"
+    player = axl.FreqAnalyzer
     expected_classifier = {
         "memory_depth": float("inf"),
-        "stochastic": True,
-        "makes_use_of": {"game", "length"},
+        "stochastic": False,
         "long_run_time": False,
         "inspects_source": False,
         "manipulates_source": False,
@@ -22,72 +21,4 @@ class TestStalker(TestPlayer):
     }
 
     def test_strategy(self):
-        actions = [(C, C)] * 3 + [(D, C)]
-        self.versus_test(opponent=axl.Cooperator(), expected_actions=actions)
-
-        # wish_score < current_average_score < very_good_score
-        actions = [(C, C)] * 7 + [(C, D), (C, D), (C, C), (C, C), (D, C)]
-        self.versus_test(
-            opponent=axl.MockPlayer(actions=[C] * 7 + [D] * 2),
-            expected_actions=actions,
-        )
-
-        actions = [(C, C)] * 7 + [(C, D), (C, C), (D, C)]
-        self.versus_test(
-            opponent=axl.MockPlayer(actions=[C] * 7 + [D]),
-            expected_actions=actions,
-        )
-
-        # current_average_score > 2
-        actions = [(C, C)] * 9 + [(D, C)]
-        self.versus_test(axl.Cooperator(), expected_actions=actions)
-
-        # 1 < current_average_score < 2
-        actions = [(C, C)] * 7 + [(C, D)] * 4 + [(D, D)]
-        self.versus_test(
-            opponent=axl.MockPlayer(actions=[C] * 7 + [D] * 5),
-            expected_actions=actions,
-        )
-
-    def test_strategy2(self):
-        # current_average_score < 1
-        actions = (
-            [(C, D)]
-            + [(D, D)] * 2
-            + [(C, D)] * 3
-            + [(D, D), (C, D), (D, D), (C, D), (D, D), (C, D), (D, D)]
-        )
-        self.versus_test(axl.Defector(), expected_actions=actions, seed=3222)
-
-    def test_strategy3(self):
-        actions = [(C, D)] * 3 + [
-            (D, D),
-            (C, D),
-            (D, D),
-            (C, D),
-            (C, D),
-            (D, D),
-            (C, D),
-            (C, D),
-            (C, D),
-            (D, D),
-        ]
-        self.versus_test(axl.Defector(), expected_actions=actions, seed=649)
-
-    def test_strategy4(self):
-        # defect in last round
-        actions = [(C, C)] * 199 + [(D, C)]
-        self.versus_test(
-            axl.Cooperator(),
-            expected_actions=actions,
-            match_attributes={"length": 200},
-        )
-
-        # Force a defection before the end of the actual match which ensures
-        # that current_average_score > very_good_score
-        actions = [(C, C)] * 3 + [(D, C)] * 3
-        self.versus_test(
-            opponent=axl.Cooperator(),
-            expected_actions=actions,
-            match_attributes={"length": 4},
-        )
+        pass
