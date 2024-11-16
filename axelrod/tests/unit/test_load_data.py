a6f17c0e21db9c533cb1d3e8b3267cd9ae218b67
T.J. Gaffney <gaffney.tj@gmail.com>
2024-11-16 17:09:50 -0600

    Add test for new assert

--- axelrod/tests/unit/test_load_data.py
+++ axelrod/tests/unit/test_load_data.py
@@ -2,7 +2,7 @@ import os
 import pathlib
 import unittest
 
-from axelrod.load_data_ import axl_filename
+from axelrod.load_data_ import axl_filename, load_file
 
 
 class TestLoadData(unittest.TestCase):
@@ -15,3 +15,8 @@ class TestLoadData(unittest.TestCase):
         expected_fn = os.path.join(dirname, "../../strategies/titfortat.py")
 
         self.assertTrue(os.path.samefile(actual_fn, expected_fn))
+
+    def test_raise_error_if_file_empty(self):
+        path = pathlib.Path("not/a/file.py")
+        with self.assertRaises(FileNotFoundError):
+            load_file(path, ".")
