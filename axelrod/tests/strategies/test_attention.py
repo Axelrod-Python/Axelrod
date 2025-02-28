4a8fba1084b2906a9136c7238c75e57415886544
Marc-Olivier Derouin <marc-olivier.derouin@polymtl.ca>
2025-02-28 12:17:08 -0500

    Adding coverage on attention player

--- axelrod/tests/strategies/test_attention.py
+++ axelrod/tests/strategies/test_attention.py
@@ -42,6 +42,27 @@ class TestFeatureComputation(unittest.TestCase):
         self.assertEqual(features[3].item(), GameState.DefectCooperate)
         self.assertEqual(features[4].item(), GameState.CooperateDefect)
 
+    def test_compute_features_right_pad(self):
+        """Test that features are computed correctly."""
+        player = axl.MockPlayer(actions=[C, D, C, D])
+        opponent = axl.MockPlayer(actions=[D, C, C, D])
+        # Play the actions to populate history
+        match = axl.Match((player, opponent), turns=4)
+        match.play()
+
+        features = compute_features(player, opponent, True)
+
+        # Check the shape and type
+        self.assertIsInstance(features, torch.Tensor)
+        self.assertEqual(features.shape, (MEMORY_LENGTH + 1,))
+
+        # Check specific values (CLS token and game states)
+        self.assertEqual(features[0].item(), 0)  # CLS token
+        self.assertEqual(features[1].item(), GameState.DefectDefect)
+        self.assertEqual(features[2].item(), GameState.CooperateCooperate)
+        self.assertEqual(features[3].item(), GameState.DefectCooperate)
+        self.assertEqual(features[4].item(), GameState.CooperateDefect)
+
     def test_actions_to_game_state(self):
         """Test the mapping from actions to game states."""
         self.assertEqual(
@@ -50,6 +71,13 @@ class TestFeatureComputation(unittest.TestCase):
         self.assertEqual(actions_to_game_state(C, D), GameState.CooperateDefect)
         self.assertEqual(actions_to_game_state(D, C), GameState.DefectCooperate)
         self.assertEqual(actions_to_game_state(D, D), GameState.DefectDefect)
+
+
+class TestAttention(unittest.TestCase):
+    def test_initilization(self):
+        """Test that the model is initialized correctly."""
+        player = axl.Attention()
+        self.assertIsInstance(player.model, PlayerModel)
 
 
 class TestEvolvedAttention(TestPlayer):
@@ -66,7 +94,7 @@ class TestEvolvedAttention(TestPlayer):
     }
 
     def test_model_initialization(self):
-        """Test that the model is initialized with pretrained weights."""
+        """Test that the model is initialized correctly."""
         player = self.player()
         self.assertIsInstance(player.model, PlayerModel)
 
