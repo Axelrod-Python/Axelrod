1e564f50f115f8733effda9c314105d48e82ae38
cath <catherine.jg.wang@gmail.com>
2025-09-14 12:53:13 +0100

    Create test_adaptive_cooperator.py

---
+++ axelrod/tests/strategies/test_adaptive_cooperator.py
@@ -0,0 +1,31 @@
+import axelrod
+from test_player import TestPlayer
+
+C, D = axelrod.Action.C, axelrod.Action.D
+
+class TestAdaptiveCooperator(TestPlayer):
+    name = 'Adaptive Cooperator'
+    player = axelrod.AdaptiveCooperator
+    expected_classifier = {
+        'memory': 1,
+        'stochastic': False,
+        'inspects_source': False,
+        'manipulates_source': False,
+        'manipulates_state': False
+    }
+
+    def test_first__strategy(self):
+        self.first_play_test(C)
+
+    def test_strategy_cooperator(self):
+        actions = [(C, C), (C, C), (C, C), (C, C), (C, C)]
+        self.versus_test(axelrod.Cooperator(), expected_actions=actions)
+
+    def test_strategy_defector(self):
+        actions = [(C, D), (D, D), (D, D), (D, D), (D, D)]
+        self.versus_test(axelrod.Defector(), expected_actions=actions)
+
+    def test_strategy_alternator(self):
+        actions = [(C, C), (C, D), (D, C), (D, D), (D, C)]
+        self.versus_test(axelrod.Alternator(), expected_actions=actions)
+
