9ff570fe2d7c91d5f6a8cae4ee0b78c690afaa55
Paul Franceschi <5023585+paulfranceschi@users.noreply.github.com>
2026-05-15 20:04:02 +0200

    Refactor test actions in four duality optimizer tests
    Updated test actions for strategy tests with new expected outcomes.

--- axelrod/tests/strategies/test_four_duality_optimizer.py
+++ axelrod/tests/strategies/test_four_duality_optimizer.py
@@ -22,15 +22,8 @@ class TestFourDualityOptimizer1(TestPlayer):
     }
 
     def test_strategy(self):
-        actions = [(C, C), (D, D)] + [(C, C), (C, D)] * 10
-        self.versus_test(axl.Alternator(), expected_actions=actions)
 
-        actions = [(C, C), (D, C)] + [(D, C)] * 20
+        actions = [(C, C), (C, C)] + [(C, C)] * 20
         self.versus_test(axl.Cooperator(), expected_actions=actions)
 
-        opponent = axl.MockPlayer([D, C])
-        actions = [(C, D), (D, C)] + [(D, D), (D, C)] * 10
-        self.versus_test(opponent, expected_actions=actions)
 
-        actions = [(C, D), (D, D)] + [(D, D)] * 20
-        self.versus_test(axl.Defector(), expected_actions=actions)
