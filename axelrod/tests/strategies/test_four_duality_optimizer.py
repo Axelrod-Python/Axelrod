d4e392c5da1b4f46fd9a7d0c5ec91398fde35ccf
Paul Franceschi <5023585+paulfranceschi@users.noreply.github.com>
2026-05-15 20:07:24 +0200

    Add test for Retaliate strategy in four_duality_optimizer

--- axelrod/tests/strategies/test_four_duality_optimizer.py
+++ axelrod/tests/strategies/test_four_duality_optimizer.py
@@ -29,3 +29,6 @@ class TestFourDualityOptimizer1(TestPlayer):
         actions = [(C, C), (C, C)] + [(C, C)] * 20
         self.versus_test(axl.TitForTat(), expected_actions=actions)
 
+        actions = [(C, C), (C, C)] + [(C, C)] * 20
+        self.versus_test(axl.Retaliate(), expected_actions=actions)
+
