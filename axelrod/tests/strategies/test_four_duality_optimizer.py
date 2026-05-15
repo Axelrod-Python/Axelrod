3e5b96c3f8c9ff68c6d257db1fbfa096bd476e3d
Paul Franceschi <5023585+paulfranceschi@users.noreply.github.com>
2026-05-15 17:50:15 +0200

    Create test_four_duality_optimizer.py

---
+++ axelrod/tests/strategies/test_four_duality_optimizer.py
@@ -0,0 +1,36 @@
+"""Tests for the FourDualityOptimizer strategy."""
+
+import axelrod as axl
+
+from .test_four_duality_optimizer import TestFourDualityOptimizer1
+
+C, D = axl.Action.C, axl.Action.D
+
+
+class TestFourDualityOptimizer1(TestPlayer):
+
+    name = "4-Duality Optimizer 1"
+    player = axl.Handshake
+    expected_classifier = {
+        "memory_depth": float("inf"),
+        "stochastic": False,
+        "makes_use_of": set(),
+        "long_run_time": False,
+        "inspects_source": False,
+        "manipulates_source": False,
+        "manipulates_state": False,
+    }
+
+    def test_strategy(self):
+        actions = [(C, C), (D, D)] + [(C, C), (C, D)] * 10
+        self.versus_test(axl.Alternator(), expected_actions=actions)
+
+        actions = [(C, C), (D, C)] + [(D, C)] * 20
+        self.versus_test(axl.Cooperator(), expected_actions=actions)
+
+        opponent = axl.MockPlayer([D, C])
+        actions = [(C, D), (D, C)] + [(D, D), (D, C)] * 10
+        self.versus_test(opponent, expected_actions=actions)
+
+        actions = [(C, D), (D, D)] + [(D, D)] * 20
+        self.versus_test(axl.Defector(), expected_actions=actions)
