0701b6fd2aa88ac35b93c23d53c91d2ca0c9106c
Paul Franceschi <5023585+paulfranceschi@users.noreply.github.com>
2026-05-18 16:50:04 +0200

    Refactor action references to use Action directly

--- axelrod/strategies/four_duality_optimizer.py
+++ axelrod/strategies/four_duality_optimizer.py
@@ -31,7 +31,7 @@ class FourDualityOptimizer1(Player):
         waves_greater_than_one = 0
         current_wave_length = 0
         for action in opponent.history:
-            if action == axl.Action.D:
+            if action == Action.D:
                 current_wave_length += 1
             else:
                 if current_wave_length > 1:
@@ -41,7 +41,7 @@ class FourDualityOptimizer1(Player):
         # Calculate the length of our current betrayal streak
         consecutive_our_defections = 0
         for i in range(len(self.history) - 1, -1, -1):
-            if self.history[i] == axl.Action.D:
+            if self.history[i] == Action.D:
                 consecutive_our_defections += 1
             else:
                 break
@@ -49,7 +49,7 @@ class FourDualityOptimizer1(Player):
         # Calculate the length of the opponent's current betrayal streak
         consecutive_opp_defections = 0
         for i in range(len(opponent.history) - 1, -1, -1):
-            if opponent.history[i] == axl.Action.D:
+            if opponent.history[i] == Action.D:
                 consecutive_opp_defections += 1
             else:
                 break
